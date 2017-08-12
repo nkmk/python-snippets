34d53fbc05e7c93a38e9f40c035feea428b8935e
nkmk <nkmk.on@gmail.com>
2017-08-12 22:09:27 +0900

    add urllib_request

---
+++ notebook/urllib_request.py
@@ -0,0 +1,14 @@
+# http://docs.python.jp/3.6/library/urllib.request.html
+
+import urllib.request
+
+url = "https://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"
+path = "data/src/lenna_square.png"
+
+result = urllib.request.urlretrieve(url, path)
+print(result)
+# ('data/src/lenna_square.png', <http.client.HTTPMessage object at 0x10cce6cf8>)
+
+data = urllib.request.urlopen(url).read()
+with open(path, mode="wb") as f:
+    f.write(data)
