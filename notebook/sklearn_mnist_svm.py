64d3a16f8993223b6fc75f4d969e79b4e1625167
nkmk <nkmk.on@gmail.com>
2017-08-14 15:08:27 +0900

    update sklearn mnist svm

--- notebook/sklearn_mnist_svm.py
+++ notebook/sklearn_mnist_svm.py
@@ -26,16 +26,53 @@ pre = clf.predict(data_test)
 
 ac_score = metrics.accuracy_score(label_test, pre)
 print(ac_score)
-# 0.66
+# 0.73
 
-train_size = 5000
-test_size = 1000
+import timeit
+
+num = 10
+
+train_size = 500
+test_size = 100
 data_train, data_test, label_train, label_test = model_selection.train_test_split(mnist_data, mnist_label, test_size=test_size, train_size=train_size)
 
 clf = svm.SVC()
+print(timeit.timeit(lambda: clf.fit(data_train, label_train), number=num) / num)
+pre = clf.predict(data_test)
+ac_score = metrics.accuracy_score(label_test, pre)
+print(ac_score)
+# 0.35348284359788523
+# 0.79
+
+clf = svm.LinearSVC()
+print(timeit.timeit(lambda: clf.fit(data_train, label_train), number=num) / num)
+pre = clf.predict(data_test)
+ac_score = metrics.accuracy_score(label_test, pre)
+print(ac_score)
+# 0.10056947720004246
+# 0.77
+
+train_size = 10000
+test_size = 2000
+data_train, data_test, label_train, label_test = model_selection.train_test_split(mnist_data, mnist_label, test_size=test_size, train_size=train_size)
+
+clf = svm.LinearSVC()
 clf.fit(data_train, label_train)
 pre = clf.predict(data_test)
 
 ac_score = metrics.accuracy_score(label_test, pre)
 print(ac_score)
-# 0.907
+# 0.8805
+
+co_mat = metrics.confusion_matrix(label_test, pre)
+print(co_mat)
+# [[187   0   4   1   0   2   1   1   2   0]
+#  [  0 229   3   4   0   1   1   0   2   0]
+#  [  1   3 152   5   1   1   1   3   8   2]
+#  [  2   4  11 178   0   4   0   1   6   7]
+#  [  3   2   5   1 182   0   2   1   0  10]
+#  [  2   2   0   8   3 150   6   1   8   3]
+#  [  3   2   1   0   1   1 192   0   5   0]
+#  [  0   3   4   0   1   5   0 192   0   8]
+#  [  0   2   3   5   2   6   2   0 140   6]
+#  [  6   1   0   3  13   5   0  11   1 159]]
