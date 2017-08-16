2f25d533ca3c8867de07e150ca839fca186bf430
nkmk <nkmk.on@gmail.com>
2017-08-16 10:32:37 +0900

    update sklearn_mnist_svm

--- notebook/sklearn_mnist_svm.py
+++ notebook/sklearn_mnist_svm.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn import datasets, model_selection, svm, metrics
 
 mnist = datasets.fetch_mldata('MNIST original', data_home='data/src/download/')
@@ -8,8 +7,8 @@ print(mnist.keys())
 # <class 'sklearn.datasets.base.Bunch'>
 # dict_keys(['DESCR', 'COL_NAMES', 'target', 'data'])
 
-mnist_data = pd.DataFrame(mnist.data / 255)
-mnist_label = pd.Series(mnist.target)
+mnist_data = mnist.data / 255
+mnist_label = mnist.target
 
 print(mnist_data.shape)
 print(mnist_label.shape)
@@ -26,7 +25,7 @@ pre = clf.predict(data_test)
 
 ac_score = metrics.accuracy_score(label_test, pre)
 print(ac_score)
-# 0.71
+# 0.77
 
 import timeit
 
@@ -41,38 +40,39 @@ print(timeit.timeit(lambda: clf.fit(data_train, label_train), number=num) / num)
 pre = clf.predict(data_test)
 ac_score = metrics.accuracy_score(label_test, pre)
 print(ac_score)
-# 0.35409306720248424
-# 0.77
+# 0.3631532890023664
+# 0.73
 
 clf = svm.LinearSVC()
 print(timeit.timeit(lambda: clf.fit(data_train, label_train), number=num) / num)
 pre = clf.predict(data_test)
 ac_score = metrics.accuracy_score(label_test, pre)
 print(ac_score)
-# 0.11930906050256454
-# 0.77
+# 0.10481857029953971
+# 0.76
 
 train_size = 10000
 test_size = 2000
 data_train, data_test, label_train, label_test = model_selection.train_test_split(mnist_data, mnist_label, test_size=test_size, train_size=train_size)
 
 clf = svm.LinearSVC()
-clf.fit(data_train, label_train)
+print(timeit.timeit(lambda: clf.fit(data_train, label_train), number=num) / num)
 pre = clf.predict(data_test)
 
 ac_score = metrics.accuracy_score(label_test, pre)
 print(ac_score)
-# 0.8965
+# 5.980748841702007
+# 0.8945
 
 co_mat = metrics.confusion_matrix(label_test, pre)
 print(co_mat)
-# [[162   0   0   2   0   0   0   1   1   0]
-#  [  0 249   3   0   0   1   1   1   3   0]
-#  [  2   2 183   7   3   1   2   3   4   0]
-#  [  1   1  10 178   2   4   0   4   5   2]
-#  [  1   2   2   0 158   2   2   0   6   6]
-#  [  4   1   2   8   1 160   5   2   2   5]
-#  [  3   0   3   1   2   1 176   1   2   0]
-#  [  2   0   1   1   3   0   0 187   1  10]
-#  [  4   5   2   5   3   3   4   0 167   4]
-#  [  0   1   2   1  11   2   0   8   4 173]]
+# [[186   0   0   2   1   1   0   1   2   0]
+#  [  0 210   1   0   1   2   2   1   3   0]
+#  [  1   3 192   8   2   6   2   3   5   3]
+#  [  1   1   7 163   0   6   1   1   7   2]
+#  [  1   0   1   0 166   1   2   2   1   6]
+#  [  5   3   1   6   5 168   7   3   6   3]
+#  [  1   0   4   0   1   4 170   0   3   0]
+#  [  1   1   3   2   2   0   0 196   0   6]
+#  [  0   4   3   3   3  13   1   1 162   6]
+#  [  2   1   0   2   5   2   0   6   2 176]]
