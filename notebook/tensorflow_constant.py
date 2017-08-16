179e45062aa18966027157777df0f7195d0b86a5
nkmk <nkmk.on@gmail.com>
2017-08-16 12:48:29 +0900

    update tensorflow_constant

--- notebook/tensorflow_constant.py
+++ notebook/tensorflow_constant.py
@@ -8,33 +8,9 @@ print(const2)
 # Tensor("Const:0", shape=(), dtype=int32)
 # Tensor("Const_1:0", shape=(), dtype=int32)
 
-add_op = tf.add(const1, const2)
-mul_op = tf.multiply(add_op, const2)
-
-print(add_op)
-print(mul_op)
-# Tensor("Add:0", shape=(), dtype=int32)
-# Tensor("Mul:0", shape=(), dtype=int32)
-
 with tf.Session() as sess:
     const1_result, const2_result = sess.run([const1, const2])
     print(const1_result)
     print(const2_result)
-
-    add_result, mul_result = sess.run([add_op, mul_op])
-    print(add_result)
-    print(mul_result)
 # 5
 # 10
-# 15
-# 150
-
-add_op_2 = const1 + const2
-mul_op_2 = const1 * const2
-
-with tf.Session() as sess:
-    add_op_2_result, mul_op_2_result = sess.run([add_op_2, mul_op_2])
-    print(add_op_2_result)
-    print(mul_op_2_result)
-# 15
-# 50
