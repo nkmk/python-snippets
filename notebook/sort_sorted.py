c342fe6b863832acdd491e0f0abbca9679a9798f
nkmk <nkmk.on@gmail.com>
2017-09-03 06:05:40 +0900

    add sort_sorted

---
+++ notebook/sort_sorted.py
@@ -0,0 +1,13 @@
+org_list = [6, 1, 4, 5, -4]
+
+org_list.sort()
+print(org_list)
+# [-4, 1, 4, 5, 6]
+
+org_list = [6, 1, 4, 5, -4]
+
+new_list = sorted(org_list)
+print(org_list)
+print(new_list)
+# [6, 1, 4, 5, -4]
+# [-4, 1, 4, 5, 6]
