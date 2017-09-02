953e9c652f40325a180d39ec3d91edb3cd9dad29
nkmk <nkmk.on@gmail.com>
2017-09-03 05:43:49 +0900

    add divmode_test

---
+++ notebook/divmod-test.py
@@ -0,0 +1,14 @@
+q = 10 // 3
+mod = 10 % 3
+print(q, mod)
+# 3 1
+
+q, mod = divmod(10, 3)
+print(q, mod)
+# 3 1
+
+answer = divmod(10, 3)
+print(answer)
+print(answer[0], answer[1])
+# (3, 1)
+# 3 1
