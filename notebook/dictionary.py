7b03713f278ae2dfeb2180e2409fb40d3fef7c95
nkmk <nkmk.on@gmail.com>
2017-09-03 09:09:37 +0900

    update dictionary

--- notebook/dictionary.py
+++ notebook/dictionary.py
@@ -1,26 +1,37 @@
+# https://docs.python.jp/3/library/stdtypes.html#mapping-types-dict
+
 d = {'one': 1, 'two': 2, 'three': 3}
 
+print(d)
 print(d.keys())
 print(d.values())
 print(d.items())
+# {'one': 1, 'two': 2, 'three': 3}
 # dict_keys(['one', 'two', 'three'])
 # dict_values([1, 2, 3])
 # dict_items([('one', 1), ('two', 2), ('three', 3)])
 
 print(d.get('one'))
-print(d.get('four'))
-print(d.get('five', 'nothing'))
 # 1
+
+print(d.get('four'))
 # None
-# nothing
+
+print(d.get('five', 'default_value'))
+# default_value
+
+print(d)
+# {'one': 1, 'two': 2, 'three': 3}
 
 print(d.setdefault('one'))
+# 1
+
 print(d.setdefault('four'))
 print(d)
-print(d.setdefault('five', 'nothing'))
-print(d)
-# 1
 # None
 # {'one': 1, 'two': 2, 'three': 3, 'four': None}
-# nothing
-# {'one': 1, 'two': 2, 'three': 3, 'four': None, 'five': 'nothing'}
+
+print(d.setdefault('five', 'default_value'))
+print(d)
+# default_value
+# {'one': 1, 'two': 2, 'three': 3, 'four': None, 'five': 'default_value'}
