ef5c1b69fa029f9082d1b9e440ebbf1d43d8571b
nkmk <nkmk.on@gmail.com>
2017-10-28 22:22:02 +0900

    rename

--- notebook/numpy_image_gamma.py
+++ notebook/numpy_image_gamma.py
@@ -9,4 +9,4 @@ im_22 = 255.0 * (im / 255.0)**2.2
 im_gamma = np.concatenate((im_1_22, im, im_22), axis=1)
 
 pil_img = Image.fromarray(np.uint8(im_gamma))  # floatをuintに変換
-pil_img.save('data/dst/lena_gamma.jpg')
+pil_img.save('data/dst/lena_numpy_gamma.jpg')
--- notebook/numpy_image_split_color.py
+++ notebook/numpy_image_split_color.py
@@ -23,4 +23,4 @@ im_RGB = np.concatenate((im_R, im_G, im_B), axis=1)
 # im_RGB = np.c_['1', im_R, im_G, im_B]
 
 pil_img = Image.fromarray(im_RGB)
-pil_img.save('data/dst/lena_split_color.jpg')
+pil_img.save('data/dst/lena_numpy_split_color.jpg')
