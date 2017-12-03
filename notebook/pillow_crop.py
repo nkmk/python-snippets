d969d4c00be37e4e5798188f160685876f30ad0f
nkmk <nkmk.on@gmail.com>
2017-12-03 10:24:18 +0900

    update pillow crop

--- notebook/pillow_crop.py
+++ notebook/pillow_crop.py
@@ -1,29 +1,17 @@
 from PIL import Image
 
-im = Image.open('data/src/lena_square.png')
+im = Image.open('data/src/lena.jpg')
 
-# ![lena_square](data/src/lena_square.png)
+# ![lena](data/src/lena.jpg)
 
-im_crop = im.crop((100, 200, 400, 300))
-im_crop.save('data/dst/lena_square_pillow_crop.jpg', quality=95)
+im_crop = im.crop((100, 75, 300, 150))
+im_crop.save('data/dst/lena_pillow_crop.jpg', quality=95)
 
-im.crop((100, 200, 400, 300)).save('data/dst/lena_square_pillow_crop.jpg', quality=95)
+im.crop((100, 75, 300, 150)).save('data/dst/lena_pillow_crop.jpg', quality=95)
 
-# ![lena_square_pillow_crop](data/dst/lena_square_pillow_crop.jpg)
+# ![lena_pillow_crop](data/dst/lena_pillow_crop.jpg)
 
-im_crop_outside = im.crop((300, 300, 600, 600))
-im_crop_outside.save('data/dst/lena_square_pillow_crop_outside.jpg', quality=95)
+im_crop_outside = im.crop((100, 175, 300, 250))
+im_crop_outside.save('data/dst/lena_pillow_crop_outside.jpg', quality=95)
 
-# ![lena_square_pillow_crop_outside](data/dst/lena_square_pillow_crop_outside.jpg)
-
-def crop_center(pil_img, width, height):
-    img_width, img_height = pil_img.size
-    return pil_img.crop(((img_width - width) / 2,
-                         (img_height - height) / 2,
-                         (img_width + width) / 2,
-                         (img_height + height) / 2))
-
-im_crop_center = crop_center(im, 200, 200)
-im_crop_center.save('data/dst/lena_square_pillow_crop_center.jpg', quality=95)
-
-# ![lena_square_pillow_crop_center](data/dst/lena_square_pillow_crop_center.jpg)
+# ![lena_pillow_crop_outside](data/dst/lena_pillow_crop_outside.jpg)
