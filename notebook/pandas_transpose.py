9678f4123cdb7cc239b15eb1c2f919fdcafee179
nkmk <nkmk.on@gmail.com>
2018-01-12 12:52:17 +0900

    add pandas transpose

---
+++ notebook/pandas_transpose.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv('data/src/sample_pandas_normal.csv').head(3)
+print(df)
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 1      Bob   42    CA     92
+# 2  Charlie   18    CA     70
+
+df_t = df.T
+print(df_t)
+#            0    1        2
+# name   Alice  Bob  Charlie
+# age       24   42       18
+# state     NY   CA       CA
+# point     64   92       70
+
+df_tr =df.transpose()
+print(df_tr)
+#            0    1        2
+# name   Alice  Bob  Charlie
+# age       24   42       18
+# state     NY   CA       CA
+# point     64   92       70
+
+df = df.T
+print(df)
+#            0    1        2
+# name   Alice  Bob  Charlie
+# age       24   42       18
+# state     NY   CA       CA
+# point     64   92       70
