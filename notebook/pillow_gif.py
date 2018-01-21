f30f4e583d59a2ece33feeca9de533ea784b1965
nkmk <nkmk.on@gmail.com>
2018-01-21 21:45:33 +0900

    add pillow gif

---
+++ notebook/pillow_gif.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageDraw
+
+images = []
+
+width = 200
+center = width // 2
+color_1 = (0, 0, 0)
+color_2 = (255, 255, 255)
+max_radius = int(center * 1.5)
+step = 8
+
+for i in range(0, max_radius, step):
+    im = Image.new('RGB', (width, width), color_1)
+    draw = ImageDraw.Draw(im)
+    draw.ellipse((center - i, center - i, center + i, center + i), fill=color_2)
+    images.append(im)
+
+for i in range(0, max_radius, step):
+    im = Image.new('RGB', (width, width), color_2)
+    draw = ImageDraw.Draw(im)
+    draw.ellipse((center - i, center - i, center + i, center + i), fill=color_1)
+    images.append(im)
+
+im_0 = images.pop(0)
+im_0.save('data/dst/test.gif', save_all=True, append_images=images, optimize=False, duration=40)
