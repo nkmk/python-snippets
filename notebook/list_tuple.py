bfb6e36b868a0b600278c0a3a794e2c00c2640f0
nkmk <nkmk.on@gmail.com>
2018-02-13 19:53:49 +0900

    update list tuple

--- notebook/list_tuple.py
+++ notebook/list_tuple.py
@@ -44,17 +44,32 @@ print(type(rt))
 # TypeError: 'tuple' object does not support item assignment
 
 tl = list(t)
-tl.append('four')
-t_new = tuple(tl)
-print(t_new)
-print(type(t_new))
-# ('one', 'two', 'three', 'four')
-# <class 'tuple'>
-
-tl = list(t)
 tl[0] = 'ONE'
 t_new = tuple(tl)
 print(t_new)
 print(type(t_new))
 # ('ONE', 'two', 'three')
+# <class 'tuple'>
+
+t2 = t + ('four', 'five')
+print(t)
+print(t2)
+# ('one', 'two', 'three')
+# ('one', 'two', 'three', 'four', 'five')
+
+# t2 = t + ('four')
+# TypeError: can only concatenate tuple (not "str") to tuple
+
+t2 = t + ('four', )
+print(t)
+print(t2)
+# ('one', 'two', 'three')
+# ('one', 'two', 'three', 'four')
+
+tl = list(t)
+tl.insert(2, 'XXX')
+t_new = tuple(tl)
+print(t_new)
+print(type(t_new))
+# ('one', 'two', 'XXX', 'three')
 # <class 'tuple'>
