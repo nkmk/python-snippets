89e5645981cca40159ab7157603e29bf4deaacf7
nkmk <nkmk.on@gmail.com>
2018-03-14 20:06:48 +0900

    update format, numeric types

--- notebook/format.py
+++ notebook/format.py
@@ -1,88 +1,180 @@
-# https://docs.python.jp/3/library/string.html#format-string-syntax
-# https://docs.python.jp/3/library/string.html#formatspec
-# https://docs.python.jp/3/library/string.html#format-examples
+s = format(255, '04x')
+print(s)
+print(type(s))
+# 00ff
+# <class 'str'>
 
-print('{}'.format('foo'))
-# foo
+print(format('center', '*^16'))
+# *****center*****
 
-print('{}, {}, {}'.format('one', 'two', 'three'))
-# one, two, three
+s = '{:04x}'.format(255)
+print(s)
+print(type(s))
+# 00ff
+# <class 'str'>
 
-print('{0}, {1}, {0}'.format('foo', 'bar'))
-# foo, bar, foo
+print('{:*^16}'.format('center'))
+# *****center*****
 
-print('{word1}, {word2}, {word1}'.format(word1='one', word2='two'))
-# one, two, one
+print('{}-{}-{}'.format('100', '二百', 300))
+# 100-二百-300
+
+print('{0}-{1}-{0}'.format('foo', 'bar'))
+# foo-bar-foo
+
+print('{year}年{month}月{day}日'.format(year=2018, month=1, day=11))
+# 2018年1月11日
 
 person1 = {'name': 'Alice', 'age': 20}
 person2 = {'name': 'Bob', 'age': 30}
-print('{0[name]} is {0[age]} years old. {1[name]} is {1[age]} years old.'.format(person1, person2))
-# Alice is 20 years old. Bob is 30 years old.
+print('{0[name]} is {0[age]} years old.{1[name]} is {1[age]} years old.'.format(person1, person2))
+# Alice is 20 years old.Bob is 30 years old.
 
-print('{number:0>5}'.format(number=123))
-# 00123
+print('{num:x}'.format(num=255))
+# ff
 
-i = 100
-print('left  : {:<10}'.format(i))
-print('center: {:^10}'.format(i))
-print('right : {:>10}'.format(i))
+print('{year}年{month:02}月{day:02}日'.format(year=2018, month=1, day=11))
+# 2018年01月11日
+
+print('{{}}-{num}-{{{num}}}'.format(num=100))
+# {}-100-{100}
+
+print('left  : {:<10}'.format(100))
+print('center: {:^10}'.format(100))
+print('right : {:>10}'.format(100))
 # left  : 100       
 # center:    100    
 # right :        100
 
-print('left  : {:*<10}'.format(i))
-print('center: {:*^10}'.format(i))
-print('right : {:*>10}'.format(i))
+print('left  : {:*<10}'.format(100))
+print('center: {:a^10}'.format(100))
+print('right : {:鬼>10}'.format(100))
 # left  : 100*******
-# center: ***100****
-# right : *******100
+# center: aaa100aaaa
+# right : 鬼鬼鬼鬼鬼鬼鬼100
 
-print('zero padding 1: {:0>10}'.format(i))
-print('zero padding 2: {:010}'.format(i))
-# zero padding 1: 0000000100
-# zero padding 2: 0000000100
+print('zero padding: {:0>10}'.format(100))
+print('zero padding: {:010}'.format(100))
+# zero padding: 0000000100
+# zero padding: 0000000100
 
-i = 255
-print('bin: {:b}'.format(i))
-print('oct: {:o}'.format(i))
-print('dec: {:d}'.format(i))
-print('hex: {:x}'.format(i))
-print('HEX: {:X}'.format(i))
+print('sign: {}'.format(100))
+print('sign: {}'.format(-100))
+# sign: 100
+# sign: -100
+
+print('sign: {:+}'.format(100))
+print('sign: {:+}'.format(-100))
+# sign: +100
+# sign: -100
+
+print('sign: {: }'.format(100))
+print('sign: {: }'.format(-100))
+# sign:  100
+# sign: -100
+
+print('sign: {:06}'.format(100))
+print('sign: {:06}'.format(-100))
+# sign: 000100
+# sign: -00100
+
+print('sign: {:+06}'.format(100))
+print('sign: {:+06}'.format(-100))
+# sign: +00100
+# sign: -00100
+
+print('sign: {: 06}'.format(100))
+print('sign: {: 06}'.format(-100))
+# sign:  00100
+# sign: -00100
+
+print('{:,}'.format(100000000))
+# 100,000,000
+
+print('{:_}'.format(100000000))
+# 100_000_000
+
+print('bin: {:b}'.format(255))
+print('oct: {:o}'.format(255))
+print('dec: {:d}'.format(255))
+print('hex: {:x}'.format(255))
+print('HEX: {:X}'.format(255))
 # bin: 11111111
 # oct: 377
 # dec: 255
 # hex: ff
 # HEX: FF
 
-print('bin: {:08b}'.format(i))
-print('oct: {:08o}'.format(i))
-print('dec: {:08d}'.format(i))
-print('hex: {:08x}'.format(i))
-print('HEX: {:08X}'.format(i))
+print('bin: {:08b}'.format(255))
+print('oct: {:08o}'.format(255))
+print('dec: {:08d}'.format(255))
+print('hex: {:08x}'.format(255))
+print('HEX: {:08X}'.format(255))
 # bin: 11111111
 # oct: 00000377
 # dec: 00000255
 # hex: 000000ff
 # HEX: 000000FF
 
-i = 100000000
-print('{:,}'.format(i))
-# 100,000,000
+print('bin: {:#010b}'.format(255))
+print('oct: {:#010o}'.format(255))
+print('dec: {:#010d}'.format(255))
+print('hex: {:#010x}'.format(255))
+print('HEX: {:#010X}'.format(255))
+# bin: 0b11111111
+# oct: 0o00000377
+# dec: 0000000255
+# hex: 0x000000ff
+# HEX: 0X000000FF
 
-print('{:e}'.format(i))
-print('{:E}'.format(i))
-# 1.000000e+08
-# 1.000000E+08
+print('hex: {:08x}'.format(255))
+print('hex: {:09_x}'.format(255))
+print('hex: {:#011_x}'.format(255))
+# hex: 000000ff
+# hex: 0000_00ff
+# hex: 0x0000_00ff
 
-ratio = 0.4567
-print('{:.2}'.format(ratio))
-print('{:.5}'.format(ratio))
-print('{:.5f}'.format(ratio))
-# 0.46
-# 0.4567
-# 0.45670
+# print('hex: {:08x}'.format('255'))
+# ValueError: Unknown format code 'X' for object of type 'str'
 
-print('{:%}'.format(ratio))
-print('{:.1%}'.format(ratio))
-# 45.670000%
-# 45.7%
+print('hex: {:08x}'.format(int('255')))
+# hex: 000000ff
+
+print('{:.2}'.format(1.234))
+print('{:.5}'.format(1.234))
+print('{:.5f}'.format(1.234))
+# 1.2
+# 1.234
+# 1.23400
+
+print('{}'.format(0.0001234))
+print('{}'.format(0.00001234))
+# 0.0001234
+# 1.234e-05
+
+print('{}'.format(1234000000000000.0))
+print('{}'.format(12340000000000000.0))
+print('{}'.format(12340000000000000000000000))
+# 1234000000000000.0
+# 1.234e+16
+# 12340000000000000000000000
+
+print('{:e}'.format(0.0001234))
+print('{:E}'.format(0.0001234))
+# 1.234000e-04
+# 1.234000E-04
+
+print('{:.4e}'.format(0.0001234))
+print('{:.2E}'.format(0.0001234))
+# 1.2340e-04
+# 1.23E-04
+
+print('{:%}'.format(0.12345))
+print('{:.2%}'.format(0.12345))
+# 12.345000%
+# 12.35%
+
+print('{:%}'.format(10))
+print('{:.2%}'.format(10))
+# 1000.000000%
+# 1000.00%
--- notebook/numeric_types.py
+++ notebook/numeric_types.py
@@ -1,9 +1,21 @@
 bin_num = 0b10
 oct_num = 0o10
 hex_num = 0x10
+
 print(bin_num)
 print(oct_num)
 print(hex_num)
+# 2
+# 8
+# 16
+
+Bin_num = 0B10
+Oct_num = 0O10
+Hex_num = 0X10
+
+print(Bin_num)
+print(Oct_num)
+print(Hex_num)
 # 2
 # 8
 # 16
@@ -15,16 +27,6 @@ print(type(hex_num))
 # <class 'int'>
 # <class 'int'>
 
-Bin_num = 0B10
-Oct_num = 0O10
-Hex_num = 0X10
-print(Bin_num)
-print(Oct_num)
-print(Hex_num)
-# 2
-# 8
-# 16
-
 print(type(Bin_num))
 print(type(Oct_num))
 print(type(Hex_num))
@@ -32,15 +34,23 @@ print(type(Hex_num))
 # <class 'int'>
 # <class 'int'>
 
-num = 0b10 * 0o10 + 0x10
-print(num)
+result = 0b10 * 0o10 + 0x10
+print(result)
 # 32
+
+print(0b111111111111 is 0b1_1_1_1_1_1_1_1_1_1_1_1)
+# True
+
+bin_num = 0b1111_1111_1111
+print(bin_num)
+# 4095
 
 i = 255
 
 bin_str = bin(i)
 oct_str = oct(i)
 hex_str = hex(i)
+
 print(bin_str)
 print(oct_str)
 print(hex_str)
@@ -58,6 +68,7 @@ print(type(hex_str))
 bin_str = format(i, 'b')
 oct_str = format(i, 'o')
 hex_str = format(i, 'x')
+
 print(bin_str)
 print(oct_str)
 print(hex_str)
@@ -72,9 +83,21 @@ print(type(hex_str))
 # <class 'str'>
 # <class 'str'>
 
+bin_str = format(i, '#b')
+oct_str = format(i, '#o')
+hex_str = format(i, '#x')
+
+print(bin_str)
+print(oct_str)
+print(hex_str)
+# 0b11111111
+# 0o377
+# 0xff
+
 bin_str = format(i, '08b')
 oct_str = format(i, '08o')
 hex_str = format(i, '08x')
+
 print(bin_str)
 print(oct_str)
 print(hex_str)
@@ -82,12 +105,16 @@ print(hex_str)
 # 00000377
 # 000000ff
 
-print(type(bin_str))
-print(type(oct_str))
-print(type(hex_str))
-# <class 'str'>
-# <class 'str'>
-# <class 'str'>
+bin_str = format(i, '#010b')
+oct_str = format(i, '#010o')
+hex_str = format(i, '#010x')
+
+print(bin_str)
+print(oct_str)
+print(hex_str)
+# 0b11111111
+# 0o00000377
+# 0x000000ff
 
 print('bin is {:08b}'.format(i))
 print('oct is {:08o}'.format(i))
@@ -100,6 +127,7 @@ dec_num = int('10')
 bin_num = int('10', 2)
 oct_num = int('10', 8)
 hex_num = int('10', 16)
+
 print(dec_num)
 print(bin_num)
 print(oct_num)
@@ -109,10 +137,20 @@ print(hex_num)
 # 8
 # 16
 
+print(type(dec_num))
+print(type(bin_num))
+print(type(oct_num))
+print(type(hex_num))
+# <class 'int'>
+# <class 'int'>
+# <class 'int'>
+# <class 'int'>
+
 dec_num = int('10', 0)
 bin_num = int('0b10', 0)
 oct_num = int('0o10', 0)
 hex_num = int('0x10', 0)
+
 print(dec_num)
 print(bin_num)
 print(oct_num)
@@ -125,6 +163,7 @@ print(hex_num)
 Bin_num = int('0B10', 0)
 Oct_num = int('0O10', 0)
 Hex_num = int('0X10', 0)
+
 print(Bin_num)
 print(Oct_num)
 print(Hex_num)
@@ -134,8 +173,19 @@ print(Hex_num)
 
 a = '0b1001'
 b = '0b0011'
+
 c = int(a, 0) + int(b, 0)
+
 print(c)
 print(bin(c))
 # 12
 # 0b1100
+
+a_0b = '0b1110001010011'
+
+a_0x = format(int(a, 0), '#010x')
+a_0o = format(int(a, 0), '#010o')
+print(a_0x)
+print(a_0o)
+# 0x00000009
+# 0o00000011
