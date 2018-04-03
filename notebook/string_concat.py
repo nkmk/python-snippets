6c84b4c81063dc81ec8a3f5117674075a3d3e79b
nkmk <nkmk.on@gmail.com>
2018-04-03 21:01:37 +0900

    update string concat

--- notebook/string_concat.py
+++ notebook/string_concat.py
@@ -14,9 +14,15 @@ s = s1 + s2 + s3 + 'ddd'
 print(s)
 # aaabbbcccddd
 
-s = 'aaa' + str(100) + 'bbb' + str(0.25)
+s1 += s2
+print(s1)
+# aaabbb
+
+s = 'aaa'
+
+s += 'xxx'
 print(s)
-# aaa100bbb0.25
+# aaaxxx
 
 s = 'aaa''bbb''ccc'
 print(s)
@@ -34,6 +40,27 @@ print(s)
 
 # s = s1 s2 s3
 # SyntaxError: invalid syntax
+
+s1 = 'aaa'
+s2 = 'bbb'
+
+i = 100
+f = 0.25
+
+# s = s1 + i
+# TypeError: must be str, not int
+
+s = s1 + '_' + str(i) + '_' + s2 + '_' + str(f)
+print(s)
+# aaa_100_bbb_0.25
+
+s = s1 + '_' + format(i, '05') + '_' + s2 + '_' + format(f, '.5f')
+print(s)
+# aaa_00100_bbb_0.25000
+
+s = '{}_{:05}_{}_{:.5f}'.format(s1, i, s2, f)
+print(s)
+# aaa_00100_bbb_0.25000
 
 l = ['aaa', 'bbb', 'ccc']
 
