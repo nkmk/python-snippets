a8e8dc3356f11a5d0c4c48dff60d8c68bcbfa4b7
nkmk <nkmk.on@gmail.com>
2018-04-03 21:15:54 +0900

    update pandas reset_index

--- notebook/pandas_reset_index.py
+++ notebook/pandas_reset_index.py
@@ -49,3 +49,46 @@ print(df)
 # 3    Alice   24    NY     64
 # 4    Frank   30    NY     57
 # 5     Dave   68    TX     70
+
+df = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=0)
+print(df)
+#          age state  point
+# name                     
+# Alice     24    NY     64
+# Bob       42    CA     92
+# Charlie   18    CA     70
+# Dave      68    TX     70
+# Ellen     24    CA     88
+# Frank     30    NY     57
+
+df_r = df.reset_index()
+print(df_r)
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 1      Bob   42    CA     92
+# 2  Charlie   18    CA     70
+# 3     Dave   68    TX     70
+# 4    Ellen   24    CA     88
+# 5    Frank   30    NY     57
+
+df_s = df.set_index('state')
+print(df_s)
+#        age  point
+# state            
+# NY      24     64
+# CA      42     92
+# CA      18     70
+# TX      68     70
+# CA      24     88
+# NY      30     57
+
+df_rs = df.reset_index().set_index('state')
+print(df_rs)
+#           name  age  point
+# state                     
+# NY       Alice   24     64
+# CA         Bob   42     92
+# CA     Charlie   18     70
+# TX        Dave   68     70
+# CA       Ellen   24     88
+# NY       Frank   30     57
