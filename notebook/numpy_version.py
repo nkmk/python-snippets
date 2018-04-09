f52a4c109037cc185386376f2b2fb52838c6eca4
nkmk <nkmk.on@gmail.com>
2018-04-09 20:20:47 +0900

    add numpy pandas version

---
+++ notebook/numpy_version.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+print(np.__version__)
+# 1.14.1
+
+print(np.version.version)
+# 1.14.1
+
+print(np.version.short_version)
+# 1.14.1
+
+print(np.version.full_version)
+# 1.14.1
+
+print(np.version.git_revision)
+# 7dcee7a469ad1bbfef1cd8980dc18bf5869c5391
+
+print(np.version.release)
+# True
---
+++ notebook/pandas_version.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+print(pd.__version__)
+# 0.22.0
+
+pd.show_versions()
+# INSTALLED VERSIONS
+# ------------------
+# commit: None
+# python: 3.6.5.final.0
+# python-bits: 64
+# OS: Darwin
+# OS-release: 17.5.0
+# machine: x86_64
+# processor: i386
+# byteorder: little
+# LC_ALL: None
+# LANG: ja_JP.UTF-8
+# LOCALE: ja_JP.UTF-8
+# pandas: 0.22.0
+# pytest: None
+# pip: 9.0.3
+# setuptools: 39.0.1
+# Cython: 0.26
+# numpy: 1.14.1
+# scipy: 0.18.1
+# pyarrow: None
+# xarray: None
+# IPython: 6.1.0
+# sphinx: None
+# patsy: None
+# dateutil: 2.6.1
+# pytz: 2018.3
+# blosc: None
+# bottleneck: None
+# tables: None
+# numexpr: None
+# feather: None
+# matplotlib: 2.0.0
+# openpyxl: 2.4.8
+# xlrd: 1.0.0
+# xlwt: None
+# xlsxwriter: None
+# lxml: 4.1.1
+# bs4: 4.6.0
+# html5lib: 0.9999999
+# sqlalchemy: 1.1.10
+# pymysql: None
+# psycopg2: None
+# jinja2: 2.9.6
+# s3fs: None
+# fastparquet: None
+# pandas_gbq: None
+# pandas_datareader: 0.6.0
