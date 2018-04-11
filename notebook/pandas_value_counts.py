ae29f60b4b803662edabb16d93c9c42298f3e3b5
nkmk <nkmk.on@gmail.com>
2018-04-11 22:55:21 +0900

    update pandas value_counts

--- notebook/pandas_value_counts.py
+++ notebook/pandas_value_counts.py
@@ -1,59 +1,116 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/src/sample_pandas_normal.csv')
+df.iloc[1] = np.nan
 print(df)
-#       name  age state  point
-# 0    Alice   24    NY     64
-# 1      Bob   42    CA     92
-# 2  Charlie   18    CA     70
-# 3     Dave   68    TX     70
-# 4    Ellen   24    CA     88
-# 5    Frank   30    NY     57
+#       name   age state  point
+# 0    Alice  24.0    NY   64.0
+# 1      NaN   NaN   NaN    NaN
+# 2  Charlie  18.0    CA   70.0
+# 3     Dave  68.0    TX   70.0
+# 4    Ellen  24.0    CA   88.0
+# 5    Frank  30.0    NY   57.0
 
 u = df['state'].unique()
 print(u)
 print(type(u))
-# ['NY' 'CA' 'TX']
+# ['NY' nan 'CA' 'TX']
 # <class 'numpy.ndarray'>
 
 vc = df['state'].value_counts()
 print(vc)
 print(type(vc))
-# CA    3
 # NY    2
+# CA    2
 # TX    1
 # Name: state, dtype: int64
 # <class 'pandas.core.series.Series'>
 
-vc_f = df['state'].value_counts(sort=False)
-print(vc_f)
-# CA    3
+print(df['state'].value_counts(ascending=True))
 # TX    1
+# CA    2
 # NY    2
 # Name: state, dtype: int64
 
-print(len(df['state'].unique()))
+print(df['state'].value_counts(sort=False))
+# TX    1
+# CA    2
+# NY    2
+# Name: state, dtype: int64
+
+print(df['state'].value_counts(dropna=False))
+# NY     2
+# CA     2
+# TX     1
+# NaN    1
+# Name: state, dtype: int64
+
+print(df['state'].value_counts(dropna=False, normalize=True))
+# NY     0.333333
+# CA     0.333333
+# TX     0.166667
+# NaN    0.166667
+# Name: state, dtype: float64
+
+nu = df['state'].nunique()
+print(nu)
+print(type(nu))
+# 3
+# <class 'int'>
+
+print(df['state'].nunique(dropna=False))
+# 4
+
+nu_col = df.nunique()
+print(nu_col)
+print(type(nu_col))
+# name     5
+# age      4
+# state    3
+# point    4
+# dtype: int64
+# <class 'pandas.core.series.Series'>
+
+print(df.nunique(dropna=False))
+# name     6
+# age      5
+# state    4
+# point    5
+# dtype: int64
+
+print(df.nunique(dropna=False, axis='columns'))
+# 0    4
+# 1    1
+# 2    4
+# 3    4
+# 4    4
+# 5    4
+# dtype: int64
+
+print(df['state'].nunique())
 # 3
 
-print(len(df['state'].value_counts()))
-# 3
-
-print(df['state'].value_counts().count())
-# 3
+print(df.nunique())
+# name     5
+# age      4
+# state    3
+# point    4
+# dtype: int64
 
 print(df['state'].unique().tolist())
 print(type(df['state'].unique().tolist()))
-# ['NY', 'CA', 'TX']
+# ['NY', nan, 'CA', 'TX']
 # <class 'list'>
 
 print(df['state'].value_counts().index.tolist())
 print(type(df['state'].value_counts().index.tolist()))
-# ['CA', 'NY', 'TX']
+# ['NY', 'CA', 'TX']
 # <class 'list'>
 
 print(df['state'].value_counts().index.values)
 print(type(df['state'].value_counts().index.values))
-# ['CA' 'NY' 'TX']
+# ['NY' 'CA' 'TX']
 # <class 'numpy.ndarray'>
 
 print(df['state'].value_counts()['NY'])
@@ -64,14 +121,14 @@ print(df['state'].value_counts().NY)
 
 for index, value in df['state'].value_counts().iteritems():
     print(index, ': ', value)
-# CA :  3
 # NY :  2
+# CA :  2
 # TX :  1
 
 d = df['state'].value_counts().to_dict()
 print(d)
 print(type(d))
-# {'CA': 3, 'NY': 2, 'TX': 1}
+# {'NY': 2, 'CA': 2, 'TX': 1}
 # <class 'dict'>
 
 print(d['NY'])
@@ -79,13 +136,6 @@ print(d['NY'])
 
 for key, value in d.items():
     print(key, ': ', value)
-# CA :  3
 # NY :  2
+# CA :  2
 # TX :  1
-
-vc_norm = df['state'].value_counts(normalize=True)
-print(vc_norm)
-# CA    0.500000
-# NY    0.333333
-# TX    0.166667
-# Name: state, dtype: float64
