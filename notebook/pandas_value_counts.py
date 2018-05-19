861b0704a0ae10f3e131695f2bd6612b88b491b7
nkmk <nkmk.on@gmail.com>
2018-05-19 23:48:49 +0900

    update pandas value_counts

--- notebook/pandas_value_counts.py
+++ notebook/pandas_value_counts.py
@@ -34,9 +34,9 @@ print(df['state'].value_counts(ascending=True))
 # Name: state, dtype: int64
 
 print(df['state'].value_counts(sort=False))
-# TX    1
 # CA    2
 # NY    2
+# TX    1
 # Name: state, dtype: int64
 
 print(df['state'].value_counts(dropna=False))
@@ -108,9 +108,9 @@ print(type(df['state'].value_counts().index.tolist()))
 # ['NY', 'CA', 'TX']
 # <class 'list'>
 
-print(df['state'].value_counts().index.values)
+print(df['state'].value_counts(dropna=False).index.values)
 print(type(df['state'].value_counts().index.values))
-# ['NY' 'CA' 'TX']
+# ['NY' 'CA' 'TX' nan]
 # <class 'numpy.ndarray'>
 
 print(df['state'].value_counts()['NY'])
@@ -139,3 +139,86 @@ for key, value in d.items():
 # NY :  2
 # CA :  2
 # TX :  1
+
+print(df['state'].value_counts())
+# NY    2
+# CA    2
+# TX    1
+# Name: state, dtype: int64
+
+print(df['state'].value_counts().index[0])
+# NY
+
+print(df['state'].value_counts().iat[0])
+# 2
+
+print(df.apply(lambda x: x.value_counts().index[0]))
+# name     Frank
+# age         24
+# state       NY
+# point       70
+# dtype: object
+
+print(df.apply(lambda x: x.value_counts().iat[0]))
+# name     1
+# age      2
+# state    2
+# point    2
+# dtype: int64
+
+print(df['state'].mode())
+# 0    CA
+# 1    NY
+# dtype: object
+
+print(df['state'].mode().tolist())
+# ['CA', 'NY']
+
+print(df['age'].mode().tolist())
+# [24.0]
+
+s_mode = df.apply(lambda x: x.mode().tolist())
+print(s_mode)
+# name     [Alice, Charlie, Dave, Ellen, Frank]
+# age                                    [24.0]
+# state                                [CA, NY]
+# point                                  [70.0]
+# dtype: object
+
+print(type(s_mode))
+# <class 'pandas.core.series.Series'>
+
+print(s_mode['name'])
+# ['Alice', 'Charlie', 'Dave', 'Ellen', 'Frank']
+
+print(type(s_mode['name']))
+# <class 'list'>
+
+print(df.mode())
+#       name   age state  point
+# 0    Alice  24.0    CA   70.0
+# 1  Charlie   NaN    NY    NaN
+# 2     Dave   NaN   NaN    NaN
+# 3    Ellen   NaN   NaN    NaN
+# 4    Frank   NaN   NaN    NaN
+
+print(df.mode().count())
+# name     5
+# age      1
+# state    2
+# point    1
+# dtype: int64
+
+print(df.astype('str').describe())
+#          name   age state point
+# count       6     6     6     6
+# unique      6     5     4     5
+# top     Frank  24.0    CA  70.0
+# freq        1     2     2     2
+
+print(df.astype('str').describe().loc['top'])
+# name     Frank
+# age       24.0
+# state       CA
+# point     70.0
+# Name: top, dtype: object
