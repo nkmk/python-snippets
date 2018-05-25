abb49547242724e38b08005dcbf45fbef9b02de7
nkmk <nkmk.on@gmail.com>
2018-05-25 22:15:12 +0900

    update pandas multiindex indexing

--- notebook/pandas_multiindex_indexing.py
+++ notebook/pandas_multiindex_indexing.py
@@ -21,25 +21,10 @@ print(df)
 #         B3      C2         100     77
 #                 C3          22     50
 
-print(df.sum(level='level_2'))
-#          val_1  val_2
-# level_2              
-# B0         236    125
-# B1         196    180
-# B2         146    299
-# B3         185    138
-
-print(df.sum(level=['level_2', 'level_3']))
-#                  val_1  val_2
-# level_2 level_3              
-# B0      C0         173    112
-#         C1          63     13
-# B1      C2          64     69
-#         C3         132    111
-# B2      C0          65    143
-#         C1          81    156
-# B3      C2         147     83
-#         C3          38     55
+print(df.index)
+# MultiIndex(levels=[['A0', 'A1', 'A2', 'A3'], ['B0', 'B1', 'B2', 'B3'], ['C0', 'C1', 'C2', 'C3']],
+#            labels=[[0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3], [0, 0, 1, 1, 2, 2, 3, 3, 0, 0, 1, 1, 2, 2, 3, 3], [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]],
+#            names=['level_1', 'level_2', 'level_3'])
 
 print(df.loc['A0', 'val_1'])
 # level_2  level_3
@@ -104,7 +89,20 @@ print(df.loc[('A0', 'B1', 'C2'), :])
 # val_2    17
 # Name: (A0, B1, C2), dtype: int64
 
-# print(df.loc[(:, 'B1'), :]) => SyntaxError
+print(df.loc[(['A0', 'A1'], ['B0', 'B3']), :])
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A0      B0      C0          98     90
+#                 C1          44      9
+# A1      B3      C2          47      6
+#                 C3          16      5
+
+# print(df.loc[(:, 'B1'), :])
+# SyntaxError: invalid syntax
+
+# print(df.loc[('A1':'A3', 'B2'), :])
+# SyntaxError: invalid syntax
+
 print(df.loc[(slice(None), 'B1'), :])
 #                          val_1  val_2
 # level_1 level_2 level_3              
@@ -112,6 +110,21 @@ print(df.loc[(slice(None), 'B1'), :])
 #                 C3          75     71
 # A2      B1      C2          25     52
 #                 C3          57     40
+
+print(df.loc[(slice('A1', 'A3'), 'B2'), :])
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A1      B2      C0           1     89
+#                 C1          54     60
+# A3      B2      C0          64     54
+#                 C1          27     96
+
+print(df.loc[(slice('A1', 'A3'), ['B0', 'B2'], 'C1'), :])
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A1      B2      C1          54     60
+# A2      B0      C1          19      4
+# A3      B2      C1          27     96
 
 print(df.loc[pd.IndexSlice[:, 'B1'], :])
 #                          val_1  val_2
@@ -121,6 +134,21 @@ print(df.loc[pd.IndexSlice[:, 'B1'], :])
 # A2      B1      C2          25     52
 #                 C3          57     40
 
+print(df.loc[pd.IndexSlice['A1':'A3', 'B2'], :])
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A1      B2      C0           1     89
+#                 C1          54     60
+# A3      B2      C0          64     54
+#                 C1          27     96
+
+print(df.loc[pd.IndexSlice['A1':'A3', ['B0', 'B2'], 'C1'], :])
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A1      B2      C1          54     60
+# A2      B0      C1          19      4
+# A3      B2      C1          27     96
+
 print(df.xs('B1', level='level_2'))
 #                  val_1  val_2
 # level_1 level_3              
@@ -129,13 +157,55 @@ print(df.xs('B1', level='level_2'))
 # A2      C2          25     52
 #         C3          57     40
 
+print(df.xs('C1', level=2))
+#                  val_1  val_2
+# level_1 level_2              
+# A0      B0          44      9
+# A1      B2          54     60
+# A2      B0          19      4
+# A3      B2          27     96
+
 print(df.xs(['B1', 'C2'], level=['level_2', 'level_3']))
 #          val_1  val_2
 # level_1              
 # A0          39     17
 # A2          25     52
 
-# print(df.xs(['B1', 'B2'], level='level_2')) => KeyError
+print(df.xs(pd.IndexSlice['A1':'A3'], level='level_1'))
+#                  val_1  val_2
+# level_2 level_3              
+# B2      C0           1     89
+#         C1          54     60
+# B3      C2          47      6
+#         C3          16      5
+# B0      C0          75     22
+#         C1          19      4
+# B1      C2          25     52
+#         C3          57     40
+# B2      C0          64     54
+#         C1          27     96
+# B3      C2         100     77
+#         C3          22     50
+
+print(df.xs(slice('A1', 'A3'), level='level_1'))
+#                  val_1  val_2
+# level_2 level_3              
+# B2      C0           1     89
+#         C1          54     60
+# B3      C2          47      6
+#         C3          16      5
+# B0      C0          75     22
+#         C1          19      4
+# B1      C2          25     52
+#         C3          57     40
+# B2      C0          64     54
+#         C1          27     96
+# B3      C2         100     77
+#         C3          22     50
+
+# print(df.xs(['B1', 'B2'], level='level_2'))
+# KeyError: ('B1', 'B2')
+
 print(df.loc[pd.IndexSlice[:, ['B1', 'B2']], :])
 #                          val_1  val_2
 # level_1 level_2 level_3              
@@ -147,3 +217,72 @@ print(df.loc[pd.IndexSlice[:, ['B1', 'B2']], :])
 #                 C3          57     40
 # A3      B2      C0          64     54
 #                 C1          27     96
+
+df.loc[(['A0', 'A1'], ['B0', 'B3']), :] = -100
+
+print(df)
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A0      B0      C0        -100   -100
+#                 C1        -100   -100
+#         B1      C2          39     17
+#                 C3          75     71
+# A1      B2      C0           1     89
+#                 C1          54     60
+#         B3      C2        -100   -100
+#                 C3        -100   -100
+# A2      B0      C0          75     22
+#                 C1          19      4
+#         B1      C2          25     52
+#                 C3          57     40
+# A3      B2      C0          64     54
+#                 C1          27     96
+#         B3      C2         100     77
+#                 C3          22     50
+
+df.loc[(['A0', 'A1'], ['B0', 'B3']), :] = [-200, -300]
+
+print(df)
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A0      B0      C0        -200   -300
+#                 C1        -200   -300
+#         B1      C2          39     17
+#                 C3          75     71
+# A1      B2      C0           1     89
+#                 C1          54     60
+#         B3      C2        -200   -300
+#                 C3        -200   -300
+# A2      B0      C0          75     22
+#                 C1          19      4
+#         B1      C2          25     52
+#                 C3          57     40
+# A3      B2      C0          64     54
+#                 C1          27     96
+#         B3      C2         100     77
+#                 C3          22     50
+
+df.loc[(['A0', 'A1'], ['B0', 'B3']), :] = [[-1, -2], [-3, -4], [-5, -6], [-7, -8]]
+
+print(df)
+#                          val_1  val_2
+# level_1 level_2 level_3              
+# A0      B0      C0          -1     -2
+#                 C1          -3     -4
+#         B1      C2          39     17
+#                 C3          75     71
+# A1      B2      C0           1     89
+#                 C1          54     60
+#         B3      C2          -5     -6
+#                 C3          -7     -8
+# A2      B0      C0          75     22
+#                 C1          19      4
+#         B1      C2          25     52
+#                 C3          57     40
+# A3      B2      C0          64     54
+#                 C1          27     96
+#         B3      C2         100     77
+#                 C3          22     50
+
+# df.xs(['B1', 'C2'], level=['level_2', 'level_3']) = 0
+# SyntaxError: can't assign to function call
