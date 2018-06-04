7c78f70bfa2929e17ae52e39b26a755963e61696
nkmk <nkmk.on@gmail.com>
2018-06-04 22:35:00 +0900

    update random shuffle

--- notebook/random_shuffle.py
+++ notebook/random_shuffle.py
@@ -6,7 +6,7 @@ print(l)
 
 random.shuffle(l)
 print(l)
-# [0, 3, 1, 4, 2]
+# [4, 3, 2, 1, 0]
 
 l = list(range(5))
 print(l)
@@ -14,18 +14,30 @@ print(l)
 
 lr = random.sample(l, len(l))
 print(lr)
-# [4, 2, 3, 1, 0]
+# [3, 2, 4, 1, 0]
 
 print(l)
 # [0, 1, 2, 3, 4]
+
+s = 'abcde'
+
+# random.shuffle(s)
+# TypeError: 'str' object does not support item assignment
 
 t = tuple(range(5))
 print(t)
 # (0, 1, 2, 3, 4)
 
+# random.shuffle(t)
+# TypeError: 'tuple' object does not support item assignment
+
+sr = ''.join(random.sample(s, len(s)))
+print(sr)
+# caebd
+
 tr = tuple(random.sample(t, len(l)))
 print(tr)
-# (0, 4, 3, 1, 2)
+# (2, 1, 4, 3, 0)
 
 random.seed(0)
 l = list(range(5))
