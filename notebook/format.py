e81db2b3c05370582fdf7d5f636fc8ede4908b11
nkmk <nkmk.on@gmail.com>
2018-06-11 23:55:22 +0900

    update format

--- notebook/format.py
+++ notebook/format.py
@@ -25,19 +25,32 @@ print('{0}-{1}-{0}'.format('foo', 'bar'))
 print('{year}年{month}月{day}日'.format(year=2018, month=1, day=11))
 # 2018年1月11日
 
-person1 = {'name': 'Alice', 'age': 20}
-person2 = {'name': 'Bob', 'age': 30}
-print('{0[name]} is {0[age]} years old.{1[name]} is {1[age]} years old.'.format(person1, person2))
-# Alice is 20 years old.Bob is 30 years old.
+l = ['one', 'two', 'three']
+print('{0[0]}-{0[1]}-{0[2]}'.format(l))
+# one-two-three
+
+d1 = {'name': 'Alice', 'age': 20}
+d2 = {'name': 'Bob', 'age': 30}
+print('{0[name]} is {0[age]} years old.\n{1[name]} is {1[age]} years old.'.format(d1, d2))
+# Alice is 20 years old.
+# Bob is 30 years old.
+
+l = ['one', 'two', 'three']
+print('{}-{}-{}'.format(*l))
+# one-two-three
+
+d = {'name': 'Alice', 'age': 20}
+print('{name} is {age} years old.'.format(**d))
+# Alice is 20 years old.
+
+print('{{}}-{num}-{{{num}}}'.format(num=100))
+# {}-100-{100}
 
 print('{num:x}'.format(num=255))
 # ff
 
 print('{year}年{month:02}月{day:02}日'.format(year=2018, month=1, day=11))
 # 2018年01月11日
-
-print('{{}}-{num}-{{{num}}}'.format(num=100))
-# {}-100-{100}
 
 print('left  : {:<10}'.format(100))
 print('center: {:^10}'.format(100))
@@ -53,10 +66,31 @@ print('right : {:鬼>10}'.format(100))
 # center: aaa100aaaa
 # right : 鬼鬼鬼鬼鬼鬼鬼100
 
-print('zero padding: {:0>10}'.format(100))
+print('sign: {:0>10}'.format(-100))
+print('sign: {:0=10}'.format(-100))
+print('sign: {:0=+10}'.format(100))
+# sign: 000000-100
+# sign: -000000100
+# sign: +000000100
+
+# print('sign: {:0=10}'.format('-100'))
+# ValueError: '=' alignment not allowed in string format specifier
+
+print('sign: {:0=10}'.format(int('-100')))
+# sign: -000000100
+
+print('zero padding: {:0=10}'.format(100))
 print('zero padding: {:010}'.format(100))
 # zero padding: 0000000100
 # zero padding: 0000000100
+
+print('zero padding: {:0=10}'.format(-100))
+print('zero padding: {:010}'.format(-100))
+# zero padding: -000000100
+# zero padding: -000000100
+
+# print('zero padding: {:010}'.format('-100'))
+# ValueError: '=' alignment not allowed in string format specifier
 
 print('sign: {}'.format(100))
 print('sign: {}'.format(-100))
@@ -87,6 +121,21 @@ print('sign: {: 06}'.format(100))
 print('sign: {: 06}'.format(-100))
 # sign:  00100
 # sign: -00100
+
+print('sign: {:_>6}'.format(100))
+print('sign: {:_>6}'.format(-100))
+# sign: ___100
+# sign: __-100
+
+print('sign: {:_>+6}'.format(100))
+print('sign: {:_>+6}'.format(-100))
+# sign: __+100
+# sign: __-100
+
+print('sign: {:_> 6}'.format(100))
+print('sign: {:_> 6}'.format(-100))
+# sign: __ 100
+# sign: __-100
 
 print('{:,}'.format(100000000))
 # 100,000,000
@@ -140,12 +189,19 @@ print('hex: {:#011_x}'.format(255))
 print('hex: {:08x}'.format(int('255')))
 # hex: 000000ff
 
-print('{:.2}'.format(1.234))
-print('{:.5}'.format(1.234))
-print('{:.5f}'.format(1.234))
-# 1.2
-# 1.234
-# 1.23400
+print('{:.2f}'.format(123.456))
+print('{:.5f}'.format(123.456))
+print('{:.3f}'.format(0.0001234))
+# 123.46
+# 123.45600
+# 0.000
+
+print('{:.0f}'.format(0.4))
+print('{:.0f}'.format(0.5))
+print('{:.0f}'.format(0.6))
+# 0
+# 0
+# 1
 
 print('{}'.format(0.0001234))
 print('{}'.format(0.00001234))
@@ -164,10 +220,58 @@ print('{:E}'.format(0.0001234))
 # 1.234000e-04
 # 1.234000E-04
 
-print('{:.4e}'.format(0.0001234))
+print('{:.5e}'.format(0.0001234))
 print('{:.2E}'.format(0.0001234))
-# 1.2340e-04
+# 1.23400e-04
 # 1.23E-04
+
+print('{:.5e}'.format(987.65))
+print('{:.2E}'.format(987.65))
+# 9.87650e+02
+# 9.88E+02
+
+print('{:.2g}'.format(123.456))
+print('{:.3g}'.format(123.456))
+print('{:.8g}'.format(123.456))
+print('{:.3g}'.format(0.0001234))
+# 1.2e+02
+# 123
+# 123.456
+# 0.000123
+
+print('{:.2}'.format(123.456))
+print('{:.3}'.format(123.456))
+print('{:.8}'.format(123.456))
+print('{:.3}'.format(0.0001234))
+# 1.2e+02
+# 1.23e+02
+# 123.456
+# 0.000123
+
+print('{:.3f}'.format(123.456))
+print('{:.3e}'.format(123.456))
+print('{:.3g}'.format(123.456))
+print('{:.3}'.format(123.456))
+# 123.456
+# 1.235e+02
+# 123
+# 1.23e+02
+
+print('{:.8f}'.format(123.456))
+print('{:.8e}'.format(123.456))
+print('{:.8g}'.format(123.456))
+print('{:.8}'.format(123.456))
+# 123.45600000
+# 1.23456000e+02
+# 123.456
+# 123.456
+
+print('{:.4e}'.format(123.456))
+print('{:.4e}'.format(0.000012345))
+print('{:.4e}'.format(12))
+# 1.2346e+02
+# 1.2345e-05
+# 1.2000e+01
 
 print('{:%}'.format(0.12345))
 print('{:.2%}'.format(0.12345))
