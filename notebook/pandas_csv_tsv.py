28ca40d4cb2e677eaf8cc195a6c2ffb720ff58a2
nkmk <nkmk.on@gmail.com>
2018-06-12 15:07:20 +0900

    update pandas read csv tsv

--- notebook/pandas_csv_tsv.py
+++ notebook/pandas_csv_tsv.py
@@ -271,6 +271,62 @@ print(df_str_col_num.dtypes)
 # d    object
 # dtype: object
 
+df_nan = pd.read_csv('data/src/sample_header_index_nan.csv', index_col=0)
+print(df_nan)
+#          a   b
+# ONE    NaN NaN
+# TWO      - NaN
+# THREE  NaN NaN
+
+print(df_nan.isnull())
+#            a     b
+# ONE     True  True
+# TWO    False  True
+# THREE   True  True
+
+df_nan_set_na = pd.read_csv('data/src/sample_header_index_nan.csv',
+                            index_col=0, na_values='-')
+print(df_nan_set_na)
+#         a   b
+# ONE   NaN NaN
+# TWO   NaN NaN
+# THREE NaN NaN
+
+print(df_nan_set_na.isnull())
+#           a     b
+# ONE    True  True
+# TWO    True  True
+# THREE  True  True
+
+df_nan_set_na_no_keep = pd.read_csv('data/src/sample_header_index_nan.csv',
+                                    index_col=0, na_values=['-', 'NaN', 'null'],
+                                    keep_default_na=False)
+print(df_nan_set_na_no_keep)
+#          a    b
+# ONE         NaN
+# TWO    NaN  nan
+# THREE  NaN  N/A
+
+print(df_nan_set_na_no_keep.isnull())
+#            a      b
+# ONE    False   True
+# TWO     True  False
+# THREE   True  False
+
+df_nan_no_filter = pd.read_csv('data/src/sample_header_index_nan.csv',
+                               index_col=0, na_filter=False)
+print(df_nan_no_filter)
+#           a    b
+# ONE          NaN
+# TWO       -  nan
+# THREE  null  N/A
+
+print(df_nan_no_filter.isnull())
+#            a      b
+# ONE    False  False
+# TWO    False  False
+# THREE  False  False
+
 df_tsv = pd.read_table('data/src/sample_header_index.tsv', index_col=0)
 print(df_tsv)
 #         a   b   c   d
