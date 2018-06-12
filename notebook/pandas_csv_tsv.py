299591f5be07db7e3a3c14df18c05160af5915a9
nkmk <nkmk.on@gmail.com>
2018-06-12 13:39:45 +0900

    add pandas read csv tsv

--- notebook/pandas_csv_tsv.py
+++ notebook/pandas_csv_tsv.py
@@ -83,21 +83,24 @@ print(df_header_usecols)
 # 1  21  23
 # 2  31  33
 
-df_header_usecols = pd.read_csv('data/src/sample_header.csv', usecols=lambda x:x is not 'b')
+df_header_usecols = pd.read_csv('data/src/sample_header.csv',
+                                usecols=lambda x: x is not 'b')
 print(df_header_usecols)
 #     a   c   d
 # 0  11  13  14
 # 1  21  23  24
 # 2  31  33  34
 
-df_header_usecols = pd.read_csv('data/src/sample_header.csv', usecols=lambda x:x not in ['a', 'c'])
+df_header_usecols = pd.read_csv('data/src/sample_header.csv',
+                                usecols=lambda x: x not in ['a', 'c'])
 print(df_header_usecols)
 #     b   d
 # 0  12  14
 # 1  22  24
 # 2  32  34
 
-df_index_usecols = pd.read_csv('data/src/sample_header_index.csv', index_col=0, usecols=[0, 1, 3])
+df_index_usecols = pd.read_csv('data/src/sample_header_index.csv',
+                               index_col=0, usecols=[0, 1, 3])
 print(df_index_usecols)
 #         a   c
 # ONE    11  13
@@ -127,7 +130,8 @@ print(df_none_skiprows)
 # 0  11  12  13  14
 # 1  31  32  33  34
 
-df_none_skiprows = pd.read_csv('data/src/sample.csv', header=None, skiprows=lambda x:x not in [0, 2])
+df_none_skiprows = pd.read_csv('data/src/sample.csv', header=None,
+                               skiprows=lambda x: x not in [0, 2])
 print(df_none_skiprows)
 #     0   1   2   3
 # 0  11  12  13  14
@@ -144,7 +148,8 @@ print(df_header_skiprows)
 #    11  12  13  14
 # 0  21  22  23  24
 
-df_none_skipfooter = pd.read_csv('data/src/sample.csv', header=None, skipfooter=1, engine='python')
+df_none_skipfooter = pd.read_csv('data/src/sample.csv', header=None,
+                                 skipfooter=1, engine='python')
 print(df_none_skipfooter)
 #     0   1   2   3
 # 0  11  12  13  14
@@ -158,25 +163,31 @@ print(df_none_nrows)
 
 df_default = pd.read_csv('data/src/sample_header_index_dtype.csv', index_col=0)
 print(df_default)
-#        a    b    c  d
-# ONE    1    1  100  x
-# TWO    2   20   20  y
-# THREE  3  300    3  z
+#        a    b      c  d
+# ONE    1    1  100.0  x
+# TWO    2   20    NaN  y
+# THREE  3  300  300.0  z
 
 print(df_default.dtypes)
-# a     int64
-# b     int64
-# c     int64
-# d    object
+# a      int64
+# b      int64
+# c    float64
+# d     object
 # dtype: object
+
+print(df_default.applymap(type))
+#                    a              b                c              d
+# ONE    <class 'int'>  <class 'int'>  <class 'float'>  <class 'str'>
+# TWO    <class 'int'>  <class 'int'>  <class 'float'>  <class 'str'>
+# THREE  <class 'int'>  <class 'int'>  <class 'float'>  <class 'str'>
 
 df_str = pd.read_csv('data/src/sample_header_index_dtype.csv',
                      index_col=0, dtype=str)
 print(df_str)
 #        a    b    c  d
 # ONE    1  001  100  x
-# TWO    2  020  020  y
-# THREE  3  300  003  z
+# TWO    2  020  NaN  y
+# THREE  3  300  300  z
 
 print(df_str.dtypes)
 # a    object
@@ -184,6 +195,33 @@ print(df_str.dtypes)
 # c    object
 # d    object
 # dtype: object
+
+print(df_str.applymap(type))
+#                    a              b                c              d
+# ONE    <class 'str'>  <class 'str'>    <class 'str'>  <class 'str'>
+# TWO    <class 'str'>  <class 'str'>  <class 'float'>  <class 'str'>
+# THREE  <class 'str'>  <class 'str'>    <class 'str'>  <class 'str'>
+
+df_object = pd.read_csv('data/src/sample_header_index_dtype.csv',
+                        index_col=0, dtype=object)
+print(df_object)
+#        a    b    c  d
+# ONE    1  001  100  x
+# TWO    2  020  NaN  y
+# THREE  3  300  300  z
+
+print(df_object.dtypes)
+# a    object
+# b    object
+# c    object
+# d    object
+# dtype: object
+
+print(df_object.applymap(type))
+#                    a              b                c              d
+# ONE    <class 'str'>  <class 'str'>    <class 'str'>  <class 'str'>
+# TWO    <class 'str'>  <class 'str'>  <class 'float'>  <class 'str'>
+# THREE  <class 'str'>  <class 'str'>    <class 'str'>  <class 'str'>
 
 # df_int = pd.read_csv('data/src/sample_header_index_dtype.csv',
 #                      index_col=0, dtype=int)
@@ -193,8 +231,8 @@ df_str_cast = df_str.astype({'a': int})
 print(df_str_cast)
 #        a    b    c  d
 # ONE    1  001  100  x
-# TWO    2  020  020  y
-# THREE  3  300  003  z
+# TWO    2  020  NaN  y
+# THREE  3  300  300  z
 
 print(df_str_cast.dtypes)
 # a     int64
@@ -208,8 +246,8 @@ df_str_col = pd.read_csv('data/src/sample_header_index_dtype.csv',
 print(df_str_col)
 #        a    b    c  d
 # ONE    1  001  100  x
-# TWO    2  020  020  y
-# THREE  3  300  003  z
+# TWO    2  020  NaN  y
+# THREE  3  300  300  z
 
 print(df_str_col.dtypes)
 # a     int64
@@ -223,8 +261,8 @@ df_str_col_num = pd.read_csv('data/src/sample_header_index_dtype.csv',
 print(df_str_col_num)
 #        a    b    c  d
 # ONE    1  001  100  x
-# TWO    2  020  020  y
-# THREE  3  300  003  z
+# TWO    2  020  NaN  y
+# THREE  3  300  300  z
 
 print(df_str_col_num.dtypes)
 # a     int64
