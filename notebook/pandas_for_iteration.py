2286c9c2d547dce9eebcda8bc0abe4a588aafd65
nkmk <nkmk.on@gmail.com>
2018-07-08 09:54:55 +0900

    update pandas for

--- notebook/pandas_for_iteration.py
+++ notebook/pandas_for_iteration.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=0).head(2)
+df = pd.DataFrame({'age': [24, 42], 'state': ['NY', 'CA'], 'point': [64, 92]},
+                  index=['Alice', 'Bob'])
+
 print(df)
 #        age state  point
-# name                   
 # Alice   24    NY     64
 # Bob     42    CA     92
 
@@ -14,16 +15,38 @@ for column_name in df:
 # <class 'str'>
 # age
 # ======
+# 
 # <class 'str'>
 # state
 # ======
+# 
 # <class 'str'>
 # point
 # ======
+# 
+
+for column_name in df.__iter__():
+    print(type(column_name))
+    print(column_name)
+    print('======\n')
+# <class 'str'>
+# age
+# ======
+# 
+# <class 'str'>
+# state
+# ======
+# 
+# <class 'str'>
+# point
+# ======
+# 
 
 for column_name, item in df.iteritems():
     print(type(column_name))
     print(column_name)
+    print('~~~~~~')
+    
     print(type(item))
     print(item)
     print('------')
@@ -34,8 +57,8 @@ for column_name, item in df.iteritems():
     print('======\n')
 # <class 'str'>
 # age
+# ~~~~~~
 # <class 'pandas.core.series.Series'>
-# name
 # Alice    24
 # Bob      42
 # Name: age, dtype: int64
@@ -44,10 +67,11 @@ for column_name, item in df.iteritems():
 # 24
 # 24
 # ======
+# 
 # <class 'str'>
 # state
+# ~~~~~~
 # <class 'pandas.core.series.Series'>
-# name
 # Alice    NY
 # Bob      CA
 # Name: state, dtype: object
@@ -56,10 +80,11 @@ for column_name, item in df.iteritems():
 # NY
 # NY
 # ======
+# 
 # <class 'str'>
 # point
+# ~~~~~~
 # <class 'pandas.core.series.Series'>
-# name
 # Alice    64
 # Bob      92
 # Name: point, dtype: int64
@@ -68,10 +93,13 @@ for column_name, item in df.iteritems():
 # 64
 # 64
 # ======
+# 
 
 for index, row in df.iterrows():
     print(type(index))
     print(index)
+    print('~~~~~~')
+    
     print(type(row))
     print(row)
     print('------')
@@ -82,6 +110,7 @@ for index, row in df.iterrows():
     print('======\n')
 # <class 'str'>
 # Alice
+# ~~~~~~
 # <class 'pandas.core.series.Series'>
 # age      24
 # state    NY
@@ -92,8 +121,10 @@ for index, row in df.iterrows():
 # 64
 # 64
 # ======
+# 
 # <class 'str'>
 # Bob
+# ~~~~~~
 # <class 'pandas.core.series.Series'>
 # age      42
 # state    CA
@@ -104,6 +135,7 @@ for index, row in df.iterrows():
 # 92
 # 92
 # ======
+# 
 
 for row in df.itertuples():
     print(type(row))
@@ -119,12 +151,14 @@ for row in df.itertuples():
 # 64
 # 64
 # ======
+# 
 # <class 'pandas.core.frame.Pandas'>
 # Pandas(Index='Bob', age=42, state='CA', point=92)
 # ------
 # 92
 # 92
 # ======
+# 
 
 for row in df.itertuples(name=None):
     print(type(row))
@@ -138,34 +172,70 @@ for row in df.itertuples(name=None):
 # ------
 # 64
 # ======
+# 
 # <class 'tuple'>
 # ('Bob', 42, 'CA', 92)
 # ------
 # 92
 # ======
+# 
+
+print(df['age'])
+# Alice    24
+# Bob      42
+# Name: age, dtype: int64
+
+print(type(df['age']))
+# <class 'pandas.core.series.Series'>
+
+for age in df['age']:
+    print(age)
+# 24
+# 42
+
+for age, point in zip(df['age'], df['point']):
+    print(age)
+    print(point)
+    print('======\n')
+# 24
+# 64
+# ======
+# 
+# 42
+# 92
+# ======
+# 
+
+print(df.index)
+# Index(['Alice', 'Bob'], dtype='object')
+
+print(type(df.index))
+# <class 'pandas.core.indexes.base.Index'>
+
+for index in df.index:
+    print(index)
+# Alice
+# Bob
 
 for index, row in df.iterrows():
-    row.point /= 2
+    row.point += 5
 
 print(df)
 #        age state  point
-# name                   
 # Alice   24    NY     64
 # Bob     42    CA     92
 
 for index, row in df.iterrows():
-    df.at[index, 'point'] /= 2
+    df.at[index, 'point'] += 5
 
 print(df)
 #        age state  point
-# name                   
-# Alice   24    NY     32
-# Bob     42    CA     46
+# Alice   24    NY     69
+# Bob     42    CA     97
 
-df['point'] /= 2
+df['point'] += 5
 
 print(df)
 #        age state  point
-# name                   
-# Alice   24    NY   16.0
-# Bob     42    CA   23.0
+# Alice   24    NY     74
+# Bob     42    CA    102
