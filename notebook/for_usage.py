3407a8864725ca31c330a966c56cda56d9d50376
nkmk <nkmk.on@gmail.com>
2018-07-08 09:54:46 +0900

    update for usage

--- notebook/for_usage.py
+++ notebook/for_usage.py
@@ -6,6 +6,8 @@ for name in l:
 # Bob
 # Charlie
 
+l = ['Alice', 'Bob', 'Charlie']
+
 for name in l:
     if name == 'Bob':
         print('!!BREAK!!')
@@ -13,6 +15,19 @@ for name in l:
     print(name)
 # Alice
 # !!BREAK!!
+
+l = ['Alice', 'Bob', 'Charlie']
+
+for name in l:
+    if name == 'Bob':
+        print('!!SKIP!!')
+        continue
+    print(name)
+# Alice
+# !!SKIP!!
+# Charlie
+
+l = ['Alice', 'Bob', 'Charlie']
 
 for name in l:
     print(name)
@@ -23,6 +38,8 @@ else:
 # Charlie
 # !!FINISH!!
 
+l = ['Alice', 'Bob', 'Charlie']
+
 for name in l:
     if name == 'Bob':
         print('!!BREAK!!')
@@ -32,3 +49,17 @@ else:
     print('!!FINISH!!')
 # Alice
 # !!BREAK!!
+
+l = ['Alice', 'Bob', 'Charlie']
+
+for name in l:
+    if name == 'Bob':
+        print('!!SKIP!!')
+        continue
+    print(name)
+else:
+    print('!!FINISH!!')
+# Alice
+# !!SKIP!!
+# Charlie
+# !!FINISH!!
