c171be1dc8f8fc4821f2c3d64ddbadd22f08a6d0
nkmk <nkmk.on@gmail.com>
2018-07-22 23:04:46 +0900

    add sys.path

---
+++ notebook/print_sys_path.py
@@ -0,0 +1,4 @@
+import sys
+import pprint
+
+pprint.pprint(sys.path)
---
+++ notebook/sys_path_check_bash.sh
@@ -0,0 +1,25 @@
+pwd
+# /Users/mbp/Documents/my-project/python-snippets/notebook
+
+python3 print_sys_path.py
+# ['/Users/mbp/Documents/my-project/python-snippets/notebook',
+#  '/Users/mbp/Documents/lib',
+#  '/Users/mbp/Documents/my-project/python-snippets/notebook',
+#  '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python37.zip',
+#  '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python3.7',
+#  '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python3.7/lib-dynload',
+#  '/usr/local/lib/python3.7/site-packages']
+
+cd ..
+
+pwd
+# /Users/mbp/Documents/my-project/python-snippets
+
+python3 notebook/print_sys_path.py
+# ['/Users/mbp/Documents/my-project/python-snippets/notebook',
+#  '/Users/mbp/Documents/lib',
+#  '/Users/mbp/Documents/my-project/python-snippets',
+#  '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python37.zip',
+#  '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python3.7',
+#  '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python3.7/lib-dynload',
+#  '/usr/local/lib/python3.7/site-packages']
