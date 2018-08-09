3b66289542bd278d1eeb9a61f7a5eb72d58267cf
nkmk <nkmk.on@gmail.com>
2018-08-09 20:40:30 +0900

    add pandas pd.np

---
+++ notebook/pandas_pd_np.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+print(pd)
+# <module 'pandas' from '/usr/local/lib/python3.7/site-packages/pandas/__init__.py'>
+
+print(pd.np)
+# <module 'numpy' from '/usr/local/lib/python3.7/site-packages/numpy/__init__.py'>
+
+import numpy as np
+
+print(pd.np is np)
+# True
+
+print(pd.np.mean is np.mean)
+# True
+
+a = pd.np.arange(12).reshape(3, 4)
+
+print(a)
+# [[ 0  1  2  3]
+#  [ 4  5  6  7]
+#  [ 8  9 10 11]]
+
+print(type(a))
+# <class 'numpy.ndarray'>
