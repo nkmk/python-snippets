d86b31b7b898666a5ea122d1185fad77ed6778d8
nkmk <nkmk.on@gmail.com>
2018-08-25 21:08:31 +0900

    add sys.argv test

---
+++ notebook/sys_argv_test.py
@@ -0,0 +1,14 @@
+import sys
+
+print('sys.argv         : ', sys.argv)
+print('type(sys.argv)   : ', type(sys.argv))
+print('len(sys.argv)    : ', len(sys.argv))
+
+print()
+
+print('sys.argv[0]      : ', sys.argv[0])
+print('sys.argv[1]      : ', sys.argv[1])
+print('sys.argv[2]      : ', sys.argv[2])
+print('type(sys.argv[0]): ', type(sys.argv[0]))
+print('type(sys.argv[1]): ', type(sys.argv[1]))
+print('type(sys.argv[2]): ', type(sys.argv[2]))
