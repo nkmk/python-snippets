04ced4a3acc2d29a41eff530ea53aba8e4ed6a25
nkmk <nkmk.on@gmail.com>
2018-09-26 22:52:12 +0900

    update reverse reversed

--- notebook/reverse_reversed.py
+++ notebook/reverse_reversed.py
@@ -1,63 +1,63 @@
-org_list = [3, 1, 4, 5, 2]
+org_list = [1, 2, 3, 4, 5]
 
 org_list.reverse()
 print(org_list)
-# [2, 5, 4, 1, 3]
+# [5, 4, 3, 2, 1]
 
 print(org_list.reverse())
 # None
 
-org_list = [3, 1, 4, 5, 2]
+org_list = [1, 2, 3, 4, 5]
 
 reverse_iterator = reversed(org_list)
 print(org_list)
-# [3, 1, 4, 5, 2]
+# [1, 2, 3, 4, 5]
 
 print(type(reverse_iterator))
 # <class 'list_reverseiterator'>
 
 for i in reversed(org_list):
     print(i)
-# 2
 # 5
 # 4
-# 1
 # 3
+# 2
+# 1
 
 new_list = list(reversed(org_list))
 print(org_list)
 print(new_list)
-# [3, 1, 4, 5, 2]
-# [2, 5, 4, 1, 3]
+# [1, 2, 3, 4, 5]
+# [5, 4, 3, 2, 1]
 
-org_list = [3, 1, 4, 5, 2]
+org_list = [1, 2, 3, 4, 5]
 
 new_list = org_list[::-1]
 print(org_list)
 print(new_list)
-# [3, 1, 4, 5, 2]
-# [2, 5, 4, 1, 3]
+# [1, 2, 3, 4, 5]
+# [5, 4, 3, 2, 1]
 
-org_str = 'cebad'
+org_str = 'abcde'
 
 new_str_list = list(reversed(org_str))
 print(new_str_list)
-# ['d', 'a', 'b', 'e', 'c']
+# ['e', 'd', 'c', 'b', 'a']
 
 new_str = ''.join(list(reversed(org_str)))
 print(new_str)
-# dabec
+# edcba
 
 new_str = org_str[::-1]
 print(new_str)
-# dabec
+# edcba
 
-org_tuple = (3, 1, 4, 5, 2)
+org_tuple = (1, 2, 3, 4, 5)
 
 new_tuple = tuple(reversed(org_tuple))
 print(new_tuple)
-# (2, 5, 4, 1, 3)
+# (5, 4, 3, 2, 1)
 
 new_tuple = org_tuple[::-1]
 print(new_tuple)
-# (2, 5, 4, 1, 3)
+# (5, 4, 3, 2, 1)
