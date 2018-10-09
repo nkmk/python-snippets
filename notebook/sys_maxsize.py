60b2df639522b816fd4d646f0a0c95a7afa18961
nkmk <nkmk.on@gmail.com>
2018-10-09 21:37:39 +0900

    add sys.maxsize

---
+++ notebook/sys_maxsize.py
@@ -0,0 +1,27 @@
+import sys
+
+print(sys.maxsize)
+# 9223372036854775807
+
+print(type(sys.maxsize))
+# <class 'int'>
+
+print(sys.maxsize == 2**63 - 1)
+# True
+
+print(hex(sys.maxsize))
+# 0x7fffffffffffffff
+
+i = 10**100
+
+print(i)
+# 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+print(i > sys.maxsize)
+# True
+
+print(float('inf'))
+# inf
+
+print(i > float('inf'))
+# False
