d0d6fc2d1312d1c9a9816c515d5a38242f1ed3bd
nkmk <nkmk.on@gmail.com>
2018-10-31 22:40:36 +0900

    add jupyter youtube vimeo

---
+++ notebook/jupyter_youtube_vimeo.py
@@ -0,0 +1,13 @@
+import IPython.display
+
+IPython.display.YouTubeVideo('6XvmhE1J9PY')
+# <IPython.lib.display.YouTubeVideo at 0x104f00630>
+
+IPython.display.YouTubeVideo('6XvmhE1J9PY', width=480, height=270)
+# <IPython.lib.display.YouTubeVideo at 0x104fd4630>
+
+IPython.display.YouTubeVideo('6XvmhE1J9PY', start=30)
+# <IPython.lib.display.YouTubeVideo at 0x104f00470>
+
+IPython.display.VimeoVideo('289502328')
+# <IPython.lib.display.VimeoVideo at 0x104f00588>
