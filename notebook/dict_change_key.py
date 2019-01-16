28f15f45b0d6bc535ff0f428cf4cc4aaf01d9966
nkmk <nkmk.on@gmail.com>
2019-01-16 20:20:21 +0900

    add dict change key

---
+++ notebook/dict_change_key.py
@@ -0,0 +1,74 @@
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+
+d['k10'] = d['k1']
+del d['k1']
+
+print(d)
+# {'k2': 2, 'k3': 3, 'k10': 1}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+
+print(d.pop('k1'))
+# 1
+
+print(d)
+# {'k2': 2, 'k3': 3}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+
+d['k10'] = d.pop('k1')
+
+print(d)
+# {'k2': 2, 'k3': 3, 'k10': 1}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+
+# print(d.pop('k10'))
+# KeyError: 'k10'
+
+print(d.pop('k10', None))
+# None
+
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+def change_dict_key(d, old_key, new_key, default_value=None):
+    d[new_key] = d.pop(old_key, default_value)
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key(d, 'k1', 'k10')
+print(d)
+# {'k2': 2, 'k3': 3, 'k10': 1}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key(d, 'k10', 'k100')
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3, 'k100': None}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key(d, 'k10', 'k100', 100)
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3, 'k100': 100}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key(d, 'k1', 'k2')
+print(d)
+# {'k2': 1, 'k3': 3}
+
+def change_dict_key_setdefault(d, old_key, new_key, default_value=None):
+    d.setdefault(new_key, d.pop(old_key, default_value))
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_setdefault(d, 'k1', 'k2')
+print(d)
+# {'k2': 2, 'k3': 3}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_setdefault(d, 'k1', 'k10')
+print(d)
+# {'k2': 2, 'k3': 3, 'k10': 1}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_setdefault(d, 'k10', 'k100')
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3, 'k100': None}
