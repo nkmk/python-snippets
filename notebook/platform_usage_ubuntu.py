6fd4fb71c10a2b21ec3794c93c7a394101b1117d
nkmk <nkmk.on@gmail.com>
2019-02-02 17:09:13 +0900

    add platform

---
+++ notebook/platform_usage_ubuntu.py
@@ -0,0 +1,16 @@
+import platform
+
+print(platform.system())
+# Linux
+
+print(platform.release())
+# 4.15.0-42-generic
+
+print(platform.version())
+# #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018
+
+print(platform.platform())
+# Linux-4.15.0-44-generic-x86_64-with-Ubuntu-18.04-bionic
+
+print(platform.linux_distribution())
+# ('Ubuntu', '18.04', 'bionic')
---
+++ notebook/platform_usage_win.py
@@ -0,0 +1,16 @@
+import platform
+
+print(platform.system())
+# Windows
+
+print(platform.release())
+# 10
+
+print(platform.version())
+# 10.0.17763
+
+print(platform.platform())
+# Windows-10-10.0.17763-SP0
+
+print(platform.win32_ver())
+# ('10', '10.0.17763', 'SP0', 'Multiprocessor Free')
