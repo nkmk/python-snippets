249150c2bbe316877905bc1d4d05cc0444792d8e
nkmk <nkmk.on@gmail.com>
2019-03-03 19:14:11 +0900

    add str upper lower

---
+++ notebook/str_upper_lower.py
@@ -0,0 +1,117 @@
+s_org = 'pYThon proGramminG laNguAge'
+
+print(s_org.upper())
+# PYTHON PROGRAMMING LANGUAGE
+
+print(s_org)
+# pYThon proGramminG laNguAge
+
+s_new = s_org.upper()
+print(s_new)
+# PYTHON PROGRAMMING LANGUAGE
+
+s_org = s_org.upper()
+print(s_org)
+# PYTHON PROGRAMMING LANGUAGE
+
+s_org = 'Pyhon Ｐｙｔｈｏｎ パイソン 123'
+
+print(s_org.upper())
+# PYHON ＰＹＴＨＯＮ パイソン 123
+
+s_org = 'pYThon proGramminG laNguAge'
+
+print(s_org.upper())
+# PYTHON PROGRAMMING LANGUAGE
+
+s_org = 'pYThon proGramminG laNguAge'
+
+print(s_org.lower())
+# python programming language
+
+s_org = 'pYThon proGramminG laNguAge'
+
+print(s_org.capitalize())
+# Python programming language
+
+s_org = 'pYThon proGramminG laNguAge'
+
+print(s_org.title())
+# Python Programming Language
+
+s_org = 'pYThon proGramminG laNguAge'
+
+print(s_org.swapcase())
+# PytHON PROgRAMMINg LAnGUaGE
+
+print('PYTHON'.isupper())
+# True
+
+print('Python'.isupper())
+# False
+
+print('ＰＹＴＨＯＮ'.isupper())
+# True
+
+print('PYTHON パイソン 123'.isupper())
+# True
+
+print('パイソン 123'.isupper())
+# False
+
+print('python'.islower())
+# True
+
+print('Python'.islower())
+# False
+
+print('ｐｙｔｈｏｎ'.islower())
+# True
+
+print('python パイソン 123'.islower())
+# True
+
+print('パイソン 123'.islower())
+# False
+
+print('Python Programming Language'.istitle())
+# True
+
+print('PYTHON Programming Language'.istitle())
+# False
+
+print('★Python Programming Language'.istitle())
+# True
+
+print('Python★ Programming Language'.istitle())
+# True
+
+print('Py★thon Programming Language'.istitle())
+# False
+
+print('The 1st Team'.istitle())
+# False
+
+print('The 1St Team'.istitle())
+# True
+
+print('パイソン 123'.istitle())
+# False
+
+s1 = 'python'
+s2 = 'PYTHON'
+
+print(s1 == s2)
+# False
+
+print(s1.upper() == s2.upper())
+# True
+
+print(s1.lower() == s2.lower())
+# True
+
+print(s1.capitalize() == s2.capitalize())
+# True
+
+print(s1.title() == s2.title())
+# True
