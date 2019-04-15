92a7d2e45854e7c0708a3334ca5727946440e73d
nkmk <nkmk.on@gmail.com>
2019-04-15 22:12:50 +0900

    update pandas read_excel

--- notebook/pandas_read_excel.py
+++ notebook/pandas_read_excel.py
@@ -38,6 +38,18 @@ print(df_sheet_multi)
 print(type(df_sheet_multi))
 # <class 'collections.OrderedDict'>
 
+print(len(df_sheet_multi))
+# 2
+
+print(df_sheet_multi[0])
+#         A   B   C
+# one    11  12  13
+# two    21  22  23
+# three  31  32  33
+
+print(type(df_sheet_multi[0]))
+# <class 'pandas.core.frame.DataFrame'>
+
 print(df_sheet_multi['sheet2'])
 #        AA  BB  CC
 # ONE    11  12  13
@@ -70,23 +82,51 @@ print(df_sheet_all['sheet1'])
 print(type(df_sheet_all['sheet1']))
 # <class 'pandas.core.frame.DataFrame'>
 
-df_header_index = pd.read_excel('data/src/sample.xlsx', header=None, index_col=1)
+print(df_sheet_all['sheet2'])
+#        AA  BB  CC
+# ONE    11  12  13
+# TWO    21  22  23
+# THREE  31  32  33
+
+print(type(df_sheet_all['sheet2']))
+# <class 'pandas.core.frame.DataFrame'>
+
+df_header_index = pd.read_excel('data/src/sample.xlsx', header=None, index_col=None)
 
 print(df_header_index)
-#         0   2   3
-# 1                
-# A     NaN   B   C
-# 11    one  12  13
-# 21    two  22  23
-# 31  three  32  33
-
-print(df_header_index.index)
-# Index(['A', 11, 21, 31], dtype='object', name=1)
+#        0   1   2   3
+# 0    NaN   A   B   C
+# 1    one  11  12  13
+# 2    two  21  22  23
+# 3  three  31  32  33
 
 print(df_header_index.columns)
-# Int64Index([0, 2, 3], dtype='int64')
+# Int64Index([0, 1, 2, 3], dtype='int64')
 
-df_use_skip = pd.read_excel('data/src/sample.xlsx', usecols=[0, 1, 3], skiprows=[1], skip_footer=1)
+print(df_header_index.index)
+# RangeIndex(start=0, stop=4, step=1)
+
+df_default = pd.read_excel('data/src/sample.xlsx', index_col=None)
+
+print(df_default)
+#         A   B   C
+# one    11  12  13
+# two    21  22  23
+# three  31  32  33
+
+print(df_default.columns)
+# Index(['A', 'B', 'C'], dtype='object')
+
+print(df_default.index)
+# Index(['one', 'two', 'three'], dtype='object')
+
+print(pd.read_excel('data/src/sample.xlsx', index_col=0))
+#         A   B   C
+# one    11  12  13
+# two    21  22  23
+# three  31  32  33
+
+df_use_skip = pd.read_excel('data/src/sample.xlsx', usecols=[0, 1, 3], skiprows=[1], skipfooter=1)
 
 print(df_use_skip)
 #       A   C
