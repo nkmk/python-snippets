a6fd5b9dac703d012eefd720d363464966532ef1
nkmk <nkmk.on@gmail.com>
2019-05-04 21:30:33 +0900

    add tuple operation

---
+++ notebook/tuple_operation.py
@@ -0,0 +1,70 @@
+t = (0, 1, 2)
+
+print(t)
+# (0, 1, 2)
+
+print(type(t))
+# <class 'tuple'>
+
+print(t[0])
+# 0
+
+print(t[:2])
+# (0, 1)
+
+# t[0] = 100
+# TypeError: 'tuple' object does not support item assignment
+
+# t.append(100)
+# AttributeError: 'tuple' object has no attribute 'append'
+
+t_add = t + (3, 4, 5)
+
+print(t_add)
+# (0, 1, 2, 3, 4, 5)
+
+print(t)
+# (0, 1, 2)
+
+# print(t + [3, 4, 5])
+# TypeError: can only concatenate tuple (not "list") to tuple
+
+t_add_one = t + (3,)
+
+print(t_add_one)
+# (0, 1, 2, 3)
+
+l = list(t)
+
+print(l)
+# [0, 1, 2]
+
+print(type(l))
+# <class 'list'>
+
+l.insert(2, 100)
+
+print(l)
+# [0, 1, 100, 2]
+
+t_insert = tuple(l)
+
+print(t_insert)
+# (0, 1, 100, 2)
+
+print(type(t_insert))
+# <class 'tuple'>
+
+l = list(t)
+l[1] = 100
+t_change = tuple(l)
+
+print(t_change)
+# (0, 100, 2)
+
+l = list(t)
+l.remove(1)
+t_remove = tuple(l)
+
+print(t_remove)
+# (0, 2)
