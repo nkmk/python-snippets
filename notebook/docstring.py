74a02a9821e3396140b9f045fa4530534a578de8
nkmk <nkmk.on@gmail.com>
2019-05-26 21:16:55 +0900

    update docstring

--- notebook/docstring.py
+++ notebook/docstring.py
@@ -10,17 +10,20 @@ print(my_func.__doc__)
 #     line1
 #     line2
 #     line3
+#     
 
 print(type(my_func.__doc__))
 # <class 'str'>
 
 help(my_func)
 # Help on function my_func in module __main__:
+# 
 # my_func()
 #     docstring-test
 #     line1
 #     line2
 #     line3
+# 
 
 def my_func2():
     'docstring-test'
@@ -51,30 +54,31 @@ print(MyClass.__doc__)
 #     line1
 #     line2
 #     line3
+#     
 
-def func_rest(arg1, arg2):
+def func_rest(param1, param2):
     """Summary line.
     
-    :param arg1: Description of arg1
-    :type arg1: int
-    :param arg2: Description of arg2
-    :type arg2: str
+    :param param1: Description of param1
+    :type param1: int
+    :param param2: Description of param2
+    :type param2: str
     :returns: Description of return value
     :rtype: bool
     """
     return True
 
-def func_numpy(arg1, arg2):
+def func_numpy(param1, param2):
     """Summary line.
 
     Extended description of function.
 
     Parameters
     ----------
-    arg1 : int
-        Description of arg1
-    arg2 : str
-        Description of arg2
+    param1 : int
+        Description of param1
+    param2 : str
+        Description of param2
 
     Returns
     -------
@@ -83,14 +87,14 @@ def func_numpy(arg1, arg2):
     """
     return True
 
-def func_google(arg1, arg2):
+def func_google(param1, param2):
     """Summary line.
 
     Extended description of function.
 
     Args:
-        arg1 (int): Description of arg1
-        arg2 (str): Description of arg2
+        param1 (int): Description of param1
+        param2 (str): Description of param2
 
     Returns:
         bool: Description of return value
