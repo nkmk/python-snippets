b67306e2d0b26f316d59e6f37748e0f7583f32b3
nkmk <nkmk.on@gmail.com>
2019-06-05 23:28:12 +0900

    update dict add update

--- notebook/dict_add_update.py
+++ notebook/dict_add_update.py
@@ -15,6 +15,17 @@ d1.update(d2)
 print(d1)
 # {'k1': 100, 'k2': 2, 'k3': 3, 'k4': 4}
 
+d1 = {'k1': 1, 'k2': 2}
+d2 = {'k3': 3, 'k4': 4}
+d3 = {'k5': 5, 'k6': 6}
+
+# d1.update(d2, d3)
+# TypeError: update expected at most 1 arguments, got 2
+
+d1.update(**d2, **d3)
+print(d1)
+# {'k1': 1, 'k2': 2, 'k3': 3, 'k4': 4, 'k5': 5, 'k6': 6}
+
 d = {'k1': 1, 'k2': 2}
 
 d.update(k1=100, k3=3, k4=4)
