ec8b79048a12534c89fb7cd661fdb86f24c4a0da
nkmk <nkmk.on@gmail.com>
2019-06-19 00:15:51 +0900

    update numpy hstack

--- notebook/numpy_hstack.py
+++ notebook/numpy_hstack.py
@@ -31,3 +31,15 @@ print(np.hstack([a1, a2]))
 
 print(np.concatenate([a1, a2], 0))
 # [1 1 1 2 2 2]
+
+a1 = np.ones((2, 3), int)
+print(a1)
+# [[1 1 1]
+#  [1 1 1]]
+
+a2 = np.full(2, 2)
+print(a2)
+# [2 2]
+
+# print(np.hstack([a1, a2]))
+# ValueError: all the input arrays must have same number of dimensions
