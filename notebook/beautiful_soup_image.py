e849df2bd213c3ff88296ba1ce1069c7ac949699
nkmk <nkmk.on@gmail.com>
2019-06-20 23:32:01 +0900

    update download image

--- notebook/beautiful_soup_image.py
+++ notebook/beautiful_soup_image.py
@@ -5,7 +5,7 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
-url = 'https://news.yahoo.co.jp/photo/'
+url = 'https://news.yahoo.co.jp/list/'
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '\
      'AppleWebKit/537.36 (KHTML, like Gecko) '\
      'Chrome/55.0.2883.95 Safari/537.36 '
@@ -15,10 +15,7 @@ html = urllib.request.urlopen(req)
 
 soup = BeautifulSoup(html, "html.parser")
 
-img_list = soup.find(class_='headline').find_all('img')
-url_list = []
-for img in img_list:
-    url_list.append(img.get('src'))
+url_list = [img.get('data-src') for img in soup.find(class_='list').find_all('img')]
 
 def download_image(url, dst_path):
     try:
@@ -28,21 +25,13 @@ def download_image(url, dst_path):
     except urllib.error.URLError as e:
         print(e)
 
+def download_image_dir(url, dst_dir):
+    download_image(url, os.path.join(dst_dir, os.path.basename(url)))
+
 download_dir = 'data/temp'
 sleep_time_sec = 1
 
 for url in url_list:
-    filename = os.path.basename(url)
-    dst_path = os.path.join(download_dir, filename)
+#     print(url)
+#     download_image_dir(url, download_dir)
     time.sleep(sleep_time_sec)
-    print(url, dst_path)
-    download_image(url, dst_path)
-# https://giwiz-newstop.c.yimg.jp/im_siggecJNKV0BleHsiAnBMXsewQ---x223-y122-n0-pril-exp30d/q/amd/20171121-00010002-esquire-000-1-view.jpg data/temp/20171121-00010002-esquire-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggric32PEeS_iH0vtb0LCUZQ---x192-y122-n0-pril-exp30d/q/amd/20171121-00010006-mensplus-000-1-view.jpg data/temp/20171121-00010006-mensplus-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggdu75GDeqZ7B_Eh0pxdKKeg---x161-y122-n0-pril-exp30d/q/amd/20171121-00000023-mnet-000-0-thumb.jpg data/temp/20171121-00000023-mnet-000-0-thumb.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggWLXlL2dWHrPr.mrrLLkL9w---x192-y108-n0-pril-exp30d/q/amd/20171121-00000006-ignjapan-000-1-view.jpg data/temp/20171121-00000006-ignjapan-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggeyzG3wHHU0NxUvrp0xPvPg---x192-y108-n0-pril-exp30d/q/amd/20171121-00010006-giz-000-1-view.jpg data/temp/20171121-00010006-giz-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggXn_6JufQa4Orbdr3IEWONA---x192-y108-n0-pril-exp30d/q/amd/20171121-00010003-biz_lifeh-000-1-view.jpg data/temp/20171121-00010003-biz_lifeh-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggSGvaG83y7pmaqZg2Bsxl4Q---x213-y120-n0-pril-exp30d/q/amd/20171121-00010021-abema-000-1-view.jpg data/temp/20171121-00010021-abema-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_sigg7rXGpJA7JHwJHGSJzWPBRA---x180-y120-n0-pril-exp30d/q/amd/20171121-00010003-binsider-000-1-view.jpg data/temp/20171121-00010003-binsider-000-1-view.jpg
-# https://giwiz-newstop.c.yimg.jp/im_siggbZPC5gZWE944mVVJ7w2xEw---x179-y119-n0-pril-exp30d/q/amd/20171121-00085892-esse-000-1-view.jpg data/temp/20171121-00085892-esse-000-1-view.jpg
--- notebook/urllib_request_image.py
+++ notebook/urllib_request_image.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 import time
 import urllib.error
 import urllib.request
@@ -11,32 +12,40 @@ def download_image(url, dst_path):
     except urllib.error.URLError as e:
         print(e)
 
-url = 'https://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png'
-dst_path = 'data/src/lena_square.png'
-# dst_dir = 'data/src'
-# dst_path = os.path.join(dst_dir, os.path.basename(url))
+url = 'https://www.python.org/static/img/python-logo.png'
+dst_path = 'data/temp/py-logo.png'
+
 download_image(url, dst_path)
 
-error_url = 'https://upload.wikimedia.org/wikipedia/en/2/24/Lenna_xxx.png'
+def download_image_dir(url, dst_dir):
+    download_image(url, os.path.join(dst_dir, os.path.basename(url)))
+
+dst_dir = 'data/temp'
+
+download_image_dir(url, dst_dir)
+
+error_url = 'https://www.python.org/static/img/python-logo_xxx.png'
 download_image(error_url, dst_path)
 # HTTP Error 404: Not Found
 
-url_list = []
-for i in range(5):
-    url = 'http://example.com/basedir/base_{:03}.jpg'.format(i)
-    url_list.append(url)
+url_list = ['https://example.com/basedir/base_{:03}.jpg'.format(i) for i in range(5)]
 
-download_dir = 'temp/dir'
+pprint.pprint(url_list)
+# ['https://example.com/basedir/base_000.jpg',
+#  'https://example.com/basedir/base_001.jpg',
+#  'https://example.com/basedir/base_002.jpg',
+#  'https://example.com/basedir/base_003.jpg',
+#  'https://example.com/basedir/base_004.jpg']
+
+download_dir = 'data/temp'
 sleep_time_sec = 1
 
 for url in url_list:
-    filename = os.path.basename(url)
-    dst_path = os.path.join(download_dir, filename)
+    print(url)
+#     download_image_dir(url, download_dir)
     time.sleep(sleep_time_sec)
-    print(url, dst_path)
-    # download_image(url, dst_path)
-# http://example.com/basedir/base_000.jpg temp/dir/base_000.jpg
-# http://example.com/basedir/base_001.jpg temp/dir/base_001.jpg
-# http://example.com/basedir/base_002.jpg temp/dir/base_002.jpg
-# http://example.com/basedir/base_003.jpg temp/dir/base_003.jpg
-# http://example.com/basedir/base_004.jpg temp/dir/base_004.jpg
+# https://example.com/basedir/base_000.jpg
+# https://example.com/basedir/base_001.jpg
+# https://example.com/basedir/base_002.jpg
+# https://example.com/basedir/base_003.jpg
+# https://example.com/basedir/base_004.jpg
