20e93110497e5195bedd58c14723c02f3d1ebe24
nkmk <nkmk.on@gmail.com>
2019-07-11 22:20:04 +0900

    update pandas drop

--- notebook/pandas_drop.py
+++ notebook/pandas_drop.py
@@ -1,66 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('data/src/sample_pandas_normal.csv')
+df = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=0)
 print(df)
-#       name  age state  point
-# 0    Alice   24    NY     64
-# 1      Bob   42    CA     92
-# 2  Charlie   18    CA     70
-# 3     Dave   68    TX     70
-# 4    Ellen   24    CA     88
-# 5    Frank   30    NY     57
-
-df_new = df.drop(2, axis=0)
-print(df_new)
-#     name  age state  point
-# 0  Alice   24    NY     64
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 4  Ellen   24    CA     88
-# 5  Frank   30    NY     57
-
-df_new = df.drop(2)
-print(df_new)
-#     name  age state  point
-# 0  Alice   24    NY     64
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 4  Ellen   24    CA     88
-# 5  Frank   30    NY     57
-
-df_new = df.drop(index=2)
-print(df_new)
-#     name  age state  point
-# 0  Alice   24    NY     64
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 4  Ellen   24    CA     88
-# 5  Frank   30    NY     57
-
-df_new = df.drop([0, 2, 4])
-print(df_new)
-#     name  age state  point
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 5  Frank   30    NY     57
-
-df_new = df.drop(index=[0, 2, 4])
-print(df_new)
-#     name  age state  point
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 5  Frank   30    NY     57
-
-df_org = df.copy()
-df_org.drop(index=[0, 2, 4], inplace=True)
-print(df_org)
-#     name  age state  point
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 5  Frank   30    NY     57
-
-df2 = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=0)
-print(df2)
 #          age state  point
 # name                     
 # Alice     24    NY     64
@@ -70,76 +11,220 @@ print(df2)
 # Ellen     24    CA     88
 # Frank     30    NY     57
 
-df_new = df2.drop(index=['Bob', 'Dave', 'Frank'])
-print(df_new)
+print(df.drop('Charlie', axis=0))
+#        age state  point
+# name                   
+# Alice   24    NY     64
+# Bob     42    CA     92
+# Dave    68    TX     70
+# Ellen   24    CA     88
+# Frank   30    NY     57
+
+print(df.drop('Charlie'))
+#        age state  point
+# name                   
+# Alice   24    NY     64
+# Bob     42    CA     92
+# Dave    68    TX     70
+# Ellen   24    CA     88
+# Frank   30    NY     57
+
+print(df.drop(index='Charlie'))
+#        age state  point
+# name                   
+# Alice   24    NY     64
+# Bob     42    CA     92
+# Dave    68    TX     70
+# Ellen   24    CA     88
+# Frank   30    NY     57
+
+print(df.drop(['Bob', 'Dave', 'Frank']))
 #          age state  point
 # name                     
 # Alice     24    NY     64
 # Charlie   18    CA     70
 # Ellen     24    CA     88
 
-df_new = df.drop('state', axis=1)
-print(df_new)
-#       name  age  point
-# 0    Alice   24     64
-# 1      Bob   42     92
-# 2  Charlie   18     70
-# 3     Dave   68     70
-# 4    Ellen   24     88
-# 5    Frank   30     57
+print(df.drop(index=['Bob', 'Dave', 'Frank']))
+#          age state  point
+# name                     
+# Alice     24    NY     64
+# Charlie   18    CA     70
+# Ellen     24    CA     88
 
-df_new = df.drop(columns='state')
-print(df_new)
-#       name  age  point
-# 0    Alice   24     64
-# 1      Bob   42     92
-# 2  Charlie   18     70
-# 3     Dave   68     70
-# 4    Ellen   24     88
-# 5    Frank   30     57
+df_org = df.copy()
+df_org.drop(index=['Bob', 'Dave', 'Frank'], inplace=True)
+print(df_org)
+#          age state  point
+# name                     
+# Alice     24    NY     64
+# Charlie   18    CA     70
+# Ellen     24    CA     88
 
-df_new = df.drop(['state', 'point'], axis=1)
-print(df_new)
-#       name  age
-# 0    Alice   24
-# 1      Bob   42
-# 2  Charlie   18
-# 3     Dave   68
-# 4    Ellen   24
-# 5    Frank   30
+print(df.index[[1, 3, 5]])
+# Index(['Bob', 'Dave', 'Frank'], dtype='object', name='name')
 
-df_new = df.drop(columns=['state', 'point'])
-print(df_new)
-#       name  age
-# 0    Alice   24
-# 1      Bob   42
-# 2  Charlie   18
-# 3     Dave   68
-# 4    Ellen   24
-# 5    Frank   30
+print(df.drop(df.index[[1, 3, 5]]))
+#          age state  point
+# name                     
+# Alice     24    NY     64
+# Charlie   18    CA     70
+# Ellen     24    CA     88
+
+print(df.drop(index=df.index[[1, 3, 5]]))
+#          age state  point
+# name                     
+# Alice     24    NY     64
+# Charlie   18    CA     70
+# Ellen     24    CA     88
+
+df_noindex = pd.read_csv('data/src/sample_pandas_normal.csv')
+print(df_noindex)
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 1      Bob   42    CA     92
+# 2  Charlie   18    CA     70
+# 3     Dave   68    TX     70
+# 4    Ellen   24    CA     88
+# 5    Frank   30    NY     57
+
+print(df_noindex.index)
+# RangeIndex(start=0, stop=6, step=1)
+
+print(df_noindex.drop([1, 3, 5]))
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 2  Charlie   18    CA     70
+# 4    Ellen   24    CA     88
+
+print(df_noindex.drop(df_noindex.index[[1, 3, 5]]))
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 2  Charlie   18    CA     70
+# 4    Ellen   24    CA     88
+
+df_noindex_sort = df_noindex.sort_values('state')
+print(df_noindex_sort)
+#       name  age state  point
+# 1      Bob   42    CA     92
+# 2  Charlie   18    CA     70
+# 4    Ellen   24    CA     88
+# 0    Alice   24    NY     64
+# 5    Frank   30    NY     57
+# 3     Dave   68    TX     70
+
+print(df_noindex_sort.index)
+# Int64Index([1, 2, 4, 0, 5, 3], dtype='int64')
+
+print(df_noindex_sort.drop([1, 3, 5]))
+#       name  age state  point
+# 2  Charlie   18    CA     70
+# 4    Ellen   24    CA     88
+# 0    Alice   24    NY     64
+
+print(df_noindex_sort.drop(df_noindex_sort.index[[1, 3, 5]]))
+#     name  age state  point
+# 1    Bob   42    CA     92
+# 4  Ellen   24    CA     88
+# 5  Frank   30    NY     57
+
+print(df.drop('state', axis=1))
+#          age  point
+# name               
+# Alice     24     64
+# Bob       42     92
+# Charlie   18     70
+# Dave      68     70
+# Ellen     24     88
+# Frank     30     57
+
+print(df.drop(columns='state'))
+#          age  point
+# name               
+# Alice     24     64
+# Bob       42     92
+# Charlie   18     70
+# Dave      68     70
+# Ellen     24     88
+# Frank     30     57
+
+print(df.drop(['state', 'point'], axis=1))
+#          age
+# name        
+# Alice     24
+# Bob       42
+# Charlie   18
+# Dave      68
+# Ellen     24
+# Frank     30
+
+print(df.drop(columns=['state', 'point']))
+#          age
+# name        
+# Alice     24
+# Bob       42
+# Charlie   18
+# Dave      68
+# Ellen     24
+# Frank     30
 
 df_org = df.copy()
 df_org.drop(columns=['state', 'point'], inplace=True)
 print(df_org)
-#       name  age
-# 0    Alice   24
-# 1      Bob   42
-# 2  Charlie   18
-# 3     Dave   68
-# 4    Ellen   24
-# 5    Frank   30
+#          age
+# name        
+# Alice     24
+# Bob       42
+# Charlie   18
+# Dave      68
+# Ellen     24
+# Frank     30
 
-df_new = df.drop(index=[0, 2, 4], columns=['state', 'point'])
-print(df_new)
-#     name  age
-# 1    Bob   42
-# 3   Dave   68
-# 5  Frank   30
+print(df.columns[[1, 2]])
+# Index(['state', 'point'], dtype='object')
+
+print(df.drop(df.columns[[1, 2]], axis=1))
+#          age
+# name        
+# Alice     24
+# Bob       42
+# Charlie   18
+# Dave      68
+# Ellen     24
+# Frank     30
+
+print(df.drop(columns=df.columns[[1, 2]]))
+#          age
+# name        
+# Alice     24
+# Bob       42
+# Charlie   18
+# Dave      68
+# Ellen     24
+# Frank     30
+
+print(df.drop(index=['Bob', 'Dave', 'Frank'],
+              columns=['state', 'point']))
+#          age
+# name        
+# Alice     24
+# Charlie   18
+# Ellen     24
+
+print(df.drop(index=df.index[[1, 3, 5]],
+              columns=df.columns[[1, 2]]))
+#          age
+# name        
+# Alice     24
+# Charlie   18
+# Ellen     24
 
 df_org = df.copy()
-df_org.drop(index=[0, 2, 4], columns=['state', 'point'], inplace=True)
+df_org.drop(index=['Bob', 'Dave', 'Frank'],
+            columns=['state', 'point'], inplace=True)
 print(df_org)
-#     name  age
-# 1    Bob   42
-# 3   Dave   68
-# 5  Frank   30
+#          age
+# name        
+# Alice     24
+# Charlie   18
+# Ellen     24
