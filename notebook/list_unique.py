32dd1d1599dae845eda703ab63b96ab6f94507ca
nkmk <nkmk.on@gmail.com>
2019-08-06 21:30:55 +0900

    update list unique

--- notebook/list_unique.py
+++ notebook/list_unique.py
@@ -1,27 +1,21 @@
 l = [3, 3, 2, 1, 5, 1, 4, 2, 3]
 
-l_unique = list(set(l))
+print(set(l))
+# {1, 2, 3, 4, 5}
 
-print(l_unique)
+print(list(set(l)))
 # [1, 2, 3, 4, 5]
 
 print(dict.fromkeys(l))
 # {3: None, 2: None, 1: None, 5: None, 4: None}
 
-l_unique_order = list(dict.fromkeys(l))
-
-print(l_unique_order)
+print(list(dict.fromkeys(l)))
 # [3, 2, 1, 5, 4]
 
-l_unique_order = sorted(set(l), key=l.index)
-
-print(l_unique_order)
+print(sorted(set(l), key=l.index))
 # [3, 2, 1, 5, 4]
 
-l_2d = [[0], [2], [2], [1], [0], [0]]
-
-print(l_2d)
-# [[0], [2], [2], [1], [0], [0]]
+l_2d = [[1, 1], [0, 1], [0, 1], [0, 0], [1, 0], [1, 1], [1, 1]]
 
 # l_2d_unique = list(set(l_2d))
 # TypeError: unhashable type: 'list'
@@ -33,37 +27,23 @@ def get_unique_list(seq):
     seen = []
     return [x for x in seq if x not in seen and not seen.append(x)]
 
-l_2d_unique = get_unique_list(l_2d)
+print(get_unique_list(l_2d))
+# [[1, 1], [0, 1], [0, 0], [1, 0]]
 
-print(l_2d_unique)
-# [[0], [2], [1]]
-
-l_unique = get_unique_list(l)
-
-print(l_unique)
+print(get_unique_list(l))
 # [3, 2, 1, 5, 4]
 
-l = [3, 3, 2, 1, 5, 1, 4, 2, 3]
-
-l_duplicate = [x for x in set(l) if l.count(x) > 1]
-
-print(l_duplicate)
+print([x for x in set(l) if l.count(x) > 1])
 # [1, 2, 3]
 
-l_duplicate_order = [x for x in dict.fromkeys(l) if l.count(x) > 1]
-
-print(l_duplicate_order)
+print([x for x in dict.fromkeys(l) if l.count(x) > 1])
 # [3, 2, 1]
 
-l_duplicate_order = sorted([x for x in set(l) if l.count(x) > 1], key=l.index)
-
-print(l_duplicate_order)
+print(sorted([x for x in set(l) if l.count(x) > 1], key=l.index))
 # [3, 2, 1]
 
-l_2d = [[0], [2], [2], [1], [0], [0]]
-
-print(l_2d)
-# [[0], [2], [2], [1], [0], [0]]
+print([x for x in l if l.count(x) > 1])
+# [3, 3, 2, 1, 1, 2, 3]
 
 def get_duplicate_list(seq):
     seen = []
@@ -73,22 +53,17 @@ def get_duplicate_list_order(seq):
     seen = []
     return [x for x in seq if seq.count(x) > 1 and not seen.append(x) and seen.count(x) == 1]
 
-l_2d_duplicate = get_duplicate_list(l_2d)
+print(get_duplicate_list(l_2d))
+# [[0, 1], [1, 1]]
 
-print(l_2d_duplicate)
-# [[2], [0]]
+print(get_duplicate_list_order(l_2d))
+# [[1, 1], [0, 1]]
 
-l_2d_duplicate_order = get_duplicate_list_order(l_2d)
-
-print(l_2d_duplicate_order)
-# [[0], [2]]
-
-l_duplicate = get_duplicate_list(l)
-
-print(l_duplicate)
+print(get_duplicate_list(l))
 # [3, 1, 2]
 
-l_duplicate_order = get_duplicate_list_order(l)
-
-print(l_duplicate_order)
+print(get_duplicate_list_order(l))
 # [3, 2, 1]
+
+print([x for x in l_2d if l_2d.count(x) > 1])
+# [[1, 1], [0, 1], [0, 1], [1, 1], [1, 1]]
