3e49230bb00816fdcf23a0d05a1da5693f7090b3
nkmk <nkmk.on@gmail.com>
2019-08-06 21:31:16 +0900

    update pandas str extract extractall

--- notebook/pandas_str_extract_extractall.py
+++ notebook/pandas_str_extract_extractall.py
@@ -34,43 +34,56 @@ print(type(df_name))
 # C   ccc
 # <class 'pandas.core.frame.DataFrame'>
 
-df_multi = s_org.str.extract('(?P<local>.+)@(?P<domain>.+)', expand=True)
-print(df_multi)
+print(s_org.str.extract('(.+)@(.+)'))
+#      0          1
+# A  aaa    xxx.com
+# B  bbb    yyy.net
+# C  ccc  zzz.co.jp
+
+print(s_org.str.extract('(?P<local>.+)@(?P<domain>.+)'))
 #   local     domain
 # A   aaa    xxx.com
 # B   bbb    yyy.net
 # C   ccc  zzz.co.jp
 
-df_nan = s_org.str.extract('(a+)', expand=True)
-print(df_nan)
+print(s_org.str.extract('(a+)', expand=True))
 #      0
 # A  aaa
 # B  NaN
 # C  NaN
 
-df_single = s_org.str.extract('(\w+)', expand=True)
-print(df_single)
-#      0
-# A  aaa
-# B  bbb
-# C  ccc
+s_org2 = pd.Series(['aaa@xxx.com, iii@xxx.com', 'bbb@yyy.net, jjj@yyy.net', 'ccc@zzz.co.jp'],
+                   index=['A', 'B', 'C'])
+print(s_org2)
+# A    aaa@xxx.com, iii@xxx.com
+# B    bbb@yyy.net, jjj@yyy.net
+# C               ccc@zzz.co.jp
+# dtype: object
 
-df_all = s_org.str.extractall('(\w+)')
+print(s_org2.str.extract('([a-z]+)@([a-z.]+)', expand=True))
+#      0          1
+# A  aaa    xxx.com
+# B  bbb    yyy.net
+# C  ccc  zzz.co.jp
+
+df_all = s_org2.str.extractall('([a-z]+)@([a-z.]+)')
 print(df_all)
-#            0
-#   match     
-# A 0      aaa
-#   1      xxx
-#   2      com
-# B 0      bbb
-#   1      yyy
-#   2      net
-# C 0      ccc
-#   1      zzz
-#   2       co
-#   3       jp
+#            0          1
+#   match                
+# A 0      aaa    xxx.com
+#   1      iii    xxx.com
+# B 0      bbb    yyy.net
+#   1      jjj    yyy.net
+# C 0      ccc  zzz.co.jp
 
 print(df_all.index)
-# MultiIndex(levels=[['A', 'B', 'C'], [0, 1, 2, 3]],
-#            labels=[[0, 0, 0, 1, 1, 1, 2, 2, 2, 2], [0, 1, 2, 0, 1, 2, 0, 1, 2, 3]],
+# MultiIndex(levels=[['A', 'B', 'C'], [0, 1]],
+#            labels=[[0, 0, 1, 1, 2], [0, 1, 0, 1, 0]],
 #            names=[None, 'match'])
+
+print(s_org.str.extractall('([a-z]+)@([a-z.]+)'))
+#            0          1
+#   match                
+# A 0      aaa    xxx.com
+# B 0      bbb    yyy.net
+# C 0      ccc  zzz.co.jp
