1f8f2f5ffb3a22c94bbbbb7a0101321c48cdec49
nkmk <nkmk.on@gmail.com>
2019-08-21 23:34:47 +0900

    update list ndarray example

--- notebook/numpy_ndarray_example.py
+++ notebook/numpy_ndarray_example.py
@@ -9,15 +9,22 @@ print(arr_2d)
 # [[0 1 2]
 #  [3 4 5]]
 
-arr_2d_sqrt = np.sqrt(arr_2d)
-print(arr_2d_sqrt)
+print(arr[1])
+# 1
+
+print(arr_2d[1, 1])
+# 4
+
+print(arr_2d[0, 1:])
+# [1 2]
+
+print(np.sqrt(arr_2d))
 # [[0.         1.         1.41421356]
 #  [1.73205081 2.         2.23606798]]
 
 arr_1 = np.array([[1, 2], [3, 4]])
 arr_2 = np.array([[1, 2, 3], [4, 5, 6]])
 
-arr_product = np.dot(arr_1, arr_2)
-print(arr_product)
+print(np.dot(arr_1, arr_2))
 # [[ 9 12 15]
 #  [19 26 33]]
