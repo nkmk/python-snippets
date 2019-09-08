dc50e7e21f7e090fb98b6ce0f24d49ffe6fbf1e3
nkmk <nkmk.on@gmail.com>
2019-09-08 18:33:14 +0900

    update chage dict key

--- notebook/dict_change_key.py
+++ notebook/dict_change_key.py
@@ -72,3 +72,31 @@ d = {'k1': 1, 'k2': 2, 'k3': 3}
 change_dict_key_setdefault(d, 'k10', 'k100')
 print(d)
 # {'k1': 1, 'k2': 2, 'k3': 3, 'k100': None}
+
+def change_dict_key_exist(d, old_key, new_key):
+    if old_key in d:
+        d[new_key] = d.pop(old_key)
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_exist(d, 'k1', 'k10')
+print(d)
+# {'k2': 2, 'k3': 3, 'k10': 1}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_exist(d, 'k10', 'k100')
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_exist(d, 'k1', 'k2')
+print(d)
+# {'k2': 1, 'k3': 3}
+
+def change_dict_key_exist_setdefault(d, old_key, new_key):
+    if old_key in d:
+        d.setdefault(new_key, d.pop(old_key))
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+change_dict_key_exist_setdefault(d, 'k1', 'k2')
+print(d)
+# {'k2': 2, 'k3': 3}
