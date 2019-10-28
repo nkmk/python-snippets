8cd2941cdad298679b8afc22dff07daa0fc1eae2
nkmk <nkmk.on@gmail.com>
2019-10-28 23:13:49 +0900

    update numpy nan remove

--- notebook/numpy_nan_remove.py
+++ notebook/numpy_nan_remove.py
@@ -1,35 +1,57 @@
 import numpy as np
 
-arr = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
-print(arr)
-# [[ 11.  12.  nan  14.]
-#  [ 21.  nan  nan  24.]
-#  [ 31.  32.  33.  34.]]
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+print(a)
+# [[11. 12. nan 14.]
+#  [21. nan nan 24.]
+#  [31. 32. 33. 34.]]
 
-print(np.isnan(arr))
+print(np.isnan(a))
 # [[False False  True False]
 #  [False  True  True False]
 #  [False False False False]]
 
-print(arr[~np.isnan(arr)])
-# [ 11.  12.  14.  21.  24.  31.  32.  33.  34.]
+print(~np.isnan(a))
+# [[ True  True False  True]
+#  [ True False False  True]
+#  [ True  True  True  True]]
 
-print(np.isnan(arr).any(axis=1))
+print(a[~np.isnan(a)])
+# [11. 12. 14. 21. 24. 31. 32. 33. 34.]
+
+print(np.isnan(a).any(axis=1))
 # [ True  True False]
 
-print(~np.isnan(arr).any(axis=1))
+print(~np.isnan(a).any(axis=1))
 # [False False  True]
 
-print(arr[~np.isnan(arr).any(axis=1), :])
-# [[ 31.  32.  33.  34.]]
+print(a[~np.isnan(a).any(axis=1), :])
+# [[31. 32. 33. 34.]]
 
-print(arr[~np.isnan(arr).any(axis=1)])
-# [[ 31.  32.  33.  34.]]
+print(a[~np.isnan(a).any(axis=1)])
+# [[31. 32. 33. 34.]]
 
-print(~np.isnan(arr).any(axis=0))
+print(~np.isnan(a).any(axis=0))
 # [ True False False  True]
 
-print(arr[:, ~np.isnan(arr).any(axis=0)])
-# [[ 11.  14.]
-#  [ 21.  24.]
-#  [ 31.  34.]]
+print(a[:, ~np.isnan(a).any(axis=0)])
+# [[11. 14.]
+#  [21. 24.]
+#  [31. 34.]]
+
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+a[2, 2] = np.nan
+print(a)
+# [[11. 12. nan 14.]
+#  [21. nan nan 24.]
+#  [31. 32. nan 34.]]
+
+print(a[:, ~np.isnan(a).any(axis=0)])
+# [[11. 14.]
+#  [21. 24.]
+#  [31. 34.]]
+
+print(a[:, ~np.isnan(a).all(axis=0)])
+# [[11. 12. 14.]
+#  [21. nan 24.]
+#  [31. 32. 34.]]
