9012be0a5425f1c94fff02ea926e2d1a9c14d869
nkmk <nkmk.on@gmail.com>
2019-10-28 23:14:00 +0900

    update numpy nan replace

--- notebook/numpy_nan_replace.py
+++ notebook/numpy_nan_replace.py
@@ -1,31 +1,83 @@
 import numpy as np
 
-arr = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
-print(arr)
-# [[ 11.  12.  nan  14.]
-#  [ 21.  nan  nan  24.]
-#  [ 31.  32.  33.  34.]]
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+print(a)
+# [[11. 12. nan 14.]
+#  [21. nan nan 24.]
+#  [31. 32. 33. 34.]]
 
-arr_fill = np.genfromtxt('data/src/sample_nan.csv', delimiter=',', filling_values=0)
-print(arr_fill)
-# [[ 11.  12.   0.  14.]
-#  [ 21.   0.   0.  24.]
-#  [ 31.  32.  33.  34.]]
+a_nan = np.array([0, 1, np.nan, float('nan')])
+print(a_nan)
+# [ 0.  1. nan nan]
 
-print(np.isnan(arr))
+print(np.nan == np.nan)
+# False
+
+print(np.isnan(np.nan))
+# True
+
+print(a_nan == np.nan)
+# [False False False False]
+
+print(np.isnan(a_nan))
+# [False False  True  True]
+
+a_fill = np.genfromtxt('data/src/sample_nan.csv', delimiter=',', filling_values=0)
+print(a_fill)
+# [[11. 12.  0. 14.]
+#  [21.  0.  0. 24.]
+#  [31. 32. 33. 34.]]
+
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+print(np.nan_to_num(a))
+# [[11. 12.  0. 14.]
+#  [21.  0.  0. 24.]
+#  [31. 32. 33. 34.]]
+
+print(a)
+# [[11. 12. nan 14.]
+#  [21. nan nan 24.]
+#  [31. 32. 33. 34.]]
+
+print(np.nan_to_num(a, copy=False))
+# [[11. 12.  0. 14.]
+#  [21.  0.  0. 24.]
+#  [31. 32. 33. 34.]]
+
+print(a)
+# [[11. 12.  0. 14.]
+#  [21.  0.  0. 24.]
+#  [31. 32. 33. 34.]]
+
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+print(np.nan_to_num(a, nan=-1))
+# [[11. 12. -1. 14.]
+#  [21. -1. -1. 24.]
+#  [31. 32. 33. 34.]]
+
+print(np.nanmean(a))
+# 23.555555555555557
+
+print(np.nan_to_num(a, nan=np.nanmean(a)))
+# [[11.         12.         23.55555556 14.        ]
+#  [21.         23.55555556 23.55555556 24.        ]
+#  [31.         32.         33.         34.        ]]
+
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+print(np.isnan(a))
 # [[False False  True False]
 #  [False  True  True False]
 #  [False False False False]]
 
-arr[np.isnan(arr)] = 0
-print(arr)
-# [[ 11.  12.   0.  14.]
-#  [ 21.   0.   0.  24.]
-#  [ 31.  32.  33.  34.]]
+a[np.isnan(a)] = 0
+print(a)
+# [[11. 12.  0. 14.]
+#  [21.  0.  0. 24.]
+#  [31. 32. 33. 34.]]
 
-arr = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
-arr[np.isnan(arr)] = np.nanmean(arr)
-print(arr)
-# [[ 11.          12.          23.55555556  14.        ]
-#  [ 21.          23.55555556  23.55555556  24.        ]
-#  [ 31.          32.          33.          34.        ]]
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+a[np.isnan(a)] = np.nanmean(a)
+print(a)
+# [[11.         12.         23.55555556 14.        ]
+#  [21.         23.55555556 23.55555556 24.        ]
+#  [31.         32.         33.         34.        ]]
