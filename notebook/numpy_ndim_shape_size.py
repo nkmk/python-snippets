248943c63ac9a6c31a235ef14c68998cb7eef33b
nkmk <nkmk.on@gmail.com>
2019-10-29 22:17:00 +0900

    update numpy ndim shape size

--- notebook/numpy_ndim_shape_size.py
+++ notebook/numpy_ndim_shape_size.py
@@ -15,6 +15,7 @@ print(a_3d)
 # [[[ 0  1  2  3]
 #   [ 4  5  6  7]
 #   [ 8  9 10 11]]
+# 
 #  [[12 13 14 15]
 #   [16 17 18 19]
 #   [20 21 22 23]]]
@@ -67,3 +68,24 @@ print(a_2d.size)
 
 print(a_3d.size)
 # 24
+
+print(len(a_1d))
+# 3
+
+print(a_1d.shape[0])
+# 3
+
+print(a_1d.size)
+# 3
+
+print(len(a_2d))
+# 3
+
+print(a_2d.shape[0])
+# 3
+
+print(len(a_3d))
+# 2
+
+print(a_3d.shape[0])
+# 2
