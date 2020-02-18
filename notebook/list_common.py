f367eb8066cf842312246d9249f8b2b0dc3bf294
nkmk <nkmk.on@gmail.com>
2020-02-18 20:18:15 +0900

    update list common

--- notebook/list_common.py
+++ notebook/list_common.py
@@ -4,14 +4,14 @@ l3 = ['c', 'd', 'e']
 
 l1_l2_and = set(l1) & set(l2)
 print(l1_l2_and)
-# {'b', 'c'}
+# {'c', 'b'}
 
 print(type(l1_l2_and))
 # <class 'set'>
 
 l1_l2_and_list = list(l1_l2_and)
 print(l1_l2_and_list)
-# ['b', 'c']
+# ['c', 'b']
 
 print(type(l1_l2_and_list))
 # <class 'list'>
@@ -25,51 +25,54 @@ print(l1_l2_l3_and)
 
 l1_l2_sym_diff = set(l1) ^ set(l2)
 print(l1_l2_sym_diff)
-# {'d', 'a'}
+# {'a', 'd'}
 
 print(list(l1_l2_sym_diff))
-# ['d', 'a']
+# ['a', 'd']
 
 print(len(l1_l2_sym_diff))
 # 2
 
 l1_l2_l3_sym_diff = set(l1) ^ set(l2) ^ set(l3)
 print(l1_l2_l3_sym_diff)
-# {'e', 'c', 'a'}
+# {'c', 'a', 'e'}
 
 l_all = l1 + l2 + l3
 print(l_all)
 # ['a', 'b', 'c', 'b', 'c', 'd', 'c', 'd', 'e']
 
-l_all_only = [x for x in l_all if l_all.count(x) == 1]
+print(set(l_all))
+# {'d', 'b', 'a', 'c', 'e'}
+
+l_all_only = [x for x in set(l_all) if l_all.count(x) == 1]
 print(l_all_only)
 # ['a', 'e']
 
 l1_duplicate = ['a', 'a', 'b', 'c'] 
 
 l_duplicate_all = l1_duplicate + l2 + l3
-l_duplicate_all_only = [x for x in l_duplicate_all if l_duplicate_all.count(x) == 1]
+l_duplicate_all_only = [x for x in set(l_duplicate_all) if l_duplicate_all.count(x) == 1]
 print(l_duplicate_all_only)
 # ['e']
 
 l_unique_all = list(set(l1_duplicate)) + list(set(l2)) + list(set(l3))
 print(l_unique_all)
-# ['b', 'c', 'a', 'd', 'b', 'c', 'd', 'e', 'c']
+# ['c', 'b', 'a', 'c', 'b', 'd', 'c', 'd', 'e']
 
-l_uniaues_all_only = [x for x in l_unique_all if l_unique_all.count(x) == 1]
+l_uniaues_all_only = [x for x in set(l_unique_all) if l_unique_all.count(x) == 1]
 print(l_uniaues_all_only)
 # ['a', 'e']
 
 l1_l2_or = set(l1 + l2)
 print(l1_l2_or)
-# {'d', 'b', 'c', 'a'}
+# {'c', 'b', 'a', 'd'}
 
 print(list(l1_l2_or))
-# ['d', 'b', 'c', 'a']
+# ['c', 'b', 'a', 'd']
 
 print(len(l1_l2_or))
 # 4
 
 l1_l2_l3_or = set(l1 + l2 + l3)
 print(l1_l2_l3_or)
-# {'a', 'c', 'b', 'd', 'e'}
+# {'d', 'b', 'a', 'c', 'e'}
