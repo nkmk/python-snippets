803a3e9e7b80820da4357bafc95a11a991cffce0
nkmk <nkmk.on@gmail.com>
2020-05-12 22:31:56 +0900

    update adcoder version check

--- notebook/atcoder-version-check.py
+++ notebook/atcoder-version-check.py
@@ -2,6 +2,17 @@ import platform
 import numpy
 import scipy
 
-print('Python:', platform.python_version())
-print('NumPy: ', numpy.__version__)
-print('SciPy: ', scipy.__version__)
+print('Python:       ', platform.python_version())
+print('NumPy:        ', numpy.__version__)
+print('SciPy:        ', scipy.__version__)
+
+try:
+    import sklearn
+    import numba
+    import networkx
+
+    print('scikit-learn: ', sklearn.__version__)
+    print('numba:        ', numba.__version__)
+    print('networkx:     ', networkx.__version__)
+except:
+    pass
