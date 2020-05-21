bd5fe66dce8bcbf75abd0e87ff816e7fb3e2d032
nkmk <nkmk.on@gmail.com>
2020-05-21 21:37:51 +0900

    update pandas add column, row

--- notebook/pandas_add_column.py
+++ notebook/pandas_add_column.py
@@ -35,12 +35,6 @@ print(df)
 # ValueError: Length of values does not match length of index
 
 df['F'] = df['B'] + df['C']
-print(df)
-#        A   B   C  D  E     F
-# ONE    0  B1  C1  0  0  B1C1
-# TWO    0  B2  C2  0  1  B2C2
-# THREE  0  B3  C3  0  2  B3C3
-
 df['G'] = df['B'].str.lower()
 print(df)
 #        A   B   C  D  E     F   G
@@ -76,21 +70,14 @@ df = pd.DataFrame({'A': ['A1', 'A2', 'A3'],
                    'B': ['B1', 'B2', 'B3'],
                    'C': ['C1', 'C2', 'C3']},
                   index=['ONE', 'TWO', 'THREE'])
-print(df)
-#         A   B   C
-# ONE    A1  B1  C1
-# TWO    A2  B2  C2
-# THREE  A3  B3  C3
 
-df_new = df.assign(A=0)
-print(df_new)
+print(df.assign(A=0))
 #        A   B   C
 # ONE    0  B1  C1
 # TWO    0  B2  C2
 # THREE  0  B3  C3
 
-df_new = df.assign(D=0)
-print(df_new)
+print(df.assign(D=0))
 #         A   B   C  D
 # ONE    A1  B1  C1  0
 # TWO    A2  B2  C2  0
@@ -123,29 +110,52 @@ df = pd.DataFrame({'A': ['A1', 'A2', 'A3'],
                    'B': ['B1', 'B2', 'B3'],
                    'C': ['C1', 'C2', 'C3']},
                   index=['ONE', 'TWO', 'THREE'])
-print(df)
-#         A   B   C
-# ONE    A1  B1  C1
-# TWO    A2  B2  C2
-# THREE  A3  B3  C3
-
 s = pd.Series(['X2', 'X3', 'X4'], index=['TWO', 'THREE', 'FOUR'], name='X')
-print(s)
-# TWO      X2
-# THREE    X3
-# FOUR     X4
-# Name: X, dtype: object
 
-df_concat = pd.concat([df, s], axis=1)
-print(df_concat)
+df.insert(0, 'D', 0)
+print(df)
+#        D   A   B   C
+# ONE    0  A1  B1  C1
+# TWO    0  A2  B2  C2
+# THREE  0  A3  B3  C3
+
+df.insert(len(df.columns), 'E', s)
+print(df)
+#        D   A   B   C    E
+# ONE    0  A1  B1  C1  NaN
+# TWO    0  A2  B2  C2   X2
+# THREE  0  A3  B3  C3   X3
+
+# df.insert(10, 'F', 10)
+# ValueError: cannot insert F, already exists
+
+# df.insert(-1, 'F', 10)
+# ValueError: unbounded slice
+
+# df.insert(0, 'D', 10)
+# ValueError: cannot insert D, already exists
+
+df.insert(0, 'D', 10, allow_duplicates=True)
+print(df)
+#         D  D   A   B   C    E
+# ONE    10  0  A1  B1  C1  NaN
+# TWO    10  0  A2  B2  C2   X2
+# THREE  10  0  A3  B3  C3   X3
+
+df = pd.DataFrame({'A': ['A1', 'A2', 'A3'],
+                   'B': ['B1', 'B2', 'B3'],
+                   'C': ['C1', 'C2', 'C3']},
+                  index=['ONE', 'TWO', 'THREE'])
+s = pd.Series(['X2', 'X3', 'X4'], index=['TWO', 'THREE', 'FOUR'], name='X')
+
+print(pd.concat([df, s], axis=1))
 #          A    B    C    X
-# FOUR   NaN  NaN  NaN   X4
 # ONE     A1   B1   C1  NaN
-# THREE   A3   B3   C3   X3
 # TWO     A2   B2   C2   X2
+# THREE   A3   B3   C3   X3
+# FOUR   NaN  NaN  NaN   X4
 
-df_concat_in = pd.concat([df, s], axis=1, join='inner')
-print(df_concat_in)
+print(pd.concat([df, s], axis=1, join='inner'))
 #         A   B   C   X
 # TWO    A2  B2  C2  X2
 # THREE  A3  B3  C3  X3
@@ -153,8 +163,7 @@ print(df_concat_in)
 s1 = pd.Series(['X1', 'X2', 'X3'], index=df.index, name='X')
 s2 = pd.Series(['Y1', 'Y2', 'Y3'], index=df.index, name='Y')
 
-df_concat = pd.concat([df, s1, s2], axis=1)
-print(df_concat)
+print(pd.concat([df, s1, s2], axis=1))
 #         A   B   C   X   Y
 # ONE    A1  B1  C1  X1  Y1
 # TWO    A2  B2  C2  X2  Y2
@@ -167,8 +176,7 @@ print(df2)
 # TWO          0        1
 # THREE        0        2
 
-df_concat2 = pd.concat([df, df2], axis=1)
-print(df_concat2)
+print(pd.concat([df, df2], axis=1))
 #         A   B   C  df_col1  df_col2
 # ONE    A1  B1  C1        0        0
 # TWO    A2  B2  C2        0        1
--- notebook/pandas_add_row.py
+++ notebook/pandas_add_row.py
@@ -18,13 +18,6 @@ print(df)
 # THREE  A3  B3  C3
 
 df.loc['FOUR'] = 0
-print(df)
-#         A   B   C
-# ONE     0   0   0
-# TWO    A2  B2  C2
-# THREE  A3  B3  C3
-# FOUR    0   0   0
-
 df.loc['FIVE'] = ['A5', 'B5', 'C5']
 print(df)
 #         A   B   C
@@ -37,16 +30,6 @@ print(df)
 # df.loc['SIX'] = ['A6', 'B6']
 # ValueError: cannot set a row with mismatched columns
 
-df.loc['XXX'] = df.loc['TWO'] + df.loc['THREE']
-print(df)
-#           A     B     C
-# ONE       0     0     0
-# TWO      A2    B2    C2
-# THREE    A3    B3    C3
-# FOUR      0     0     0
-# FIVE     A5    B5    C5
-# XXX    A2A3  B2B3  C2C3
-
 s = pd.Series(['B6', 'C6', 'D6'], index=['B', 'C', 'D'], name='SIX')
 print(s)
 # B    B6
@@ -54,17 +37,8 @@ print(s)
 # D    D6
 # Name: SIX, dtype: object
 
+df.loc['XXX'] = df.loc['TWO'] + df.loc['THREE']
 df.loc['YYY'] = s
-print(df)
-#           A     B     C
-# ONE       0     0     0
-# TWO      A2    B2    C2
-# THREE    A3    B3    C3
-# FOUR      0     0     0
-# FIVE     A5    B5    C5
-# XXX    A2A3  B2B3  C2C3
-# YYY     NaN    B6    C6
-
 df.loc['ZZZ'] = s.values
 print(df)
 #           A     B     C
@@ -81,21 +55,29 @@ df = pd.DataFrame({'A': ['A1', 'A2', 'A3'],
                    'B': ['B1', 'B2', 'B3'],
                    'C': ['C1', 'C2', 'C3']},
                   index=['ONE', 'TWO', 'THREE'])
-print(df)
-#         A   B   C
-# ONE    A1  B1  C1
-# TWO    A2  B2  C2
-# THREE  A3  B3  C3
+
+# print(df.append(0))
+# TypeError: cannot concatenate object of type '<class 'int'>'; only Series and DataFrame objs are valid
+
+print(df.append([0, 1, 2]))
+#          A    B    C    0
+# ONE     A1   B1   C1  NaN
+# TWO     A2   B2   C2  NaN
+# THREE   A3   B3   C3  NaN
+# 0      NaN  NaN  NaN  0.0
+# 1      NaN  NaN  NaN  1.0
+# 2      NaN  NaN  NaN  2.0
+
+print(df.append({'A': 0, 'B': 1, 'C': 2}, ignore_index=True))
+#     A   B   C
+# 0  A1  B1  C1
+# 1  A2  B2  C2
+# 2  A3  B3  C3
+# 3   0   1   2
 
 s = pd.Series(['A4', 'B4', 'C4'], index=df.columns, name='FOUR')
-print(s)
-# A    A4
-# B    B4
-# C    C4
-# Name: FOUR, dtype: object
 
-df_append = df.append(s)
-print(df_append)
+print(df.append(s))
 #         A   B   C
 # ONE    A1  B1  C1
 # TWO    A2  B2  C2
@@ -103,14 +85,8 @@ print(df_append)
 # FOUR   A4  B4  C4
 
 s_mismatch = pd.Series(['B5', 'C5', 'D5'], index=['B', 'C', 'D'], name='FIVE')
-print(s_mismatch)
-# B    B5
-# C    C5
-# D    D5
-# Name: FIVE, dtype: object
 
-df_append_mismatch = df.append(s_mismatch)
-print(df_append_mismatch)
+print(df.append(s_mismatch))
 #          A   B   C    D
 # ONE     A1  B1  C1  NaN
 # TWO     A2  B2  C2  NaN
@@ -125,11 +101,17 @@ print(df)
 
 s_no_name = pd.Series(['B4', 'C4', 'D4'], index=['B', 'C', 'D'])
 
-# df_append_no_name = df.append(s_no_name)
+# print(df.append(s_no_name))
 # TypeError: Can only append a Series if ignore_index=True or if the Series has a name
 
-df_append_multi = df.append([s, s_mismatch])
-print(df_append_multi)
+print(df.append(s_no_name, ignore_index=True))
+#      A   B   C    D
+# 0   A1  B1  C1  NaN
+# 1   A2  B2  C2  NaN
+# 2   A3  B3  C3  NaN
+# 3  NaN  B4  C4   D4
+
+print(df.append([s, s_mismatch]))
 #          A   B   C    D
 # ONE     A1  B1  C1  NaN
 # TWO     A2  B2  C2  NaN
@@ -137,85 +119,92 @@ print(df_append_multi)
 # FOUR    A4  B4  C4  NaN
 # FIVE   NaN  B5  C5   D5
 
-df2 = pd.DataFrame([['B4', 'C4', 'D4'], ['B5', 'C5', 'D5']], 
-                   index=['FOUR', 'FIVE'], columns=['B', 'C', 'D'])
+df2 = pd.DataFrame([['B6', 'C6', 'D6'], ['B7', 'C7', 'D7']], 
+                   index=['SIX', 'SEVEN'], columns=['B', 'C', 'D'])
 print(df2)
-#        B   C   D
-# FOUR  B4  C4  D4
-# FIVE  B5  C5  D5
+#         B   C   D
+# SIX    B6  C6  D6
+# SEVEN  B7  C7  D7
 
-df_append = df.append(df2)
-print(df_append)
+print(df.append(df2))
 #          A   B   C    D
 # ONE     A1  B1  C1  NaN
 # TWO     A2  B2  C2  NaN
 # THREE   A3  B3  C3  NaN
-# FOUR   NaN  B4  C4   D4
-# FIVE   NaN  B5  C5   D5
+# SIX    NaN  B6  C6   D6
+# SEVEN  NaN  B7  C7   D7
 
-df_concat = pd.concat([df, df2])
-print(df_concat)
+# print(df.append([s, df2]))
+# ValueError: all the input array dimensions for the concatenation axis must match exactly, but along dimension 1, the array at index 0 has size 5 and the array at index 1 has size 3
+
+print(df.append(s).append(df2))
 #          A   B   C    D
 # ONE     A1  B1  C1  NaN
 # TWO     A2  B2  C2  NaN
 # THREE   A3  B3  C3  NaN
-# FOUR   NaN  B4  C4   D4
-# FIVE   NaN  B5  C5   D5
+# FOUR    A4  B4  C4  NaN
+# SIX    NaN  B6  C6   D6
+# SEVEN  NaN  B7  C7   D7
 
-df_concat_in = pd.concat([df, df2], join='inner')
-print(df_concat_in)
+print(pd.concat([df, df2]))
+#          A   B   C    D
+# ONE     A1  B1  C1  NaN
+# TWO     A2  B2  C2  NaN
+# THREE   A3  B3  C3  NaN
+# SIX    NaN  B6  C6   D6
+# SEVEN  NaN  B7  C7   D7
+
+print(pd.concat([df, df2], join='inner'))
 #         B   C
 # ONE    B1  C1
 # TWO    B2  C2
 # THREE  B3  C3
-# FOUR   B4  C4
-# FIVE   B5  C5
+# SIX    B6  C6
+# SEVEN  B7  C7
 
-s = pd.Series(['AX', 'BX', 'CX'], index=df.columns, name='X')
-print(s)
-# A    AX
-# B    BX
-# C    CX
-# Name: X, dtype: object
-
-df_append_s = df.append(s)
-print(df_append_s)
+print(df.append(s))
 #         A   B   C
 # ONE    A1  B1  C1
 # TWO    A2  B2  C2
 # THREE  A3  B3  C3
-# X      AX  BX  CX
+# FOUR   A4  B4  C4
 
-df_concat_s = pd.concat([df, s])
-print(df_concat_s)
+print(pd.concat([df, s]))
 #          A    B    C    0
 # ONE     A1   B1   C1  NaN
 # TWO     A2   B2   C2  NaN
 # THREE   A3   B3   C3  NaN
-# A      NaN  NaN  NaN   AX
-# B      NaN  NaN  NaN   BX
-# C      NaN  NaN  NaN   CX
+# A      NaN  NaN  NaN   A4
+# B      NaN  NaN  NaN   B4
+# C      NaN  NaN  NaN   C4
 
-st = pd.DataFrame(s).T
-print(type(st))
-print(st)
-# <class 'pandas.core.frame.DataFrame'>
-#     A   B   C
-# X  AX  BX  CX
+print(pd.DataFrame(s).T)
+#        A   B   C
+# FOUR  A4  B4  C4
 
-df_concat_st = pd.concat([df, pd.DataFrame(s).T])
-print(df_concat_st)
+print(pd.concat([df, pd.DataFrame(s).T]))
 #         A   B   C
 # ONE    A1  B1  C1
 # TWO    A2  B2  C2
 # THREE  A3  B3  C3
-# X      AX  BX  CX
+# FOUR   A4  B4  C4
 
-df_assign = df.T.assign(FOUR=0, FIVE=['A5', 'B5', 'C5']).T
-print(df_assign)
+print(df.T.assign(FOUR=0, FIVE=['A5', 'B5', 'C5']).T)
 #         A   B   C
 # ONE    A1  B1  C1
 # TWO    A2  B2  C2
 # THREE  A3  B3  C3
 # FOUR    0   0   0
 # FIVE   A5  B5  C5
+
+# df_insert = df.T.insert(0, 'FOUR', 0).T
+# AttributeError: 'NoneType' object has no attribute 'T'
+
+df_T = df.T
+df_T.insert(0, 'FOUR', 0)
+print(df_T.T)
+#         A   B   C
+# FOUR    0   0   0
+# ONE    A1  B1  C1
+# TWO    A2  B2  C2
+# THREE  A3  B3  C3
