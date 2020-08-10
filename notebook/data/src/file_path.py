82d66d2c3cb78e25cc6bba6fb484c5c7176fb02c
nkmk <nkmk.on@gmail.com>
2020-08-10 14:00:18 +0900

    update for __file__

--- notebook/data/src/file_path.py
+++ notebook/data/src/file_path.py
@@ -9,7 +9,7 @@ print('abs dirname: ', os.path.dirname(os.path.abspath(__file__)))
 
 print()
 print('[set target path 1]')
-target_path_1 = os.path.join(os.path.dirname(__file__), 'test.txt')
+target_path_1 = os.path.join(os.path.dirname(__file__), 'target_1.txt')
 
 print('target_path_1: ', target_path_1)
 
@@ -19,9 +19,10 @@ with open(target_path_1) as f:
 
 print()
 print('[set target path 2]')
-target_path_2 = os.path.join(os.path.dirname(__file__), '../dst/test_new.json')
+target_path_2 = os.path.join(os.path.dirname(__file__), '../dst/target_2.txt')
 
 print('target_path_2: ', target_path_2)
+print('normalize    : ', os.path.normpath(target_path_2))
 
 print('read target file:')
 with open(target_path_2) as f:
@@ -34,7 +35,7 @@ print('getcwd:      ', os.getcwd())
 
 print()
 print('[set target path 1 (after chdir)]')
-target_path_1 = 'test.txt'
+target_path_1 = 'target_1.txt'
 
 print('target_path_1: ', target_path_1)
 
@@ -44,7 +45,7 @@ with open(target_path_1) as f:
 
 print()
 print('[set target path 2 (after chdir)]')
-target_path_2 = '../dst/test_new.json'
+target_path_2 = '../dst/target_2.txt'
 
 print('target_path_2: ', target_path_2)
 
--- notebook/file_path_bash.sh
+++ notebook/file_path_bash.sh
@@ -10,41 +10,68 @@ python3 data/src/file_path.py
 # abs dirname:  /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
 # 
 # [set target path 1]
-# target_path_1:  data/src/test.txt
+# target_path_1:  data/src/target_1.txt
 # read target file:
-# line 1
-# line 2
-# line 3
+# !! This is "target_1.txt" !!
 # 
 # [set target path 2]
-# target_path_2:  data/src/../dst/test_new.json
+# target_path_2:  data/src/../dst/target_2.txt
+# normalize    :  data/dst/target_2.txt
 # read target file:
-# {
-#   "A": 100,
-#   "B": "abc",
-#   "C": "あいうえお"
-# }
+# !! This is "target_2.txt" !!
 # 
 # [change directory]
 # getcwd:       /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
 # 
 # [set target path 1 (after chdir)]
-# target_path_1:  test.txt
+# target_path_1:  target_1.txt
 # read target file:
-# line 1
-# line 2
-# line 3
+# !! This is "target_1.txt" !!
 # 
 # [set target path 2 (after chdir)]
-# target_path_2:  ../dst/test_new.json
+# target_path_2:  ../dst/target_2.txt
 # read target file:
-# {
-#   "A": 100,
-#   "B": "abc",
-#   "C": "あいうえお"
-# }
+# !! This is "target_2.txt" !!
+
+pwd
+# /Users/mbp/Documents/my-project/python-snippets/notebook
+
+python3 /Users/mbp/Documents/my-project/python-snippets/notebook/data/src/file_path.py
+# getcwd:       /Users/mbp/Documents/my-project/python-snippets/notebook
+# __file__:     /Users/mbp/Documents/my-project/python-snippets/notebook/data/src/file_path.py
+# basename:     file_path.py
+# dirname:      /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
+# abspath:      /Users/mbp/Documents/my-project/python-snippets/notebook/data/src/file_path.py
+# abs dirname:  /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
+# 
+# [set target path 1]
+# target_path_1:  /Users/mbp/Documents/my-project/python-snippets/notebook/data/src/target_1.txt
+# read target file:
+# !! This is "target_1.txt" !!
+# 
+# [set target path 2]
+# target_path_2:  /Users/mbp/Documents/my-project/python-snippets/notebook/data/src/../dst/target_2.txt
+# normalize    :  /Users/mbp/Documents/my-project/python-snippets/notebook/data/dst/target_2.txt
+# read target file:
+# !! This is "target_2.txt" !!
+# 
+# [change directory]
+# getcwd:       /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
+# 
+# [set target path 1 (after chdir)]
+# target_path_1:  target_1.txt
+# read target file:
+# !! This is "target_1.txt" !!
+# 
+# [set target path 2 (after chdir)]
+# target_path_2:  ../dst/target_2.txt
+# read target file:
+# !! This is "target_2.txt" !!
 
 cd data/src
+
+pwd
+# /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
 
 python3 file_path.py
 # getcwd:       /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
@@ -55,36 +82,25 @@ python3 file_path.py
 # abs dirname:  /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
 # 
 # [set target path 1]
-# target_path_1:  test.txt
+# target_path_1:  target_1.txt
 # read target file:
-# line 1
-# line 2
-# line 3
+# !! This is "target_1.txt" !!
 # 
 # [set target path 2]
-# target_path_2:  ../dst/test_new.json
+# target_path_2:  ../dst/target_2.txt
+# normalize    :  ../dst/target_2.txt
 # read target file:
-# {
-#   "A": 100,
-#   "B": "abc",
-#   "C": "あいうえお"
-# }
+# !! This is "target_2.txt" !!
 # 
 # [change directory]
 # getcwd:       /Users/mbp/Documents/my-project/python-snippets/notebook/data/src
 # 
 # [set target path 1 (after chdir)]
-# target_path_1:  test.txt
+# target_path_1:  target_1.txt
 # read target file:
-# line 1
-# line 2
-# line 3
+# !! This is "target_1.txt" !!
 # 
 # [set target path 2 (after chdir)]
-# target_path_2:  ../dst/test_new.json
+# target_path_2:  ../dst/target_2.txt
 # read target file:
-# {
-#   "A": 100,
-#   "B": "abc",
-#   "C": "あいうえお"
-# }
+# !! This is "target_2.txt" !!
