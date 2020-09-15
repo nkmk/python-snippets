db0a92763c370a9bf0ae80163c4f23eec6d9f502
nkmk <nkmk.on@gmail.com>
2020-09-15 15:49:23 +0900

    update int max

--- notebook/sys_maxsize.py
+++ notebook/sys_maxsize.py
@@ -23,8 +23,22 @@ print(i)
 print(i > sys.maxsize)
 # True
 
+print(sys.float_info.max)
+# 1.7976931348623157e+308
+
+i_e309 = 10**309
+
+print(type(i_e309))
+# <class 'int'>
+
+print(i_e309 > sys.float_info.max)
+# True
+
 print(float('inf'))
 # inf
 
-print(i > float('inf'))
+print(i_e309 > float('inf'))
 # False
+
+# int(float('inf'))
+# OverflowError: cannot convert float infinity to integer
