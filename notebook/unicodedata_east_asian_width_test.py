3fa21483244c13722e327205c4750605e34a28bf
nkmk <nkmk.on@gmail.com>
2021-01-03 14:12:28 +0900

    update unicocedata east asian width

--- notebook/unicodedata_east_asian_width_test.py
+++ notebook/unicodedata_east_asian_width_test.py
@@ -16,3 +16,27 @@ print(unicodedata.east_asian_width('ｱ'))  # 半角カナ
 
 print(unicodedata.east_asian_width('Å'))  # 特殊文字（例: オングストローム）
 # A
+
+def get_east_asian_width_count(text):
+    count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+            count += 2
+        else:
+            count += 1
+    return count
+
+print(get_east_asian_width_count('あいうえお'))
+# 10
+
+print(get_east_asian_width_count('abcde'))
+# 5
+
+print(get_east_asian_width_count('ｱｲｳｴｵ'))
+# 5
+
+print(get_east_asian_width_count('ａｂｃｄｅ'))
+# 10
+
+print(get_east_asian_width_count('あｱaａ'))  # 全角2文字（あ, ａ）、半角2文字（ｱ, a）
+# 6
