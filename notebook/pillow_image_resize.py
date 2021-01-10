a3c261dee61e4bd3201478477389813a8aa1ed82
nkmk <nkmk.on@gmail.com>
2021-01-10 14:22:23 +0900

    update Pillow image resize

--- notebook/pillow_image_resize.py
+++ notebook/pillow_image_resize.py
@@ -8,5 +8,5 @@ img_resize.save('data/dst/lena_pillow_resize_nearest.jpg')
 img_resize_lanczos = img.resize((256, 256), Image.LANCZOS)
 img_resize_lanczos.save('data/dst/lena_pillow_resize_lanczos.jpg')
 
-img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
+img_resize = img.resize((img.width // 2, img.height // 2))
 img_resize_lanczos.save('data/dst/lena_pillow_resize_half.jpg')
--- notebook/pillow_image_resize_all.py
+++ notebook/pillow_image_resize_all.py
@@ -2,30 +2,36 @@ import os
 import glob
 from PIL import Image
 
+dst_dir = 'data/temp/images_half'
+os.makedirs(dst_dir, exist_ok=True)
+
 files = glob.glob('./data/temp/images/*.jpg')
 
 for f in files:
     img = Image.open(f)
-    img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
-    title, ext = os.path.splitext(f)
-    img_resize.save(title + '_half' + ext)
+    img_resize = img.resize((img.width // 2, img.height // 2))
+    root, ext = os.path.splitext(f)
+    basename = os.path.basename(root)
+    img_resize.save(os.path.join(dst_dir, basename + '_half' + ext))
 
 files = glob.glob('./data/temp/images/*')
 
 for f in files:
-    title, ext = os.path.splitext(f)
+    root, ext = os.path.splitext(f)
     if ext in ['.jpg', '.png']:
         img = Image.open(f)
-        img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
-        img_resize.save(title + '_half' + ext)
+        img_resize = img.resize((img.width // 2, img.height // 2))
+        basename = os.path.basename(root)
+        img_resize.save(os.path.join(dst_dir, basename + '_half' + ext))
 
 files = glob.glob('./data/temp/images/*')
 
 for f in files:
     try:
         img = Image.open(f)
-        img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
-        title, ext = os.path.splitext(f)
-        img_resize.save(title + '_half' + ext)
+        img_resize = img.resize((img.width // 2, img.height // 2))
+        root, ext = os.path.splitext(f)
+        basename = os.path.basename(root)
+        img_resize.save(os.path.join(dst_dir, basename + '_half' + ext))
     except OSError as e:
         pass
