e60fed499d3f67b3d7532128259b77af539c7a90
nkmk <nkmk.on@gmail.com>
2021-01-16 17:16:07 +0900

    updatr str num determine

--- notebook/str_num_determine.py
+++ notebook/str_num_determine.py
@@ -18,14 +18,44 @@ print('isnumeric:', s.isnumeric())
 # isdigit: True
 # isnumeric: True
 
-s = '\u00B2'
+s = '-1.23'
 print('s =', s)
 print('isdecimal:', s.isdecimal())
 print('isdigit:', s.isdigit())
 print('isnumeric:', s.isnumeric())
-# s = ²
+# s = -1.23
+# isdecimal: False
+# isdigit: False
+# isnumeric: False
+
+s = '10\u00B2'
+print('s =', s)
+print('isdecimal:', s.isdecimal())
+print('isdigit:', s.isdigit())
+print('isnumeric:', s.isnumeric())
+# s = 10²
 # isdecimal: False
 # isdigit: True
+# isnumeric: True
+
+s = '\u00BD'
+print('s =', s)
+print('isdecimal:', s.isdecimal())
+print('isdigit:', s.isdigit())
+print('isnumeric:', s.isnumeric())
+# s = ½
+# isdecimal: False
+# isdigit: False
+# isnumeric: True
+
+s = '\u2166'
+print('s =', s)
+print('isdecimal:', s.isdecimal())
+print('isdigit:', s.isdigit())
+print('isnumeric:', s.isnumeric())
+# s = Ⅶ
+# isdecimal: False
+# isdigit: False
 # isnumeric: True
 
 s = '一二三四五六七八九〇'
@@ -102,19 +132,41 @@ print('isalpha:', s.isalpha())
 # s = 〇
 # isalpha: False
 
-s = 'abc100'
+s = '\u2166'
+print('s =', s)
+print('isalpha:', s.isalpha())
+# s = Ⅶ
+# isalpha: False
+
+s = 'abc123'
 print('s =', s)
 print('isalnum:', s.isalnum())
 print('isalpha:', s.isalpha())
 print('isdecimal:', s.isdecimal())
 print('isdigit:', s.isdigit())
 print('isnumeric:', s.isnumeric())
-# s = abc100
+# s = abc123
 # isalnum: True
 # isalpha: False
 # isdecimal: False
 # isdigit: False
 # isnumeric: False
+
+s = 'abc123+-,.&'
+print('s =', s)
+print('isascii:', s.isascii())
+print('isalnum:', s.isalnum())
+# s = abc123+-,.&
+# isascii: True
+# isalnum: False
+
+s = 'あいうえお'
+print('s =', s)
+print('isascii:', s.isascii())
+print('isalnum:', s.isalnum())
+# s = あいうえお
+# isascii: False
+# isalnum: True
 
 s = ''
 print('s =', s)
@@ -123,68 +175,47 @@ print('isalpha:', s.isalpha())
 print('isdecimal:', s.isdecimal())
 print('isdigit:', s.isdigit())
 print('isnumeric:', s.isnumeric())
+print('isascii:', s.isascii())
 # s = 
 # isalnum: False
 # isalpha: False
 # isdecimal: False
 # isdigit: False
 # isnumeric: False
+# isascii: True
 
-s = '10,000,000'
+print(bool(''))
+# False
+
+print(bool('abc123'))
+# True
+
+s = '-1.23'
 print('s =', s)
 print('isalnum:', s.isalnum())
 print('isalpha:', s.isalpha())
-print('isdigit:', s.isdigit())
 print('isdecimal:', s.isdecimal())
+print('isdigit:', s.isdigit())
 print('isnumeric:', s.isnumeric())
-# s = 10,000,000
+print('isascii:', s.isascii())
+# s = -1.23
 # isalnum: False
 # isalpha: False
-# isdigit: False
 # isdecimal: False
+# isdigit: False
 # isnumeric: False
+# isascii: True
 
-s = '1.23'
-print('s =', s)
-print('isalnum:', s.isalnum())
-print('isalpha:', s.isalpha())
-print('isdigit:', s.isdigit())
-print('isdecimal:', s.isdecimal())
-print('isnumeric:', s.isnumeric())
-# s = 1.23
-# isalnum: False
-# isalpha: False
-# isdigit: False
-# isdecimal: False
-# isnumeric: False
+print(float('-1.23'))
+# -1.23
 
-s = '-100'
-print('s =', s)
-print('isalnum:', s.isalnum())
-print('isalpha:', s.isalpha())
-print('isdigit:', s.isdigit())
-print('isdecimal:', s.isdecimal())
-print('isnumeric:', s.isnumeric())
-# s = -100
-# isalnum: False
-# isalpha: False
-# isdigit: False
-# isdecimal: False
-# isnumeric: False
+print(type(float('-1.23')))
+# <class 'float'>
+
+# print(float('abc'))
+# ValueError: could not convert string to float: 'abc'
 
 def is_num(s):
-    return s.replace(',', '').replace('.', '').replace('-', '').isnumeric()
-
-print(is_num('-1.23'))
-# True
-
-print(is_num('10,000,000'))
-# True
-
-print(is_num('128.0.0.0'))
-# True
-
-def is_num2(s):
     try:
         float(s)
     except ValueError:
@@ -192,14 +223,42 @@ def is_num2(s):
     else:
         return True
 
-print(is_num2('-1.23'))
+print(is_num('123'))
 # True
 
-print(is_num2('128.0.0.0'))
+print(is_num('-1.23'))
+# True
+
+print(is_num('+1.23e10'))
+# True
+
+print(is_num('abc'))
 # False
 
-print(is_num2('10,000,000'))
+print(is_num('10,000,000'))
 # False
 
-print(is_num2('1.23e10'))
+def is_num_delimiter(s):
+    try:
+        float(s.replace(',', ''))
+    except ValueError:
+        return False
+    else:
+        return True
+
+print(is_num_delimiter('10,000,000'))
+# True
+
+def is_num_delimiter2(s):
+    try:
+        float(s.replace(',', '').replace(' ', ''))
+    except ValueError:
+        return False
+    else:
+        return True
+
+print(is_num_delimiter2('10,000,000'))
+# True
+
+print(is_num_delimiter2('10 000 000'))
 # True
