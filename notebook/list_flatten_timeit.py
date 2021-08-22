13dcadcc2e7addffdb2288214d6fece1ee75356b
nkmk <nkmk.on@gmail.com>
2021-08-22 16:18:15 +0900

    update list flatten

--- notebook/list_flatten_timeit.py
+++ notebook/list_flatten_timeit.py
@@ -1,33 +1,45 @@
 import itertools
 
-l_2d_5 = [[0, 1, 2]] * 5
+l_2d_5 = [[0, 1, 2] for i in range(5)]
 print(l_2d_5)
 # [[0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]]
 
 %%timeit
 list(itertools.chain.from_iterable(l_2d_5))
-# 711 ns ± 21.2 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+# 537 ns ± 4.59 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
 
 %%timeit
 sum(l_2d_5, [])
-# 448 ns ± 10.8 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+# 319 ns ± 1.85 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
 
-l_2d_100 = [[0, 1, 2]] * 100
+%%timeit
+[x for row in l_2d_5 for x in row]
+# 764 ns ± 32.6 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+
+l_2d_100 = [[0, 1, 2] for i in range(100)]
 
 %%timeit
 list(itertools.chain.from_iterable(l_2d_100))
-# 7.27 µs ± 390 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+# 6.94 µs ± 139 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
 
 %%timeit
 sum(l_2d_100, [])
-# 41 µs ± 1.34 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
+# 35.5 µs ± 1.2 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
 
-l_2d_10000 = [[0, 1, 2]] * 10000
+%%timeit
+[x for row in l_2d_100 for x in row]
+# 13.5 µs ± 959 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+
+l_2d_10000 = [[0, 1, 2] for i in range(10000)]
 
 %%timeit
 list(itertools.chain.from_iterable(l_2d_10000))
-# 513 µs ± 15.5 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+# 552 µs ± 79.4 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
 
 %%timeit
 sum(l_2d_10000, [])
-# 418 ms ± 22.8 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+# 343 ms ± 2.19 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+
+%%timeit
+[x for row in l_2d_10000 for x in row]
+# 1.11 ms ± 110 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
