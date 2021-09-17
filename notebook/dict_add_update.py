484193e61e02628a990d840421aea10f789f7e5f
nkmk <nkmk.on@gmail.com>
2021-09-17 14:35:49 +0900

    update dict create, update, merge

--- notebook/dict_add_update.py
+++ notebook/dict_add_update.py
@@ -16,7 +16,7 @@ print(d1)
 # {'k1': 100, 'k2': 2, 'k3': 3, 'k4': 4}
 
 d1 = {'k1': 1, 'k2': 2}
-d2 = {'k3': 3, 'k4': 4}
+d2 = {'k1': 100, 'k3': 3, 'k4': 4}
 d3 = {'k5': 5, 'k6': 6}
 
 # d1.update(d2, d3)
@@ -24,7 +24,32 @@ d3 = {'k5': 5, 'k6': 6}
 
 d1.update(**d2, **d3)
 print(d1)
-# {'k1': 1, 'k2': 2, 'k3': 3, 'k4': 4, 'k5': 5, 'k6': 6}
+# {'k1': 100, 'k2': 2, 'k3': 3, 'k4': 4, 'k5': 5, 'k6': 6}
+
+d1 = {'k1': 1, 'k2': 2}
+d2 = {'k1': 100, 'k3': 3, 'k4': 4}
+d3 = {'k5': 5, 'k6': 6}
+
+# d3.update(**d1, **d2)
+# TypeError: dict.update() got multiple values for keyword argument 'k1'
+
+d1 = {'k1': 1, 'k2': 2}
+d2 = {'k3': 3, 'k4': 4}
+
+print({**d1, **d2})
+# {'k1': 1, 'k2': 2, 'k3': 3, 'k4': 4}
+
+print(dict(**d1, **d2))
+# {'k1': 1, 'k2': 2, 'k3': 3, 'k4': 4}
+
+d1 = {'k1': 1, 'k2': 2}
+d2 = {'k1': 100, 'k3': 3, 'k4': 4}
+
+print({**d1, **d2})
+# {'k1': 100, 'k2': 2, 'k3': 3, 'k4': 4}
+
+# print(dict(**d1, **d2))
+# TypeError: dict() got multiple values for keyword argument 'k1'
 
 d = {'k1': 1, 'k2': 2}
 
@@ -46,3 +71,23 @@ values = [100, 3, 4]
 d.update(zip(keys, values))
 print(d)
 # {'k1': 100, 'k2': 2, 'k3': 3, 'k4': 4}
+
+d = {'k1': 1, 'k2': 2}
+
+# d.update(k3=3, k3=300)
+# SyntaxError: keyword argument repeated: k3
+
+d = {'k1': 1, 'k2': 2}
+
+d.update([('k3', 3), ('k3', 300)])
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 300}
+
+d = {'k1': 1, 'k2': 2}
+
+keys = ['k3', 'k3']
+values = [3, 300]
+
+d.update(zip(keys, values))
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 300}
--- notebook/dict_create.py
+++ notebook/dict_create.py
@@ -6,58 +6,6 @@ d = {'k1': 1, 'k2': 2, 'k3': 3, 'k3': 300}
 print(d)
 # {'k1': 1, 'k2': 2, 'k3': 300}
 
-d = dict(k1=1, k2=2, k3=3)
-print(d)
-# {'k1': 1, 'k2': 2, 'k3': 3}
-
-d = dict([('k1', 1), ('k2', 2), ('k3', 4)])
-print(d)
-# {'k1': 1, 'k2': 2, 'k3': 4}
-
-d = dict((['k1', 1], ['k2', 2], ['k3', 4]))
-print(d)
-# {'k1': 1, 'k2': 2, 'k3': 4}
-
-d = dict([{'k1', 1}, {'k2', 2}, {'k3', 4}])
-print(d)
-# {1: 'k1', 2: 'k2', 'k3': 4}
-
-keys = ['k1', 'k2', 'k3']
-values = [1, 2, 3]
-
-d = dict(zip(keys, values))
-print(d)
-# {'k1': 1, 'k2': 2, 'k3': 3}
-
-d_other = {'k10': 10, 'k100': 100}
-
-d = dict(d_other)
-print(d)
-# {'k10': 10, 'k100': 100}
-
-print(d == d_other)
-# True
-
-print(d is d_other)
-# False
-
-l = ['Alice', 'Bob', 'Charlie']
-
-d = {s: len(s) for s in l}
-print(d)
-# {'Alice': 5, 'Bob': 3, 'Charlie': 7}
-
-keys = ['k1', 'k2', 'k3']
-values = [1, 2, 3]
-
-d = {k: v for k, v in zip(keys, values)}
-print(d)
-# {'k1': 1, 'k2': 2, 'k3': 3}
-
-d = {k: v for k, v in zip(keys, values) if v % 2 == 1}
-print(d)
-# {'k1': 1, 'k3': 3}
-
 d1 = {'k1': 1, 'k2': 2}
 d2 = {'k3': 3, 'k4': 4}
 
@@ -84,6 +32,38 @@ d4 = {'k1': 100, 'k3': 300}
 print({**d1, **d2, **d3, **d4, 'k5': 500})
 # {'k1': 100, 'k2': 2, 'k3': 300, 'k4': 4, 'k5': 500, 'k6': 6}
 
+d = dict(k1=1, k2=2, k3=3)
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+# d = dict(k1=1, k2=2, k3=3, k3=300)
+# SyntaxError: keyword argument repeated: k3
+
+d = dict([('k1', 1), ('k2', 2), ('k3', 3)])
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+d = dict((['k1', 1], ['k2', 2], ['k3', 3]))
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+d = dict([('k1', 1), ('k2', 2), ('k3', 3), ('k3', 300)])
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 300}
+
+keys = ['k1', 'k2', 'k3']
+values = [1, 2, 3]
+
+d = dict(zip(keys, values))
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+d_other = {'k10': 10, 'k100': 100}
+
+d = dict(d_other)
+print(d)
+# {'k10': 10, 'k100': 100}
+
 d1 = {'k1': 1, 'k2': 2}
 d2 = {'k3': 3, 'k4': 4}
 
@@ -93,3 +73,30 @@ d2 = {'k3': 3, 'k4': 4}
 d = dict(**d1, **d2)
 print(d)
 # {'k1': 1, 'k2': 2, 'k3': 3, 'k4': 4}
+
+d1 = {'k1': 1, 'k2': 2}
+d2 = {'k1': 100, 'k3': 3}
+
+# d = dict(**d1, **d2)
+# TypeError: dict() got multiple values for keyword argument 'k1'
+
+d = {**d1, **d2}
+print(d)
+# {'k1': 100, 'k2': 2, 'k3': 3}
+
+l = ['Alice', 'Bob', 'Charlie']
+
+d = {s: len(s) for s in l}
+print(d)
+# {'Alice': 5, 'Bob': 3, 'Charlie': 7}
+
+keys = ['k1', 'k2', 'k3']
+values = [1, 2, 3]
+
+d = {k: v for k, v in zip(keys, values)}
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+
+d = {k: v for k, v in zip(keys, values) if v % 2 == 1}
+print(d)
+# {'k1': 1, 'k3': 3}
