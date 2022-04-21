00035de75bdac7ed6efb59c87e9f1a90e68fee98
nkmk <nkmk.on@gmail.com>
2022-04-21 13:27:21 +0900

    add pyperclip

---
+++ notebook/pyperclip_usage.py
@@ -0,0 +1,44 @@
+import pyperclip
+
+pyperclip.copy('text to be copied')
+print(pyperclip.paste())
+# text to be copied
+
+print(type(pyperclip.paste()))
+# <class 'str'>
+
+s = pyperclip.paste()
+print(s)
+# text to be copied
+
+pyperclip.copy('')
+print(pyperclip.waitForPaste())
+# some text
+
+print(pyperclip.waitForNewPaste())
+# new text
+
+# pyperclip.waitForNewPaste(5)
+# PyperclipTimeoutException: waitForNewPaste() timed out after 5 seconds.
+
+try:
+    s = pyperclip.waitForNewPaste(5)
+except pyperclip.PyperclipTimeoutException:
+    s = 'No change'
+
+print(s)
+# No change
+
+pyperclip.copy(100)
+print(pyperclip.paste())
+# 100
+
+print(type(pyperclip.paste()))
+# <class 'str'>
+
+i = int(pyperclip.paste())
+print(i)
+# 100
+
+print(type(i))
+# <class 'int'>
