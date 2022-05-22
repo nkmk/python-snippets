6c9f48ff0b59240f2b739ab5981223303c9a1e2f
nkmk <nkmk.on@gmail.com>
2022-05-22 09:46:18 +0900

    update pandas sample()

--- notebook/pandas_random_sort_shuffle.py
+++ notebook/pandas_random_sort_shuffle.py
@@ -10,18 +10,16 @@ print(df)
 # 4    Ellen   24    CA     88
 # 5    Frank   30    NY     57
 
-df_s = df.sample(frac=1)
-print(df_s)
+print(df.sample(frac=1))
 #       name  age state  point
 # 2  Charlie   18    CA     70
+# 1      Bob   42    CA     92
+# 3     Dave   68    TX     70
+# 0    Alice   24    NY     64
 # 5    Frank   30    NY     57
 # 4    Ellen   24    CA     88
-# 1      Bob   42    CA     92
-# 0    Alice   24    NY     64
-# 3     Dave   68    TX     70
 
-df_s = df.sample(frac=1, random_state=0)
-print(df_s)
+print(df.sample(frac=1, random_state=0))
 #       name  age state  point
 # 5    Frank   30    NY     57
 # 2  Charlie   18    CA     70
@@ -30,22 +28,39 @@ print(df_s)
 # 0    Alice   24    NY     64
 # 4    Ellen   24    CA     88
 
-df_s = df.sample(frac=1).reset_index(drop=True)
-print(df_s)
+print(df.sample(frac=1, random_state=0))
+#       name  age state  point
+# 5    Frank   30    NY     57
+# 2  Charlie   18    CA     70
+# 1      Bob   42    CA     92
+# 3     Dave   68    TX     70
+# 0    Alice   24    NY     64
+# 4    Ellen   24    CA     88
+
+print(df.sample(frac=1, ignore_index=True))
 #       name  age state  point
 # 0    Ellen   24    CA     88
-# 1  Charlie   18    CA     70
-# 2    Alice   24    NY     64
+# 1    Frank   30    NY     57
+# 2      Bob   42    CA     92
 # 3     Dave   68    TX     70
-# 4    Frank   30    NY     57
-# 5      Bob   42    CA     92
+# 4    Alice   24    NY     64
+# 5  Charlie   18    CA     70
 
-df = df.sample(frac=1).reset_index(drop=True)
+print(df.sample(frac=1).reset_index(drop=True))
+#       name  age state  point
+# 0      Bob   42    CA     92
+# 1     Dave   68    TX     70
+# 2    Alice   24    NY     64
+# 3  Charlie   18    CA     70
+# 4    Frank   30    NY     57
+# 5    Ellen   24    CA     88
+
+df = df.sample(frac=1)
 print(df)
 #       name  age state  point
-# 0     Dave   68    TX     70
-# 1      Bob   42    CA     92
-# 2  Charlie   18    CA     70
-# 3    Alice   24    NY     64
-# 4    Ellen   24    CA     88
+# 0    Alice   24    NY     64
 # 5    Frank   30    NY     57
+# 1      Bob   42    CA     92
+# 4    Ellen   24    CA     88
+# 3     Dave   68    TX     70
+# 2  Charlie   18    CA     70
--- notebook/pandas_sample.py
+++ notebook/pandas_sample.py
@@ -7,13 +7,41 @@ print(df.shape)
 
 print(df.sample())
 #      sepal_length  sepal_width  petal_length  petal_width    species
-# 108           6.7          2.5           5.8          1.8  virginica
+# 133           6.3          2.8           5.1          1.5  virginica
+
+print(df.sample(axis=1))
+#      petal_width
+# 0            0.2
+# 1            0.2
+# 2            0.2
+# 3            0.2
+# 4            0.2
+# ..           ...
+# 145          2.3
+# 146          1.9
+# 147          2.0
+# 148          2.3
+# 149          1.8
+# 
+# [150 rows x 1 columns]
 
 print(df.sample(n=3))
 #     sepal_length  sepal_width  petal_length  petal_width     species
-# 3            4.6          3.1           1.5          0.2      setosa
-# 1            4.9          3.0           1.4          0.2      setosa
-# 96           5.7          2.9           4.2          1.3  versicolor
+# 29           4.7          3.2           1.6          0.2      setosa
+# 67           5.8          2.7           4.1          1.0  versicolor
+# 18           5.7          3.8           1.7          0.3      setosa
+
+print(df.sample(frac=0.04))
+#      sepal_length  sepal_width  petal_length  petal_width     species
+# 15            5.7          4.4           1.5          0.4      setosa
+# 66            5.6          3.0           4.5          1.5  versicolor
+# 131           7.9          3.8           6.4          2.0   virginica
+# 64            5.6          2.9           3.6          1.3  versicolor
+# 81            5.5          2.4           3.7          1.0  versicolor
+# 137           6.4          3.1           5.5          1.8   virginica
+
+# print(df.sample(n=3, frac=0.04))
+# ValueError: Please enter a value for `frac` OR `n`, not both
 
 print(df.sample(n=3, random_state=0))
 #      sepal_length  sepal_width  petal_length  petal_width     species
@@ -21,33 +49,49 @@ print(df.sample(n=3, random_state=0))
 # 62            6.0          2.2           4.0          1.0  versicolor
 # 33            5.5          4.2           1.4          0.2      setosa
 
-print(df.sample(frac=0.04))
+print(df.sample(n=3, random_state=0))
 #      sepal_length  sepal_width  petal_length  petal_width     species
-# 119           6.0          2.2           5.0          1.5   virginica
-# 97            6.2          2.9           4.3          1.3  versicolor
-# 46            5.1          3.8           1.6          0.2      setosa
-# 137           6.4          3.1           5.5          1.8   virginica
-# 56            6.3          3.3           4.7          1.6  versicolor
+# 114           5.8          2.8           5.1          2.4   virginica
 # 62            6.0          2.2           4.0          1.0  versicolor
+# 33            5.5          4.2           1.4          0.2      setosa
+
+print(df.head(3))
+#    sepal_length  sepal_width  petal_length  petal_width species
+# 0           5.1          3.5           1.4          0.2  setosa
+# 1           4.9          3.0           1.4          0.2  setosa
+# 2           4.7          3.2           1.3          0.2  setosa
 
 print(df.head(3).sample(n=3, replace=True))
 #    sepal_length  sepal_width  petal_length  petal_width species
+# 0           5.1          3.5           1.4          0.2  setosa
+# 0           5.1          3.5           1.4          0.2  setosa
 # 2           4.7          3.2           1.3          0.2  setosa
-# 1           4.9          3.0           1.4          0.2  setosa
-# 1           4.9          3.0           1.4          0.2  setosa
 
 print(df.head(3).sample(n=5, replace=True))
 #    sepal_length  sepal_width  petal_length  petal_width species
 # 1           4.9          3.0           1.4          0.2  setosa
+# 2           4.7          3.2           1.3          0.2  setosa
+# 0           5.1          3.5           1.4          0.2  setosa
 # 0           5.1          3.5           1.4          0.2  setosa
 # 1           4.9          3.0           1.4          0.2  setosa
-# 0           5.1          3.5           1.4          0.2  setosa
-# 0           5.1          3.5           1.4          0.2  setosa
 
-print(df.head().sample(n=2, axis=1))
-#    sepal_width species
-# 0          3.5  setosa
-# 1          3.0  setosa
-# 2          3.2  setosa
-# 3          3.1  setosa
-# 4          3.6  setosa
+print(df.head(3).sample(frac=2, replace=True))
+#    sepal_length  sepal_width  petal_length  petal_width species
+# 2           4.7          3.2           1.3          0.2  setosa
+# 1           4.9          3.0           1.4          0.2  setosa
+# 2           4.7          3.2           1.3          0.2  setosa
+# 2           4.7          3.2           1.3          0.2  setosa
+# 0           5.1          3.5           1.4          0.2  setosa
+# 2           4.7          3.2           1.3          0.2  setosa
+
+print(df.sample(n=3, ignore_index=True))
+#    sepal_length  sepal_width  petal_length  petal_width     species
+# 0           5.2          2.7           3.9          1.4  versicolor
+# 1           6.3          2.5           4.9          1.5  versicolor
+# 2           5.7          3.0           4.2          1.2  versicolor
+
+print(df.sample(n=3).reset_index(drop=True))
+#    sepal_length  sepal_width  petal_length  petal_width    species
+# 0           4.9          3.1           1.5          0.2     setosa
+# 1           7.9          3.8           6.4          2.0  virginica
+# 2           6.3          2.8           5.1          1.5  virginica
