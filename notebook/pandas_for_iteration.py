826be1935d4aa889e64fea7ecc088adcb21ace87
nkmk <nkmk.on@gmail.com>
2022-06-04 16:04:48 +0900

    update pandas for iteration

--- notebook/pandas_for_iteration.py
+++ notebook/pandas_for_iteration.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame({'age': [24, 42], 'state': ['NY', 'CA'], 'point': [64, 92]},
                   index=['Alice', 'Bob'])
@@ -9,131 +10,88 @@ print(df)
 # Bob     42    CA     92
 
 for column_name in df:
-    print(type(column_name))
     print(column_name)
-    print('======\n')
-# <class 'str'>
 # age
-# ======
-# 
-# <class 'str'>
 # state
-# ======
-# 
-# <class 'str'>
 # point
-# ======
-# 
-
-for column_name in df.__iter__():
-    print(type(column_name))
-    print(column_name)
-    print('======\n')
-# <class 'str'>
-# age
-# ======
-# 
-# <class 'str'>
-# state
-# ======
-# 
-# <class 'str'>
-# point
-# ======
-# 
 
 for column_name, item in df.iteritems():
-    print(type(column_name))
     print(column_name)
-    print('~~~~~~')
-    
+    print('------')
     print(type(item))
     print(item)
     print('------')
-    
-    print(item['Alice'])
-    print(item[0])
-    print(item.Alice)
+    print(item[0], item['Alice'], item.Alice)
+    print(item[1], item['Bob'], item.Bob)
     print('======\n')
-# <class 'str'>
 # age
-# ~~~~~~
+# ------
 # <class 'pandas.core.series.Series'>
 # Alice    24
 # Bob      42
 # Name: age, dtype: int64
 # ------
-# 24
-# 24
-# 24
+# 24 24 24
+# 42 42 42
 # ======
 # 
-# <class 'str'>
 # state
-# ~~~~~~
+# ------
 # <class 'pandas.core.series.Series'>
 # Alice    NY
 # Bob      CA
 # Name: state, dtype: object
 # ------
-# NY
-# NY
-# NY
+# NY NY NY
+# CA CA CA
 # ======
 # 
-# <class 'str'>
 # point
-# ~~~~~~
+# ------
 # <class 'pandas.core.series.Series'>
 # Alice    64
 # Bob      92
 # Name: point, dtype: int64
 # ------
-# 64
-# 64
-# 64
+# 64 64 64
+# 92 92 92
 # ======
 # 
 
 for index, row in df.iterrows():
-    print(type(index))
     print(index)
-    print('~~~~~~')
-    
+    print('------')
     print(type(row))
     print(row)
     print('------')
-    
-    print(row['point'])
-    print(row[2])
-    print(row.point)
+    print(row[0], row['age'], row.age)
+    print(row[1], row['state'], row.state)
+    print(row[2], row['point'], row.point)
     print('======\n')
-# <class 'str'>
 # Alice
-# ~~~~~~
+# ------
 # <class 'pandas.core.series.Series'>
 # age      24
 # state    NY
 # point    64
 # Name: Alice, dtype: object
 # ------
-# 64
-# 64
-# 64
+# 24 24 24
+# NY NY NY
+# 64 64 64
 # ======
 # 
-# <class 'str'>
 # Bob
-# ~~~~~~
+# ------
 # <class 'pandas.core.series.Series'>
 # age      42
 # state    CA
 # point    92
 # Name: Bob, dtype: object
 # ------
-# 92
-# 92
-# 92
+# 42 42 42
+# CA CA CA
+# 92 92 92
 # ======
 # 
 
@@ -141,42 +99,43 @@ for row in df.itertuples():
     print(type(row))
     print(row)
     print('------')
-    
-    print(row[3])
-    print(row.point)
+    print(row[0], row.Index)
+    print(row[1], row.age)
+    print(row[2], row.state)
+    print(row[3], row.point)
     print('======\n')
 # <class 'pandas.core.frame.Pandas'>
 # Pandas(Index='Alice', age=24, state='NY', point=64)
 # ------
-# 64
-# 64
+# Alice Alice
+# 24 24
+# NY NY
+# 64 64
 # ======
 # 
 # <class 'pandas.core.frame.Pandas'>
 # Pandas(Index='Bob', age=42, state='CA', point=92)
 # ------
-# 92
-# 92
+# Bob Bob
+# 42 42
+# CA CA
+# 92 92
 # ======
 # 
 
 for row in df.itertuples(name=None):
     print(type(row))
     print(row)
-    print('------')
-    
-    print(row[3])
+    print(row[0], row[1], row[2], row[3])
     print('======\n')
 # <class 'tuple'>
 # ('Alice', 24, 'NY', 64)
-# ------
-# 64
+# Alice 24 NY 64
 # ======
 # 
 # <class 'tuple'>
 # ('Bob', 42, 'CA', 92)
-# ------
-# 92
+# Bob 42 CA 92
 # ======
 # 
 
@@ -230,26 +189,28 @@ print(df)
 # Alice   24    NY     88
 # Bob     42    CA    134
 
+df = pd.DataFrame({'age': [24, 42], 'state': ['NY', 'CA'], 'point': [64, 92]},
+                  index=['Alice', 'Bob'])
 df['point'] += df['age']
 print(df)
 #        age state  point
-# Alice   24    NY    112
-# Bob     42    CA    176
+# Alice   24    NY     88
+# Bob     42    CA    134
 
 df['new'] = df['point'] + df['age'] * 2
 print(df)
 #        age state  point  new
-# Alice   24    NY    112  160
-# Bob     42    CA    176  260
+# Alice   24    NY     88  136
+# Bob     42    CA    134  218
 
-df['age_sqrt'] = pd.np.sqrt(df['age'])
+df['age_sqrt'] = np.sqrt(df['age'])
 print(df)
 #        age state  point  new  age_sqrt
-# Alice   24    NY    112  160  4.898979
-# Bob     42    CA    176  260  6.480741
+# Alice   24    NY     88  136  4.898979
+# Bob     42    CA    134  218  6.480741
 
 df['state_0'] = df['state'].str.lower().str[0]
 print(df)
 #        age state  point  new  age_sqrt state_0
-# Alice   24    NY    112  160  4.898979       n
-# Bob     42    CA    176  260  6.480741       c
+# Alice   24    NY     88  136  4.898979       n
+# Bob     42    CA    134  218  6.480741       c
