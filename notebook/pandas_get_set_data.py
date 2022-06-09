82296107d3266fc5727dcfb67cb231a9b2f13b87
nkmk <nkmk.on@gmail.com>
2022-06-09 20:43:06 +0900

    update pandas at, iat, loc, iloc

--- notebook/pandas_get_set_data.py
+++ notebook/pandas_get_set_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+print(pd.__version__)
+# 1.4.1
+
 df = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=0)
 print(df)
 #          age state  point
@@ -11,34 +14,25 @@ print(df)
 # Ellen     24    CA     88
 # Frank     30    NY     57
 
-print(df.index.values)
-# ['Alice' 'Bob' 'Charlie' 'Dave' 'Ellen' 'Frank']
-
-print(df.columns.values)
-# ['age' 'state' 'point']
-
 print(df.at['Bob', 'age'])
-print(df.at['Dave', 'state'])
 # 42
-# TX
 
 df.at['Bob', 'age'] = 60
 print(df.at['Bob', 'age'])
 # 60
 
 print(df.iat[1, 0])
-print(df.iat[3, 1])
 # 60
-# TX
 
 df.iat[1, 0] = 42
 print(df.iat[1, 0])
 # 42
 
 print(df.loc['Bob', 'age'])
-print(df.iloc[3, 1])
 # 42
-# TX
+
+print(df.iloc[1, 0])
+# 42
 
 df.loc['Bob', 'age'] = 60
 print(df.loc['Bob', 'age'])
@@ -235,49 +229,117 @@ print(type(df.iloc[:, [1]]))
 # Frank      NY
 # <class 'pandas.core.frame.DataFrame'>
 
-df_state = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=2)
-print(df_state)
-#           name  age  point
-# state                     
-# NY       Alice   24     64
-# CA         Bob   42     92
-# CA     Charlie   18     70
-# TX        Dave   68     70
-# CA       Ellen   24     88
-# NY       Frank   30     57
+l_bool = [True, False, False, True, False, True]
 
-print(df_state.index.values)
-# ['NY' 'CA' 'CA' 'TX' 'CA' 'NY']
-
-print(df_state.at['NY', 'age'])
-print(type(df_state.at['NY', 'age']))
-# [24 30]
-# <class 'numpy.ndarray'>
-
-print(df_state.loc['NY', 'age'])
-print(type(df_state.loc['NY', 'age']))
-# state
-# NY    24
-# NY    30
-# Name: age, dtype: int64
-# <class 'pandas.core.series.Series'>
-
-print(df_state.loc['NY', ['age', 'point']])
-print(type(df_state.loc['NY', ['age', 'point']]))
+print(df.loc[l_bool, ['age', 'point']])
 #        age  point
-# state            
-# NY      24     64
-# NY      30     57
+# name             
+# Alice   24     64
+# Dave    40     70
+# Frank   30     57
+
+print(df.iloc[l_bool, [0, 2]])
+#        age  point
+# name             
+# Alice   24     64
+# Dave    40     70
+# Frank   30     57
+
+l_bool_wrong = [True, False, False]
+
+# print(df.loc[l_bool_wrong, ['age', 'point']])
+# IndexError: Boolean index has wrong length: 3 instead of 6
+
+s_bool = pd.Series([True, False, True, True, False, False],
+                   index=reversed(df.index))
+print(s_bool)
+# Frank       True
+# Ellen      False
+# Dave        True
+# Charlie     True
+# Bob        False
+# Alice      False
+# dtype: bool
+
+print(df.loc[s_bool, ['age', 'point']])
+#          age  point
+# name               
+# Charlie   30     70
+# Dave      40     70
+# Frank     30     57
+
+# print(df.iloc[s_bool, [0, 2]])
+# ValueError: Location based indexing can only have [integer,
+# integer slice (START point is INCLUDED, END point is EXCLUDED), listlike of integers, boolean array] types
+
+s_bool_wrong = pd.Series([True, False, False], index=['Frank', 'Ellen', 'Dave'])
+print(s_bool_wrong)
+# Frank     True
+# Ellen    False
+# Dave     False
+# dtype: bool
+
+# print(df.loc[s_bool_wrong, ['age', 'point']])
+# IndexingError: Unalignable boolean Series provided as indexer
+# (index of the boolean Seriesand of the indexed object do not match).
+
+s_bool_wrong2 = pd.Series([True, False, False, True, False, True],
+                          index=['A', 'B', 'C', 'D', 'E', 'F'])
+print(s_bool_wrong2)
+# A     True
+# B    False
+# C    False
+# D     True
+# E    False
+# F     True
+# dtype: bool
+
+# print(df.loc[s_bool_wrong2, ['age', 'point']])
+# IndexingError: Unalignable boolean Series provided as indexer
+# (index of the boolean Series and of the indexed object do not match).
+
+df_duplicated = pd.read_csv('data/src/sample_pandas_normal.csv', index_col=2)
+df_duplicated.columns = ['name', 'age', 'age']
+print(df_duplicated)
+#           name  age  age
+# state                   
+# NY       Alice   24   64
+# CA         Bob   42   92
+# CA     Charlie   18   70
+# TX        Dave   68   70
+# CA       Ellen   24   88
+# NY       Frank   30   57
+
+print(df_duplicated.at['NY', 'age'])
+print(type(df_duplicated.at['NY', 'age']))
+#        age  age
+# state          
+# NY      24   64
+# NY      30   57
 # <class 'pandas.core.frame.DataFrame'>
 
-print(df_state.iat[0, 1])
+print(df_duplicated.loc['NY', 'age'])
+print(type(df_duplicated.loc['NY', 'age']))
+#        age  age
+# state          
+# NY      24   64
+# NY      30   57
+# <class 'pandas.core.frame.DataFrame'>
+
+print(df_duplicated.iat[0, 1])
 # 24
 
-print(df_state.index.is_unique)
+print(df_duplicated.iloc[:2, [0, 1]])
+#         name  age
+# state            
+# NY     Alice   24
+# CA       Bob   42
+
+print(df_duplicated.index.is_unique)
 # False
 
-print(df_state.columns.is_unique)
-# True
+print(df_duplicated.columns.is_unique)
+# False
 
 print(df)
 #          age state  point
@@ -295,23 +357,26 @@ print(df.index[2])
 print(df.columns[1])
 # state
 
+print(df.index[2:4])
+# Index(['Charlie', 'Dave'], dtype='object', name='name')
+
+print(df.columns[[0, 2]])
+# Index(['age', 'point'], dtype='object')
+
 print(df.at[df.index[2], 'age'])
 # 30
 
-print(df.loc[['Alice', 'Dave'], df.columns[1]])
-# name
-# Alice    NY
-# Dave     TX
-# Name: state, dtype: object
+print(df.loc[['Alice', 'Dave'], df.columns[[0, 2]]])
+#        age  point
+# name             
+# Alice   24     64
+# Dave    40     70
 
 print(df['age'][2])
 # 30
 
-print(df.age[2])
-# 30
-
-print(df.loc[['Alice', 'Dave']].iloc[:, 1])
-# name
-# Alice    NY
-# Dave     TX
-# Name: state, dtype: object
+print(df.loc[['Alice', 'Dave']].iloc[:, [0, 2]])
+#        age  point
+# name             
+# Alice   24     64
+# Dave    40     70
