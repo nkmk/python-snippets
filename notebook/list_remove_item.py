b92c5e0940b6c0b27ae9b4e5cee1c74fac8b1596
nkmk <nkmk.on@gmail.com>
2022-08-28 14:10:56 +0900

    update list_remove_item

--- notebook/list_remove_item.py
+++ notebook/list_remove_item.py
@@ -1,45 +1,39 @@
-l = list(range(10))
-print(l)
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+l = [0, 1, 2]
 
 l.clear()
 print(l)
 # []
 
-l = list(range(10))
-print(l)
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+l = [0, 10, 20, 30, 40, 50]
 
 print(l.pop(0))
 # 0
 
 print(l)
-# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [10, 20, 30, 40, 50]
 
 print(l.pop(3))
-# 4
+# 40
 
 print(l)
-# [1, 2, 3, 5, 6, 7, 8, 9]
+# [10, 20, 30, 50]
 
 print(l.pop(-2))
-# 8
+# 30
 
 print(l)
-# [1, 2, 3, 5, 6, 7, 9]
+# [10, 20, 50]
 
 print(l.pop())
-# 9
+# 50
 
 print(l)
-# [1, 2, 3, 5, 6, 7]
+# [10, 20]
 
 # print(l.pop(100))
 # IndexError: pop index out of range
 
 l = ['Alice', 'Bob', 'Charlie', 'Bob', 'Dave']
-print(l)
-# ['Alice', 'Bob', 'Charlie', 'Bob', 'Dave']
 
 l.remove('Alice')
 print(l)
@@ -52,9 +46,50 @@ print(l)
 # l.remove('xxx')
 # ValueError: list.remove(x): x not in list
 
-l = list(range(10))
+l = [0, 10, 20, 30, 40, 50]
+
+del l[0]
 print(l)
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [10, 20, 30, 40, 50]
+
+del l[3]
+print(l)
+# [10, 20, 30, 50]
+
+del l[-1]
+print(l)
+# [10, 20, 30]
+
+del l[-2]
+print(l)
+# [10, 30]
+
+l = [0, 10, 20, 30, 40, 50]
+del l[2:5]
+print(l)
+# [0, 10, 50]
+
+l = [0, 10, 20, 30, 40, 50]
+del l[:3]
+print(l)
+# [30, 40, 50]
+
+l = [0, 10, 20, 30, 40, 50]
+del l[-2:]
+print(l)
+# [0, 10, 20, 30]
+
+l = [0, 10, 20, 30, 40, 50]
+del l[:]
+print(l)
+# []
+
+l = [0, 10, 20, 30, 40, 50]
+del l[::2]
+print(l)
+# [10, 30, 50]
+
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 print([i for i in l if i % 2 == 0])
 # [0, 2, 4, 6, 8]
@@ -76,58 +111,4 @@ print([s for s in l if s.endswith('e')])
 # ['Alice', 'Charlie']
 
 print(list(set(l)))
-# ['David', 'Alice', 'Charlie', 'Bob']
-
-l = list(range(10))
-print(l)
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-del l[0]
-print(l)
-# [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-del l[-1]
-print(l)
-# [1, 2, 3, 4, 5, 6, 7, 8]
-
-del l[6]
-print(l)
-# [1, 2, 3, 4, 5, 6, 8]
-
-l = list(range(10))
-print(l)
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-del l[2:5]
-print(l)
-# [0, 1, 5, 6, 7, 8, 9]
-
-l = list(range(10))
-del l[:3]
-print(l)
-# [3, 4, 5, 6, 7, 8, 9]
-
-l = list(range(10))
-del l[4:]
-print(l)
-# [0, 1, 2, 3]
-
-l = list(range(10))
-del l[-3:]
-print(l)
-# [0, 1, 2, 3, 4, 5, 6]
-
-l = list(range(10))
-del l[:]
-print(l)
-# []
-
-l = list(range(10))
-del l[2:8:2]
-print(l)
-# [0, 1, 3, 5, 7, 8, 9]
-
-l = list(range(10))
-del l[::3]
-print(l)
-# [1, 2, 4, 5, 7, 8]
+# ['Charlie', 'Bob', 'David', 'Alice']
