4ad8b6db66ce6bd6eb6e1251dd923d662e30111c
nkmk <nkmk.on@gmail.com>
2022-12-04 14:15:28 +0900

    update rjust, center, ljust

--- notebook/rjust_center_ljust.py
+++ notebook/rjust_center_ljust.py
@@ -1,31 +1,26 @@
 s = 'abc'
 
-s_rjust = s.rjust(8)
-
-print(s_rjust)
+print(s.rjust(8))
 #      abc
 
-print(type(s_rjust))
+print(type(s.rjust(8)))
 # <class 'str'>
 
 print(s.rjust(2))
 # abc
 
-print(s.rjust(8, '+'))
-# +++++abc
+print(s.rjust(8, '*'))
+# *****abc
 
-print(s.rjust(8, '漢'))
-# 漢漢漢漢漢abc
-
-# print(s.rjust(8, 'xyz'))
+# print(s.rjust(8, 'ab'))
 # TypeError: The fill character must be exactly one character long
 
-s_n = '-123'
+s = '-123'
 
-print(s_n.rjust(8, '0'))
+print(s.rjust(8, '0'))
 # 0000-123
 
-print(s_n.zfill(8))
+print(s.zfill(8))
 # -0000123
 
 s = 'abc'
@@ -33,58 +28,58 @@ s = 'abc'
 print(s.center(8))
 #   abc   
 
-print(s.center(8, '+'))
-# ++abc+++
+print(s.center(8, '*'))
+# **abc***
 
-print(s.center(9, '+'))
-# +++abc+++
+print(s.center(9, '*'))
+# ***abc***
 
-print(s.center(10, '+'))
-# +++abc++++
+print(s.center(10, '*'))
+# ***abc****
 
 s = 'abc'
 
 print(s.ljust(8))
 # abc     
 
-print(s.ljust(8, '+'))
-# abc+++++
+print(s.ljust(8, '*'))
+# abc*****
 
-n = 123
+i = 123
 
-print(type(n))
+print(type(i))
 # <class 'int'>
 
-print(str(n).rjust(8, '@'))
-# @@@@@123
+print(str(i).rjust(8, '*'))
+# *****123
 
-print(str(n).center(8, '@'))
-# @@123@@@
+print(str(i).center(8, '*'))
+# **123***
 
-print(str(n).ljust(8, '@'))
-# 123@@@@@
+print(str(i).ljust(8, '*'))
+# 123*****
 
 s = 'abc'
 
-print('right : {:@>8}'.format(s))
-# right : @@@@@abc
+print('right : {:*>8}'.format(s))
+# right : *****abc
 
-print('center: {:@^8}'.format(s))
-# center: @@abc@@@
+print('center: {:*^8}'.format(s))
+# center: **abc***
 
-print('left  : {:@<8}'.format(s))
-# left  : abc@@@@@
+print('left  : {:*<8}'.format(s))
+# left  : abc*****
 
-n = 255
+i = 255
 
-print('right : {:08}'.format(n))
+print('right : {:08}'.format(i))
 # right : 00000255
 
-print('right : {:08x}'.format(n))
+print('right : {:08x}'.format(i))
 # right : 000000ff
 
-print(f'right : {n:08}')
+print(f'right : {i:08}')
 # right : 00000255
 
-print(f'right : {n:08x}')
+print(f'right : {i:08x}')
 # right : 000000ff
