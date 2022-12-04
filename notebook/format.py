b0b1203c208db1e8a6d85f414c89afdd82a4e79d
nkmk <nkmk.on@gmail.com>
2022-12-04 14:14:55 +0900

    update format

--- notebook/format.py
+++ notebook/format.py
@@ -1,29 +1,37 @@
-s = format(255, '04x')
-print(s)
-print(type(s))
-# 00ff
+i = 255
+
+print(format(i, '#04x'))
+print(type(format(i, '#04x')))
+# 0xff
 # <class 'str'>
 
-print(format('center', '*^16'))
-# *****center*****
+s = 'abc'
 
-s = '{:04x}'.format(255)
-print(s)
-print(type(s))
-# 00ff
+print(format(s, '*^10'))
+# ***abc****
+
+print('{:#04x}'.format(i))
+print(type('{:#04x}'.format(i)))
+# 0xff
 # <class 'str'>
 
-print('{:*^16}'.format('center'))
-# *****center*****
+print('{:*^10}'.format(s))
+# ***abc****
 
-print('{}-{}-{}'.format('100', '二百', 300))
-# 100-二百-300
+print('{}-{}-{}'.format(255, 'abc', 1.23))
+# 255-abc-1.23
 
-print('{0}-{1}-{0}'.format('foo', 'bar'))
-# foo-bar-foo
+print('{}-{}'.format(255, 'abc', 1.23))
+# 255-abc
 
-print('{year}年{month}月{day}日'.format(year=2018, month=1, day=11))
-# 2018年1月11日
+# print('{}-{}-{}-{}'.format(255, 'abc', 1.23))
+# IndexError: Replacement index 3 out of range for positional args tuple
+
+print('{0}-{1}-{0}'.format(255, 'abc'))
+# 255-abc-255
+
+print('{name} is {age} years old.'.format(name='Alice', age=20))
+# Alice is 20 years old.
 
 l = ['one', 'two', 'three']
 print('{0[0]}-{0[1]}-{0[2]}'.format(l))
@@ -46,11 +54,8 @@ print('{name} is {age} years old.'.format(**d))
 print('{{}}-{num}-{{{num}}}'.format(num=100))
 # {}-100-{100}
 
-print('{num:x}'.format(num=255))
-# ff
-
-print('{year}年{month:02}月{day:02}日'.format(year=2018, month=1, day=11))
-# 2018年01月11日
+print('{num} = {num:#x} = {num:#b}'.format(num=255))
+# 255 = 0xff = 0b11111111
 
 print('left  : {:<10}'.format(100))
 print('center: {:^10}'.format(100))
@@ -60,38 +65,22 @@ print('right : {:>10}'.format(100))
 # right :        100
 
 print('left  : {:*<10}'.format(100))
-print('center: {:a^10}'.format(100))
-print('right : {:鬼>10}'.format(100))
+print('center: {:*^10}'.format(100))
+print('right : {:*>10}'.format(100))
 # left  : 100*******
-# center: aaa100aaaa
-# right : 鬼鬼鬼鬼鬼鬼鬼100
+# center: ***100****
+# right : *******100
 
 print('sign: {:0>10}'.format(-100))
 print('sign: {:0=10}'.format(-100))
-print('sign: {:0=+10}'.format(100))
 # sign: 000000-100
 # sign: -000000100
-# sign: +000000100
 
 # print('sign: {:0=10}'.format('-100'))
 # ValueError: '=' alignment not allowed in string format specifier
 
 print('sign: {:0=10}'.format(int('-100')))
 # sign: -000000100
-
-print('left  : {:*<10}'.format(1.23))
-print('center: {:a^10}'.format(1.23))
-print('right : {:鬼>10}'.format(1.23))
-# left  : 1.23******
-# center: aaa1.23aaa
-# right : 鬼鬼鬼鬼鬼鬼1.23
-
-print('sign: {:0>10}'.format(-1.23))
-print('sign: {:0=10}'.format(-1.23))
-print('sign: {:0=+10}'.format(1.23))
-# sign: 00000-1.23
-# sign: -000001.23
-# sign: +000001.23
 
 print('zero padding: {:0=10}'.format(100))
 print('zero padding: {:010}'.format(100))
@@ -136,60 +125,64 @@ print('sign: {: 06}'.format(-100))
 # sign:  00100
 # sign: -00100
 
-print('sign: {:_>6}'.format(100))
-print('sign: {:_>6}'.format(-100))
-# sign: ___100
-# sign: __-100
+print('sign: {:*>6}'.format(100))
+print('sign: {:*>6}'.format(-100))
+# sign: ***100
+# sign: **-100
 
-print('sign: {:_>+6}'.format(100))
-print('sign: {:_>+6}'.format(-100))
-# sign: __+100
-# sign: __-100
+print('sign: {:*>+6}'.format(100))
+print('sign: {:*>+6}'.format(-100))
+# sign: **+100
+# sign: **-100
 
-print('sign: {:_> 6}'.format(100))
-print('sign: {:_> 6}'.format(-100))
-# sign: __ 100
-# sign: __-100
+print('sign: {:*> 6}'.format(100))
+print('sign: {:*> 6}'.format(-100))
+# sign: ** 100
+# sign: **-100
 
 print('{:,}'.format(100000000))
-# 100,000,000
-
 print('{:_}'.format(100000000))
+# 100,000,000
 # 100_000_000
 
 print('{:,}'.format(1234.56789))
+print('{:_}'.format(1234.56789))
 # 1,234.56789
+# 1_234.56789
 
 print('bin: {:b}'.format(255))
 print('oct: {:o}'.format(255))
-print('dec: {:d}'.format(255))
 print('hex: {:x}'.format(255))
 print('HEX: {:X}'.format(255))
 # bin: 11111111
 # oct: 377
-# dec: 255
 # hex: ff
 # HEX: FF
 
 print('bin: {:08b}'.format(255))
 print('oct: {:08o}'.format(255))
-print('dec: {:08d}'.format(255))
 print('hex: {:08x}'.format(255))
 print('HEX: {:08X}'.format(255))
 # bin: 11111111
 # oct: 00000377
-# dec: 00000255
 # hex: 000000ff
 # HEX: 000000FF
 
+print('bin: {:#b}'.format(255))
+print('oct: {:#o}'.format(255))
+print('hex: {:#x}'.format(255))
+print('HEX: {:#X}'.format(255))
+# bin: 0b11111111
+# oct: 0o377
+# hex: 0xff
+# HEX: 0XFF
+
 print('bin: {:#010b}'.format(255))
 print('oct: {:#010o}'.format(255))
-print('dec: {:#010d}'.format(255))
 print('hex: {:#010x}'.format(255))
 print('HEX: {:#010X}'.format(255))
 # bin: 0b11111111
 # oct: 0o00000377
-# dec: 0000000255
 # hex: 0x000000ff
 # HEX: 0X000000FF
 
@@ -206,19 +199,21 @@ print('hex: {:#011_x}'.format(255))
 print('hex: {:08x}'.format(int('255')))
 # hex: 000000ff
 
+print('{}'.format(0.00001234))
+print('{}'.format(12340000000000000.0))
+print('{:f}'.format(0.00001234))
+print('{:f}'.format(12340000000000000.0))
+# 1.234e-05
+# 1.234e+16
+# 0.000012
+# 12340000000000000.000000
+
 print('{:.2f}'.format(123.456))
 print('{:.5f}'.format(123.456))
 print('{:.3f}'.format(0.0001234))
 # 123.46
 # 123.45600
 # 0.000
-
-print('{:>12.5f}'.format(123.456))
-print('{:012.5f}'.format(123.456))
-print('{:06.5f}'.format(123.456))
-#    123.45600
-# 000123.45600
-# 123.45600
 
 print('{:.0f}'.format(0.4))
 print('{:.0f}'.format(0.5))
@@ -227,17 +222,10 @@ print('{:.0f}'.format(0.6))
 # 0
 # 1
 
-print('{}'.format(0.0001234))
-print('{}'.format(0.00001234))
-# 0.0001234
-# 1.234e-05
-
-print('{}'.format(1234000000000000.0))
-print('{}'.format(12340000000000000.0))
-print('{}'.format(12340000000000000000000000))
-# 1234000000000000.0
-# 1.234e+16
-# 12340000000000000000000000
+print('{:.5f}'.format(10e1000))
+print('{:.5F}'.format(10e1000))
+# inf
+# INF
 
 print('{:e}'.format(0.0001234))
 print('{:E}'.format(0.0001234))
@@ -245,19 +233,24 @@ print('{:E}'.format(0.0001234))
 # 1.234000E-04
 
 print('{:.5e}'.format(0.0001234))
-print('{:.2E}'.format(0.0001234))
+print('{:.2e}'.format(0.0001234))
 # 1.23400e-04
-# 1.23E-04
+# 1.23e-04
 
 print('{:.5e}'.format(987.65))
-print('{:.2E}'.format(987.65))
+print('{:.2e}'.format(987.65))
 # 9.87650e+02
-# 9.88E+02
+# 9.88e+02
 
 print('{:>12.5e}'.format(987.65))
-print('{:012.2E}'.format(987.65))
+print('{:012.2e}'.format(987.65))
 #  9.87650e+02
-# 00009.88E+02
+# 00009.88e+02
+
+print('{:g}'.format(0.0001234))
+print('{:g}'.format(0.00001234))
+# 0.0001234
+# 1.234e-05
 
 print('{:.2g}'.format(123.456))
 print('{:.3g}'.format(123.456))
@@ -268,6 +261,15 @@ print('{:.3g}'.format(0.0001234))
 # 123.456
 # 0.000123
 
+print('{:#.2g}'.format(123.456))
+print('{:#.3g}'.format(123.456))
+print('{:#.8g}'.format(123.456))
+print('{:#.3g}'.format(0.0001234))
+# 1.2e+02
+# 123.
+# 123.45600
+# 0.000123
+
 print('{:.2}'.format(123.456))
 print('{:.3}'.format(123.456))
 print('{:.8}'.format(123.456))
@@ -275,6 +277,15 @@ print('{:.3}'.format(0.0001234))
 # 1.2e+02
 # 1.23e+02
 # 123.456
+# 0.000123
+
+print('{:#.2}'.format(123.456))
+print('{:#.3}'.format(123.456))
+print('{:#.8}'.format(123.456))
+print('{:#.3}'.format(0.0001234))
+# 1.2e+02
+# 1.23e+02
+# 123.45600
 # 0.000123
 
 print('{:.3f}'.format(123.456))
@@ -302,6 +313,15 @@ print('{:.4e}'.format(12))
 # 1.2345e-05
 # 1.2000e+01
 
+print('{:.2g}'.format(123.456))
+print('{:.2g}'.format(10e1000))
+print('{:.2G}'.format(123.456))
+print('{:.2G}'.format(10e1000))
+# 1.2e+02
+# inf
+# 1.2E+02
+# INF
+
 print('{:%}'.format(0.12345))
 print('{:.2%}'.format(0.12345))
 # 12.345000%
@@ -312,7 +332,20 @@ print('{:.2%}'.format(10))
 # 1000.000000%
 # 1000.00%
 
-print('{:>7.2%}'.format(0.12345))
-print('{:07.2%}'.format(0.12345))
-#  12.35%
-# 012.35%
+print('{:>10.2%}'.format(0.12345))
+print('{:010.2%}'.format(0.12345))
+#     12.35%
+# 000012.35%
+
+import datetime
+
+dt = datetime.datetime(2020, 1, 5, 20, 15, 30)
+
+print('datetime: {}'.format(dt))
+# datetime: 2020-01-05 20:15:30
+
+print('datetime: {:%A, %m/%d/%Y %I:%M:%S %p}'.format(dt))
+# datetime: Sunday, 01/05/2020 08:15:30 PM
+
+print('datetime: {}'.format(dt.isoformat()))
+# datetime: 2020-01-05T20:15:30
