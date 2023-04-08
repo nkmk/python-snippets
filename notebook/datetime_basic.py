407858dae286e9a50e5ec11d407a3ee749f09ae9
nkmk <nkmk.on@gmail.com>
2023-04-08 23:32:32 +0900

    update datetime basic

--- notebook/datetime_basic.py
+++ notebook/datetime_basic.py
@@ -2,93 +2,92 @@ import datetime
 
 dt_now = datetime.datetime.now()
 print(dt_now)
-# 2018-02-02 18:31:13.271231
+# 2023-04-08 23:08:13.873520
 
 print(type(dt_now))
 # <class 'datetime.datetime'>
 
 print(dt_now.year)
-# 2018
+# 2023
 
-print(dt_now.hour)
-# 18
+print(type(dt_now.year))
+# <class 'int'>
 
-dt = datetime.datetime(2018, 2, 1, 12, 15, 30, 2000)
-print(dt)
-# 2018-02-01 12:15:30.002000
+print(datetime.datetime(2023, 4, 1, 20, 15, 30, 2000))
+# 2023-04-01 20:15:30.002000
 
-print(dt.minute)
-# 15
+print(datetime.datetime(2023, 4, 1))
+# 2023-04-01 00:00:00
 
-print(dt.microsecond)
-# 2000
+dt = datetime.datetime(2023, 4, 1, 20, 15, 30, 2000)
 
-dt = datetime.datetime(2018, 2, 1)
-print(dt)
-# 2018-02-01 00:00:00
-
-print(dt.minute)
-# 0
-
-print(dt_now)
-print(type(dt_now))
-# 2018-02-02 18:31:13.271231
-# <class 'datetime.datetime'>
-
-print(dt_now.date())
-print(type(dt_now.date()))
-# 2018-02-02
-# <class 'datetime.date'>
-
-d_today = datetime.date.today()
-print(d_today)
-# 2018-02-02
-
-print(type(d_today))
-# <class 'datetime.date'>
-
-print(d_today.year)
-# 2018
-
-d = datetime.date(2018, 2, 1)
+d = dt.date()
 print(d)
-# 2018-02-01
+# 2023-04-01
 
-print(d.month)
-# 2
+print(type(d))
+# <class 'datetime.date'>
 
-t = datetime.time(12, 15, 30, 2000)
+t = dt.time()
 print(t)
-# 12:15:30.002000
+# 20:15:30.002000
 
 print(type(t))
 # <class 'datetime.time'>
 
-print(t.hour)
-# 12
+print(datetime.datetime.combine(d, t))
+# 2023-04-01 20:15:30.002000
 
-t = datetime.time()
-print(t)
+d_today = datetime.date.today()
+print(d_today)
+# 2023-04-08
+
+print(type(d_today))
+# <class 'datetime.date'>
+
+print(d_today.month)
+# 4
+
+print(datetime.date(2023, 4, 1))
+# 2023-04-01
+
+t_now = datetime.datetime.now().time()
+print(t_now)
+# 23:08:13.919031
+
+print(type(t_now))
+# <class 'datetime.time'>
+
+print(t_now.microsecond)
+# 919031
+
+print(datetime.time(20, 15, 30, 2000))
+# 20:15:30.002000
+
+print(datetime.time())
 # 00:00:00
 
-td = dt_now - dt
+dt1 = datetime.datetime(2023, 4, 1, 20, 15, 30, 2000)
+dt2 = datetime.datetime(2023, 7, 1, 10, 45, 15, 100)
+
+td = dt2 - dt1
 print(td)
-# 1 day, 18:31:13.271231
+# 90 days, 14:29:44.998100
 
 print(type(td))
 # <class 'datetime.timedelta'>
 
 print(td.days)
-# 1
+# 90
 
 print(td.seconds)
-# 66673
+# 52184
 
 print(td.microseconds)
-# 271231
+# 998100
 
 print(td.total_seconds())
-# 153073.271231
+# 7828184.9981
 
 td_1w = datetime.timedelta(weeks=1)
 print(td_1w)
@@ -97,107 +96,73 @@ print(td_1w)
 print(td_1w.days)
 # 7
 
-d_1w = d_today - td_1w
-print(d_1w)
-# 2018-01-26
+dt = datetime.datetime(2023, 4, 1, 20, 15, 30, 2000)
+print(dt)
+# 2023-04-01 20:15:30.002000
 
-td_10d = datetime.timedelta(days=10)
-print(td_10d)
-# 10 days, 0:00:00
+print(dt - datetime.timedelta(weeks=1))
+# 2023-03-25 20:15:30.002000
 
-dt_10d = dt_now + td_10d
-print(dt_10d)
-# 2018-02-12 18:31:13.271231
+print(dt + datetime.timedelta(days=10))
+# 2023-04-11 20:15:30.002000
 
-td_50m = datetime.timedelta(minutes=50)
-print(td_50m)
-# 0:50:00
+print(dt + datetime.timedelta(minutes=50))
+# 2023-04-01 21:05:30.002000
 
-print(td_50m.seconds)
-# 3000
-
-dt_50m = dt_now + td_50m
-print(dt_50m)
-# 2018-02-02 19:21:13.271231
-
-d_target = datetime.date(2020, 7, 24)
-td = d_target - d_today
+d_target = datetime.date(2023, 5, 1)
+td = d_target - dt.date()
 print(td)
-# 903 days, 0:00:00
+# 30 days, 0:00:00
 
 print(td.days)
-# 903
+# 30
 
-print(dt_now.strftime('%Y-%m-%d %H:%M:%S'))
-# 2018-02-02 18:31:13
+dt = datetime.datetime(2023, 4, 1, 20, 15, 30, 2000)
+print(dt.strftime('%Y/%m/%d %H:%M:%S.%f'))
+# 2023/04/01 20:15:30.002000
 
-print(d_today.strftime('%y%m%d'))
-# 180202
+print(dt.strftime('%A, %B %d, %Y'))
+# Saturday, April 01, 2023
 
-print(d_today.strftime('%A, %B %d, %Y'))
-# Friday, February 02, 2018
+d = dt.date()
+print(d.strftime('%Y/%m/%d %H:%M:%S.%f'))
+# 2023/04/01 00:00:00.000000
 
-print(d_today.strftime('%Y年%m月%d日'))
-# 2018年02月02日
+t = dt.time()
+print(t.strftime('%Y/%m/%d %H:%M:%S.%f'))
+# 1900/01/01 20:15:30.002000
 
-print('日番号（1年の何日目か / 正月が001）:', d_today.strftime('%j'))
-print('週番号（週の始まりは日曜日 / 正月が00）:', d_today.strftime('%U'))
-print('週番号（週の始まりは月曜日 / 正月が00）:', d_today.strftime('%W'))
-# 日番号（1年の何日目か / 正月が001）: 033
-# 週番号（週の始まりは日曜日 / 正月が00）: 04
-# 週番号（週の始まりは月曜日 / 正月が00）: 05
-
-week_num_mon = int(d_today.strftime('%W'))
-print(week_num_mon)
-print(type(week_num_mon))
-# 5
-# <class 'int'>
-
-d = datetime.date(2018, 2, 1)
+d = datetime.date(2023, 4, 1)
 td = datetime.timedelta(weeks=2)
-n = 8
-f = '%Y年%m月%d日'
-
-l = []
-
-for i in range(n):
-    l.append((d + i * td).strftime(f))
-
-print(l)
-# ['2018年02月01日', '2018年02月15日', '2018年03月01日', '2018年03月15日', '2018年03月29日', '2018年04月12日', '2018年04月26日', '2018年05月10日']
-
-print('\n'.join(l))
-# 2018年02月01日
-# 2018年02月15日
-# 2018年03月01日
-# 2018年03月15日
-# 2018年03月29日
-# 2018年04月12日
-# 2018年04月26日
-# 2018年05月10日
+n = 4
+f = '%Y/%m/%d'
 
 l = [(d + i * td).strftime(f) for i in range(n)]
 print(l)
-# ['2018年02月01日', '2018年02月15日', '2018年03月01日', '2018年03月15日', '2018年03月29日', '2018年04月12日', '2018年04月26日', '2018年05月10日']
+# ['2023/04/01', '2023/04/15', '2023/04/29', '2023/05/13']
 
-date_str = '2018/2/1 12:30'
-date_dt = datetime.datetime.strptime(date_str, '%Y/%m/%d %H:%M')
-print(date_dt)
-# 2018-02-01 12:30:00
+print('\n'.join(l))
+# 2023/04/01
+# 2023/04/15
+# 2023/04/29
+# 2023/05/13
 
-print(type(date_dt))
+s = '2023/4/1 20:30'
+s_format = '%Y/%m/%d %H:%M'
+dt = datetime.datetime.strptime(s, s_format)
+print(dt)
+# 2023-04-01 20:30:00
+
+print(type(dt))
 # <class 'datetime.datetime'>
 
-print(date_dt.strftime('%Y年%m月%d日 %H時%M分'))
-# 2018年02月01日 12時30分
+print(dt.strftime('%Y%m%d_%H%M'))
+# 20230401_2030
 
-date_str = '2018年2月1日'
-date_format = '%Y年%m月%d日'
-td_10_d = datetime.timedelta(days=10)
+s = '2023/4/1'
+s_format = '%Y/%m/%d'
+td_10d = datetime.timedelta(days=10)
 
-date_dt = datetime.datetime.strptime(date_str, date_format)
-date_dt_new = date_dt - td_10_d
-date_str_new = date_dt_new.strftime(date_format)
-
-print(date_str_new)
-# 2018年01月22日
+dt = datetime.datetime.strptime(s, s_format) - td_10d
+print(dt.strftime(s_format))
+# 2023/03/22
