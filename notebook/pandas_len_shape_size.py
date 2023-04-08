0cc75c1fc4e04f90eaa05be52b1efaf9862c8af0
nkmk <nkmk.on@gmail.com>
2023-04-08 15:06:07 +0900

    update pandas len shape size

--- notebook/pandas_len_shape_size.py
+++ notebook/pandas_len_shape_size.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('data/src/titanic_train.csv')
+print(pd.__version__)
+# 2.0.0
 
+df = pd.read_csv('data/src/titanic_train.csv')
 print(df.head())
-#    PassengerId  Survived  Pclass  \
-# 0            1         0       3   
+#    PassengerId  Survived  Pclass   
+# 0            1         0       3  \
 # 1            2         1       1   
 # 2            3         1       3   
 # 3            4         1       1   
 # 4            5         0       3   
 # 
-#                                                 Name     Sex   Age  SibSp  \
-# 0                            Braund, Mr. Owen Harris    male  22.0      1   
+#                                                 Name     Sex   Age  SibSp   
+# 0                            Braund, Mr. Owen Harris    male  22.0      1  \
 # 1  Cumings, Mrs. John Bradley (Florence Briggs Th...  female  38.0      1   
 # 2                             Heikkinen, Miss. Laina  female  26.0      0   
 # 3       Futrelle, Mrs. Jacques Heath (Lily May Peel)  female  35.0      1   
@@ -28,26 +30,22 @@ df.info()
 # <class 'pandas.core.frame.DataFrame'>
 # RangeIndex: 891 entries, 0 to 890
 # Data columns (total 12 columns):
-# PassengerId    891 non-null int64
-# Survived       891 non-null int64
-# Pclass         891 non-null int64
-# Name           891 non-null object
-# Sex            891 non-null object
-# Age            714 non-null float64
-# SibSp          891 non-null int64
-# Parch          891 non-null int64
-# Ticket         891 non-null object
-# Fare           891 non-null float64
-# Cabin          204 non-null object
-# Embarked       889 non-null object
+#  #   Column       Non-Null Count  Dtype  
+# ---  ------       --------------  -----  
+#  0   PassengerId  891 non-null    int64  
+#  1   Survived     891 non-null    int64  
+#  2   Pclass       891 non-null    int64  
+#  3   Name         891 non-null    object 
+#  4   Sex          891 non-null    object 
+#  5   Age          714 non-null    float64
+#  6   SibSp        891 non-null    int64  
+#  7   Parch        891 non-null    int64  
+#  8   Ticket       891 non-null    object 
+#  9   Fare         891 non-null    float64
+#  10  Cabin        204 non-null    object 
+#  11  Embarked     889 non-null    object 
 # dtypes: float64(2), int64(5), object(5)
-# memory usage: 83.6+ KB
-
-print(len(df))
-# 891
-
-print(len(df.columns))
-# 12
+# memory usage: 83.7+ KB
 
 print(df.shape)
 # (891, 12)
@@ -65,11 +63,31 @@ print(row)
 print(col)
 # 12
 
+print(len(df))
+# 891
+
+print(len(df.columns))
+# 12
+
 print(df.size)
 # 10692
 
 print(df.shape[0] * df.shape[1])
 # 10692
+
+df_multiindex = df.set_index(['Sex', 'Pclass', 'Embarked', 'PassengerId'])
+
+print(df_multiindex.shape)
+# (891, 8)
+
+print(len(df_multiindex))
+# 891
+
+print(len(df_multiindex.columns))
+# 8
+
+print(df_multiindex.size)
+# 7128
 
 s = df['PassengerId']
 print(s.head())
@@ -89,16 +107,15 @@ print(s.size)
 print(s.shape)
 # (891,)
 
-df_multiindex = df.set_index(['Sex', 'Pclass', 'Embarked', 'PassengerId'])
+print(type(s.shape))
+# <class 'tuple'>
 
-print(len(df_multiindex))
-# 891
-
-print(len(df_multiindex.columns))
-# 8
-
-print(df_multiindex.shape)
-# (891, 8)
-
-print(df_multiindex.size)
-# 7128
+s.info()
+# <class 'pandas.core.series.Series'>
+# RangeIndex: 891 entries, 0 to 890
+# Series name: PassengerId
+# Non-Null Count  Dtype
+# --------------  -----
+# 891 non-null    int64
+# dtypes: int64(1)
+# memory usage: 7.1 KB
