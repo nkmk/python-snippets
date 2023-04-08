7adc9a396986a5684386fce87ef8ad496c1da1e0
nkmk <nkmk.on@gmail.com>
2023-04-08 15:05:51 +0900

    update os.path.exists, isfile, isdir

--- notebook/os_path_exists_isfile_isdir.py
+++ notebook/os_path_exists_isfile_isdir.py
@@ -1,30 +1,47 @@
 import os
 
-filepath = './data/temp/dir/file.txt'
-dirpath = './data/temp/dir'
+file_path = './data/temp/dir/file.txt'
+dir_path = './data/temp/dir'
+non_existent_path = './non_exist'
 
-print(os.path.exists(filepath))
+os.makedirs(dir_path, exist_ok=True)
+with open(file_path, mode='w') as f:
+    f.write('')
+
+print(os.path.isfile(file_path))
 # True
 
-print(os.path.exists(dirpath))
-# True
-
-print(os.path.isfile(filepath))
-# True
-
-print(os.path.isfile(dirpath))
+print(os.path.isfile(dir_path))
 # False
 
-print(os.path.isdir(filepath))
+print(os.path.isfile(non_existent_path))
 # False
 
-print(os.path.isdir(dirpath))
+print(os.path.isdir(file_path))
+# False
+
+print(os.path.isdir(dir_path))
 # True
 
-dirpath_with_sep = './data/temp/dir/'
-
-print(os.path.isfile(dirpath_with_sep))
+print(os.path.isdir(non_existent_path))
 # False
 
-print(os.path.isdir(dirpath_with_sep))
+print(os.path.exists(file_path))
+# True
+
+print(os.path.exists(dir_path))
+# True
+
+print(os.path.exists(non_existent_path))
+# False
+
+dir_path_with_sep = './data/temp/dir/'
+
+print(os.path.exists(dir_path_with_sep))
+# True
+
+print(os.path.isfile(dir_path_with_sep))
+# False
+
+print(os.path.isdir(dir_path_with_sep))
 # True
