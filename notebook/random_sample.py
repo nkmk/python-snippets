8451f39a11a8ee4ec3d2649cf512a1bf1e8532f1
nkmk <nkmk.on@gmail.com>
2023-05-11 23:00:28 +0900

    update random.sample

--- notebook/random_sample.py
+++ notebook/random_sample.py
@@ -3,13 +3,13 @@ import random
 l = [0, 1, 2, 3, 4]
 
 print(random.sample(l, 3))
-# [2, 4, 0]
+# [3, 1, 0]
 
 print(type(random.sample(l, 3)))
 # <class 'list'>
 
 print(random.sample(l, 1))
-# [3]
+# [1]
 
 print(random.sample(l, 0))
 # []
@@ -18,24 +18,24 @@ print(random.sample(l, 0))
 # ValueError: Sample larger than population or is negative
 
 print(random.sample(('xxx', 'yyy', 'zzz'), 2))
-# ['xxx', 'yyy']
+# ['zzz', 'xxx']
 
 print(random.sample('abcde', 2))
-# ['b', 'e']
+# ['c', 'd']
 
 print(tuple(random.sample(('xxx', 'yyy', 'zzz'), 2)))
-# ('xxx', 'yyy')
+# ('zzz', 'yyy')
 
 print(''.join(random.sample('abcde', 2)))
-# dc
+# be
 
 l_dup = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3]
 
 print(random.sample(l_dup, 3))
-# [3, 1, 1]
+# [2, 0, 0]
 
 print(set(l_dup))
 # {0, 1, 2, 3}
 
-print(random.sample(set(l_dup), 3))
-# [1, 3, 2]
+print(random.sample(list(set(l_dup)), 3))
+# [0, 2, 1]
