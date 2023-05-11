d89532c9d4d10df8ef97d56aef8b72b606511393
nkmk <nkmk.on@gmail.com>
2023-05-11 23:01:07 +0900

    update sort, sorted

--- notebook/sort_sorted.py
+++ notebook/sort_sorted.py
@@ -1,66 +1,92 @@
-org_list = [3, 1, 4, 5, 2]
+l = [3, 1, 4, 5, 2]
 
-org_list.sort()
-print(org_list)
+l.sort()
+print(l)
 # [1, 2, 3, 4, 5]
 
-print(org_list.sort())
+print(l.sort())
 # None
 
-org_list.sort(reverse=True)
-print(org_list)
+l.sort(reverse=True)
+print(l)
 # [5, 4, 3, 2, 1]
 
-org_list = [3, 1, 4, 5, 2]
+l = [3, 1, 4, 5, 2]
 
-new_list = sorted(org_list)
-print(org_list)
-print(new_list)
-# [3, 1, 4, 5, 2]
+l_sorted = sorted(l)
+print(l_sorted)
 # [1, 2, 3, 4, 5]
 
-new_list_reverse = sorted(org_list, reverse=True)
-print(org_list)
-print(new_list_reverse)
+print(l)
 # [3, 1, 4, 5, 2]
+
+l_reverse_sorted = sorted(l, reverse=True)
+print(l_reverse_sorted)
 # [5, 4, 3, 2, 1]
 
-org_str = 'cebad'
+print(l)
+# [3, 1, 4, 5, 2]
 
-new_str_list = sorted(org_str)
-print(org_str)
-print(new_str_list)
-# cebad
+l = [-3, 1, 4, -5, 2]
+print(sorted(l))
+# [-5, -3, 1, 2, 4]
+
+print(sorted(l, key=abs))
+# [1, 2, -3, 4, -5]
+
+l = ['b', 'cc', 'aaa']
+print(sorted(l))
+# ['aaa', 'b', 'cc']
+
+print(sorted(l, key=len))
+# ['b', 'cc', 'aaa']
+
+s = 'cebad'
+
+l_sorted = sorted(s)
+print(l_sorted)
 # ['a', 'b', 'c', 'd', 'e']
 
-new_str = ''.join(new_str_list)
-print(new_str)
+print(s)
+# cebad
+
+s_sorted = ''.join(l_sorted)
+print(s_sorted)
 # abcde
 
-new_str = ''.join(sorted(org_str))
-print(new_str)
+s_sorted = ''.join(sorted(s))
+print(s_sorted)
 # abcde
 
-new_str_reverse = ''.join(sorted(org_str, reverse=True))
-print(new_str_reverse)
+s_reverse_sorted = ''.join(sorted(s, reverse=True))
+print(s_reverse_sorted)
 # edcba
 
-org_tuple = (3, 1, 4, 5, 2)
+l = ['banana', 'cherry', 'apple']
+print(sorted(l))
+# ['apple', 'banana', 'cherry']
 
-new_tuple_list = sorted(org_tuple)
-print(org_tuple)
-print(new_tuple_list)
-# (3, 1, 4, 5, 2)
+l.sort()
+print(l)
+# ['apple', 'banana', 'cherry']
+
+t = (3, 1, 4, 5, 2)
+
+l_sorted = sorted(t)
+print(l_sorted)
 # [1, 2, 3, 4, 5]
 
-new_tuple = tuple(new_tuple_list)
-print(new_tuple)
+print(t)
+# (3, 1, 4, 5, 2)
+
+t_sorted = tuple(l_sorted)
+print(t_sorted)
 # (1, 2, 3, 4, 5)
 
-new_tuple = tuple(sorted(new_tuple_list))
-print(new_tuple)
+t_sorted = tuple(sorted(t))
+print(t_sorted)
 # (1, 2, 3, 4, 5)
 
-new_tuple_reverse = tuple(sorted(new_tuple_list, reverse=True))
-print(new_tuple_reverse)
+t_reverse_sorted = tuple(sorted(t, reverse=True))
+print(t_reverse_sorted)
 # (5, 4, 3, 2, 1)
