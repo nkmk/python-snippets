03b1e388d4a64bd30147b1baab1435347ad6e2ad
nkmk <nkmk.on@gmail.com>
2023-05-16 22:30:11 +0900

    update np.version

--- notebook/numpy_version.py
+++ notebook/numpy_version.py
@@ -1,19 +1,22 @@
 import numpy as np
 
 print(np.__version__)
-# 1.14.1
+# 1.24.3
+
+print(np.version)
+# <module 'numpy.version' from '/opt/homebrew/lib/python3.11/site-packages/numpy/version.py'>
 
 print(np.version.version)
-# 1.14.1
+# 1.24.3
 
 print(np.version.short_version)
-# 1.14.1
+# 1.24.3
 
 print(np.version.full_version)
-# 1.14.1
+# 1.24.3
 
 print(np.version.git_revision)
-# 7dcee7a469ad1bbfef1cd8980dc18bf5869c5391
+# 14bb214bca49b167abc375fa873466a811e62102
 
 print(np.version.release)
 # True
