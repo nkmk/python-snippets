5ad4cd3391ffdd8bf9d16d46d110db0fe3e9eced
nkmk <nkmk.on@gmail.com>
2023-06-10 11:36:34 +0900

    update json

--- notebook/json_example.py
+++ notebook/json_example.py
@@ -1,303 +1,190 @@
 import json
-from collections import OrderedDict
-import pprint
 
-s = r'{"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}'
-
-print(s)
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
+s = '{"A": {"X": 1, "Y": 1.0, "Z": "abc"}, "B": [true, false, null, NaN, Infinity]}'
 
 d = json.loads(s)
-
-pprint.pprint(d, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
+print(d)
+# {'A': {'X': 1, 'Y': 1.0, 'Z': 'abc'}, 'B': [True, False, None, nan, inf]}
 
 print(type(d))
 # <class 'dict'>
 
-od = json.loads(s, object_pairs_hook=OrderedDict)
+s = '{"A": True}'
 
-pprint.pprint(od)
-# OrderedDict([('C', 'あ'),
-#              ('A', OrderedDict([('i', 1), ('j', 2)])),
-#              ('B',
-#               [OrderedDict([('X', 1), ('Y', 10)]),
-#                OrderedDict([('X', 2), ('Y', 20)])])])
-
-b = s.encode()
-
-print(b)
-# b'{"C": "\\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}'
-
-print(type(b))
-# <class 'bytes'>
-
-db = json.loads(b)
-
-pprint.pprint(db, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
-
-print(type(db))
-# <class 'dict'>
-
-sb = b.decode()
-
-print(sb)
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
-
-print(type(sb))
-# <class 'str'>
-
-dsb = json.loads(sb)
-
-pprint.pprint(dsb, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
-
-print(type(dsb))
-# <class 'dict'>
-
-sb_u = b.decode('unicode-escape')
-
-print(sb_u)
-# {"C": "あ", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
-
-print(type(sb_u))
-# <class 'str'>
-
-dsb_u = json.loads(sb_u)
-
-pprint.pprint(dsb_u, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
-
-print(type(dsb_u))
-# <class 'dict'>
+# d = json.loads(s)
+# JSONDecodeError: Expecting value: line 1 column 7 (char 6)
 
 with open('data/src/test.json') as f:
     print(f.read())
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
+# {"A": {"X": 1, "Y": 1.0, "Z": "abc"}, "B": [true, false, null, NaN, Infinity]}
 
 with open('data/src/test.json') as f:
-    df = json.load(f)
+    d = json.load(f)
 
-pprint.pprint(df, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
+print(d)
+# {'A': {'X': 1, 'Y': 1.0, 'Z': 'abc'}, 'B': [True, False, None, nan, inf]}
 
-print(type(df))
+print(type(d))
 # <class 'dict'>
 
-pprint.pprint(d, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
+d = {'A': {'X': 1, 'Y': 1.0, 'Z': 'abc'}, 'B': [True, False, None]}
 
-print(d['A'])
-# {'i': 1, 'j': 2}
+s = json.dumps(d)
+print(s)
+# {"A": {"X": 1, "Y": 1.0, "Z": "abc"}, "B": [true, false, null]}
 
-print(d['A']['i'])
-# 1
-
-print(d['B'])
-# [{'X': 1, 'Y': 10}, {'X': 2, 'Y': 20}]
-
-print(d['B'][0])
-# {'X': 1, 'Y': 10}
-
-print(d['B'][0]['X'])
-# 1
-
-value = d['B'][1]['Y']
-print(value)
-# 20
-
-# print(d['D'])
-# KeyError: 'D'
-
-print(d.get('D'))
-# None
-
-d['C'] = 'ん'
-pprint.pprint(d, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'ん'}
-
-d.pop('C')
-pprint.pprint(d, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}]}
-
-d['C'] = 'あ'
-pprint.pprint(d, width=40)
-# {'A': {'i': 1, 'j': 2},
-#  'B': [{'X': 1, 'Y': 10},
-#        {'X': 2, 'Y': 20}],
-#  'C': 'あ'}
-
-sd = json.dumps(d)
-
-print(sd)
-# {"A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}], "C": "\u3042"}
-
-print(type(sd))
+print(type(s))
 # <class 'str'>
 
-pprint.pprint(od)
-# OrderedDict([('C', 'あ'),
-#              ('A', OrderedDict([('i', 1), ('j', 2)])),
-#              ('B',
-#               [OrderedDict([('X', 1), ('Y', 10)]),
-#                OrderedDict([('X', 2), ('Y', 20)])])])
-
-sod = json.dumps(od)
-
-print(sod)
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
-
-print(type(sod))
-# <class 'str'>
-
+d = {'A': {'X': 1, 'Y': 1.0, 'Z': 'abc'}, 'B': [True, False, None]}
 print(json.dumps(d, separators=(',', ':')))
-# {"A":{"i":1,"j":2},"B":[{"X":1,"Y":10},{"X":2,"Y":20}],"C":"\u3042"}
+# {"A":{"X":1,"Y":1.0,"Z":"abc"},"B":[true,false,null]}
 
-print(json.dumps(d, separators=(' / ', '-> ')))
-# {"A"-> {"i"-> 1 / "j"-> 2} / "B"-> [{"X"-> 1 / "Y"-> 10} / {"X"-> 2 / "Y"-> 20}] / "C"-> "\u3042"}
+print(json.dumps(d, separators=(' / ', '->')))
+# {"A"->{"X"->1 / "Y"->1.0 / "Z"->"abc"} / "B"->[true / false / null]}
 
+d = {'A': {'X': 1, 'Y': 1.0, 'Z': 'abc'}, 'B': [True, False, None]}
 print(json.dumps(d, indent=4))
 # {
 #     "A": {
-#         "i": 1,
-#         "j": 2
+#         "X": 1,
+#         "Y": 1.0,
+#         "Z": "abc"
 #     },
 #     "B": [
-#         {
-#             "X": 1,
-#             "Y": 10
-#         },
-#         {
-#             "X": 2,
-#             "Y": 20
-#         }
-#     ],
-#     "C": "\u3042"
+#         true,
+#         false,
+#         null
+#     ]
 # }
 
-print(json.dumps(od))
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
+d = {'B': {'Y': 2, 'X': 1}, 'A': [3, 1, 2]}
+print(json.dumps(d))
+# {"B": {"Y": 2, "X": 1}, "A": [3, 1, 2]}
 
-print(json.dumps(od, sort_keys=True))
-# {"A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}], "C": "\u3042"}
+print(json.dumps(d, sort_keys=True))
+# {"A": [3, 1, 2], "B": {"X": 1, "Y": 2}}
 
-print(json.dumps(od, ensure_ascii=False))
-# {"C": "あ", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
+d = {'A': [float('nan'), float('inf')]}
+print(json.dumps(d))
+# {"A": [NaN, Infinity]}
 
-with open('data/dst/test2.json', 'w') as f:
-    json.dump(d, f, indent=4)
+# print(json.dumps(d, allow_nan=False))
+# ValueError: Out of range float values are not JSON compliant
 
-with open('data/dst/test2.json') as f:
+d = {'A': 'あいうえお', 'B': 'abc'}
+print(json.dumps(d))
+# {"A": "\u3042\u3044\u3046\u3048\u304a", "B": "abc"}
+
+print(json.dumps(d, ensure_ascii=False))
+# {"A": "あいうえお", "B": "abc"}
+
+d = {
+    'A': {'X': 1, 'Y': 1.0, 'Z': 'abc'},
+    'B': [True, False, None, float('nan'), float('inf')]
+}
+
+with open('data/temp/test.json', 'w') as f:
+    json.dump(d, f, indent=2)
+
+with open('data/temp/test.json') as f:
     print(f.read())
 # {
-#     "A": {
-#         "i": 1,
-#         "j": 2
-#     },
-#     "B": [
-#         {
-#             "X": 1,
-#             "Y": 10
-#         },
-#         {
-#             "X": 2,
-#             "Y": 20
-#         }
-#     ],
-#     "C": "\u3042"
+#   "A": {
+#     "X": 1,
+#     "Y": 1.0,
+#     "Z": "abc"
+#   },
+#   "B": [
+#     true,
+#     false,
+#     null,
+#     NaN,
+#     Infinity
+#   ]
 # }
 
-d_new = {'A': 100, 'B': 'abc', 'C': 'あいうえお'}
+d_new = {'A': 100, 'B': 'abc', 'C': [True, False]}
 
-with open('data/dst/test_new.json', 'w') as f:
-    json.dump(d_new, f, indent=2, ensure_ascii=False)
+with open('data/temp/test_new.json', 'w') as f:
+    json.dump(d_new, f, indent=2)
 
-with open('data/dst/test_new.json') as f:
+with open('data/temp/test_new.json') as f:
     print(f.read())
 # {
 #   "A": 100,
 #   "B": "abc",
-#   "C": "あいうえお"
+#   "C": [
+#     true,
+#     false
+#   ]
 # }
 
-with open('data/dst/test_new.json') as f:
-    d_update = json.load(f, object_pairs_hook=OrderedDict)
+with open('data/temp/test_new.json') as f:
+    d_update = json.load(f)
 
 print(d_update)
-# OrderedDict([('A', 100), ('B', 'abc'), ('C', 'あいうえお')])
+# {'A': 100, 'B': 'abc', 'C': [True, False]}
 
 d_update['A'] = 200
 d_update.pop('B')
 d_update['D'] = 'new value'
 
 print(d_update)
-# OrderedDict([('A', 200), ('C', 'あいうえお'), ('D', 'new value')])
+# {'A': 200, 'C': [True, False], 'D': 'new value'}
 
-with open('data/dst/test_new_update.json', 'w') as f:
-    json.dump(d_update, f, indent=2, ensure_ascii=False)
+with open('data/temp/test_new_update.json', 'w') as f:
+    json.dump(d_update, f, indent=2)
 
-with open('data/dst/test_new_update.json') as f:
+with open('data/temp/test_new_update.json') as f:
     print(f.read())
 # {
 #   "A": 200,
-#   "C": "あいうえお",
+#   "C": [
+#     true,
+#     false
+#   ],
 #   "D": "new value"
 # }
 
-with open('data/src/test.json') as f:
-    print(f.read())
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
+d = {'A': 100, 'B': 'abc', 'C': [True, False]}
 
-with open('data/src/test.json', encoding='unicode-escape') as f:
-    print(f.read())
-# {"C": "あ", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
-
-print(b)
-# b'{"C": "\\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}'
-
-print(b.decode())
-# {"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
-
-print(b.decode(encoding='unicode-escape'))
-# {"C": "あ", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}
-
-d = {"A": 100, "B": 'abc', "C": 'あいうえお'}
-
+s = str(d)
 print(str(d))
-# {'A': 100, 'B': 'abc', 'C': 'あいうえお'}
+# {'A': 100, 'B': 'abc', 'C': [True, False]}
 
-# print(json.loads(str(d)))
+# print(json.loads(s))
 # JSONDecodeError: Expecting property name enclosed in double quotes: line 1 column 2 (char 1)
 
+s = json.dumps(d)
+print(s)
+# {"A": 100, "B": "abc", "C": [true, false]}
+
 print(json.dumps(d))
-# {"A": 100, "B": "abc", "C": "\u3042\u3044\u3046\u3048\u304a"}
+# {"A": 100, "B": "abc", "C": [true, false]}
 
-print(json.loads(json.dumps(d)))
-# {'A': 100, 'B': 'abc', 'C': 'あいうえお'}
+with open('data/src/test_u.json') as f:
+    print(f.read())
+# {"A": "\u3042\u3044\u3046\u3048\u304a", "B": "abc"}
 
-print(type(json.loads(json.dumps(d))))
-# <class 'dict'>
+with open('data/src/test_u.json', encoding='unicode-escape') as f:
+    print(f.read())
+# {"A": "あいうえお", "B": "abc"}
+
+with open('data/src/test_u.json') as f:
+    print(json.load(f))
+# {'A': 'あいうえお', 'B': 'abc'}
+
+s = r'{"A": "\u3042\u3044\u3046\u3048\u304a", "B": "abc"}'
+b = s.encode()
+print(b)
+# b'{"A": "\\u3042\\u3044\\u3046\\u3048\\u304a", "B": "abc"}'
+
+print(b.decode())
+# {"A": "\u3042\u3044\u3046\u3048\u304a", "B": "abc"}
+
+print(b.decode(encoding='unicode-escape'))
+# {"A": "あいうえお", "B": "abc"}
+
+print(json.loads(b))
+# {'A': 'あいうえお', 'B': 'abc'}
