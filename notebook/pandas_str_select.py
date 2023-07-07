9c79d3fa5576c080679cb1532125fbfe022606ab
nkmk <nkmk.on@gmail.com>
2023-07-07 17:16:48 +0900

    add pandas str select

--- notebook/pandas_str_select.py
+++ notebook/pandas_str_select.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+print(pd.__version__)
+# 2.0.3
+
 df = pd.read_csv('data/src/sample_pandas_normal.csv').head(3)
 print(df)
 #       name  age state  point
@@ -8,8 +11,7 @@ print(df)
 # 2  Charlie   18    CA     70
 
 mask = [True, False, True]
-df_mask = df[mask]
-print(df_mask)
+print(df[mask])
 #       name  age state  point
 # 0    Alice   24    NY     64
 # 2  Charlie   18    CA     70
@@ -22,6 +24,18 @@ print(df['state'] == 'CA')
 
 print(df[df['state'] == 'CA'])
 #       name  age state  point
+# 1      Bob   42    CA     92
+# 2  Charlie   18    CA     70
+
+print(df['state'].isin(['NY', 'CA']))
+# 0    True
+# 1    True
+# 2    True
+# Name: state, dtype: bool
+
+print(df[df['state'].isin(['NY', 'CA'])])
+#       name  age state  point
+# 0    Alice   24    NY     64
 # 1      Bob   42    CA     92
 # 2  Charlie   18    CA     70
 
@@ -51,7 +65,7 @@ print(df_nan['name'].str.contains('li'))
 # Name: name, dtype: object
 
 # print(df_nan[df_nan['name'].str.contains('li')])
-# ValueError: cannot index with vector containing NA / NaN values
+# ValueError: Cannot mask with non-boolean array containing NA / NaN values
 
 print(df_nan['name'].str.contains('li', na=False))
 # 0     True
@@ -106,11 +120,21 @@ print(df_q['name'].str.contains('?', regex=False))
 # 2     True
 # Name: name, dtype: bool
 
-print(df_q['name'].str.contains('\?'))
+print(df_q['name'].str.contains(r'\?'))
 # 0    False
 # 1    False
 # 2     True
 # Name: name, dtype: bool
+
+print(df['name'].str.startswith('B'))
+# 0    False
+# 1     True
+# 2    False
+# Name: name, dtype: bool
+
+print(df[df['name'].str.startswith('B')])
+#   name  age state  point
+# 1  Bob   42    CA     92
 
 print(df['name'].str.endswith('e'))
 # 0     True
@@ -123,35 +147,25 @@ print(df[df['name'].str.endswith('e')])
 # 0    Alice   24    NY     64
 # 2  Charlie   18    CA     70
 
-print(df['name'].str.startswith('B'))
-# 0    False
-# 1     True
-# 2    False
-# Name: name, dtype: bool
-
-print(df[df['name'].str.startswith('B')])
-#   name  age state  point
-# 1  Bob   42    CA     92
-
-print(df['name'].str.match('.*i.*e'))
-# 0     True
-# 1    False
-# 2     True
-# Name: name, dtype: bool
-
-print(df[df['name'].str.match('.*i.*e')])
-#       name  age state  point
-# 0    Alice   24    NY     64
-# 2  Charlie   18    CA     70
-
 print(df['name'].str.match('.*i'))
 # 0     True
 # 1    False
 # 2     True
 # Name: name, dtype: bool
 
+print(df[df['name'].str.match('.*i')])
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 2  Charlie   18    CA     70
+
 print(df['name'].str.match('i.*e'))
 # 0    False
 # 1    False
 # 2    False
+# Name: name, dtype: bool
+
+print(df['name'].str.contains('i.*e'))
+# 0     True
+# 1    False
+# 2     True
 # Name: name, dtype: bool
