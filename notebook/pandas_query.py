2f0b2d43ab73f1a2af028f2f77b571da70c6c99b
nkmk <nkmk.on@gmail.com>
2023-07-07 17:16:36 +0900

    add pandas query

--- notebook/pandas_query.py
+++ notebook/pandas_query.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('data/src/sample_pandas_normal.csv')
+print(pd.__version__)
+# 2.0.3
 
+df = pd.read_csv('data/src/sample_pandas_normal.csv')
 print(df)
 #       name  age state  point
 # 0    Alice   24    NY     64
@@ -23,17 +25,16 @@ print(df.query('age < 25'))
 # 2  Charlie   18    CA     70
 # 4    Ellen   24    CA     88
 
-print(df.query('not age < 25'))
-#     name  age state  point
-# 1    Bob   42    CA     92
-# 3   Dave   68    TX     70
-# 5  Frank   30    NY     57
+val = 25
+print(df.query('age < @val'))
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 2  Charlie   18    CA     70
+# 4    Ellen   24    CA     88
 
-print(df.query('24 <= age < 50'))
+print(df.query('30 <= age < 50'))
 #     name  age state  point
-# 0  Alice   24    NY     64
 # 1    Bob   42    CA     92
-# 4  Ellen   24    CA     88
 # 5  Frank   30    NY     57
 
 print(df.query('age < point / 3'))
@@ -47,7 +48,8 @@ print(df.query('state == "CA"'))
 # 2  Charlie   18    CA     70
 # 4    Ellen   24    CA     88
 
-print(df.query('state != "CA"'))
+s = 'CA'
+print(df.query('state != @s'))
 #     name  age state  point
 # 0  Alice   24    NY     64
 # 3   Dave   68    TX     70
@@ -71,23 +73,25 @@ print(df.query('state == ["NY", "TX"]'))
 # 3   Dave   68    TX     70
 # 5  Frank   30    NY     57
 
-print(df.query('name.str.endswith("e")', engine='python'))
+l = ['NY', 'TX']
+print(df.query('state in @l'))
+#     name  age state  point
+# 0  Alice   24    NY     64
+# 3   Dave   68    TX     70
+# 5  Frank   30    NY     57
+
+print(df.query('name.str.endswith("e")'))
 #       name  age state  point
 # 0    Alice   24    NY     64
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
 
-print(df.query('name.str.contains("li")', engine='python'))
+print(df.query('name.str.contains("li")'))
 #       name  age state  point
 # 0    Alice   24    NY     64
 # 2  Charlie   18    CA     70
 
-print(df.query('name.str.match(".*i.*e")', engine='python'))
-#       name  age state  point
-# 0    Alice   24    NY     64
-# 2  Charlie   18    CA     70
-
-print(df.query('age.astype("str").str.endswith("8")', engine='python'))
+print(df.query('age.astype("str").str.endswith("8")'))
 #       name  age state  point
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
@@ -102,26 +106,20 @@ print(df)
 # 4    Ellen   24    CA     88
 # 5    Frank   30    NY     57
 
-# print(df.query('name.str.endswith("e")', engine='python'))
-# ValueError: cannot index with vector containing NA / NaN values
+# print(df.query('name.str.endswith("e")'))
+# ValueError: unknown type object
 
 print(df[df['name'].str.endswith('e', na=False)])
 #       name  age state  point
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
 
-# print(df.query('name.str.endswith("e", na=False)', engine='python'))
-# AttributeError: 'dict' object has no attribute 'append'
-
-df['name'].fillna('Alice', inplace=True)
-print(df)
+print(df.query('name.str.endswith("e", na=False)'))
 #       name  age state  point
-# 0    Alice   24    NY     64
-# 1      Bob   42    CA     92
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
-# 4    Ellen   24    CA     88
-# 5    Frank   30    NY     57
+
+df = pd.read_csv('data/src/sample_pandas_normal.csv')
 
 print(df.query('index % 2 == 0'))
 #       name  age state  point
@@ -140,25 +138,19 @@ print(df_name)
 # Ellen     24    CA     88
 # Frank     30    NY     57
 
-print(df_name.query('name.str.endswith("e")', engine='python'))
+print(df_name.query('name.str.endswith("e")'))
 #          age state  point
 # name                     
 # Alice     24    NY     64
 # Charlie   18    CA     70
 # Dave      68    TX     70
 
-print(df_name.query('index.str.endswith("e")', engine='python'))
+print(df_name.query('index.str.endswith("e")'))
 #          age state  point
 # name                     
 # Alice     24    NY     64
 # Charlie   18    CA     70
 # Dave      68    TX     70
-
-val = 80
-print(df.query('point > @val'))
-#     name  age state  point
-# 1    Bob   42    CA     92
-# 4  Ellen   24    CA     88
 
 print(df[(df['age'] < 25) & (df['point'] > 65)])
 #       name  age state  point
@@ -175,20 +167,16 @@ print(df.query('age < 25 and point > 65'))
 # 2  Charlie   18    CA     70
 # 4    Ellen   24    CA     88
 
-print(df.query('age < 25 | point > 65'))
+print(df.query('age < 20 | point > 80'))
 #       name  age state  point
-# 0    Alice   24    NY     64
 # 1      Bob   42    CA     92
 # 2  Charlie   18    CA     70
-# 3     Dave   68    TX     70
 # 4    Ellen   24    CA     88
 
-print(df.query('age < 25 or point > 65'))
+print(df.query('age < 20 or point > 80'))
 #       name  age state  point
-# 0    Alice   24    NY     64
 # 1      Bob   42    CA     92
 # 2  Charlie   18    CA     70
-# 3     Dave   68    TX     70
 # 4    Ellen   24    CA     88
 
 print(df.query('not age < 25 and not point > 65'))
@@ -206,9 +194,9 @@ print(df.query('(age == 24 | point > 80) & state == "CA"'))
 # 1    Bob   42    CA     92
 # 4  Ellen   24    CA     88
 
-df.columns = ['名前', 'age.year', 'state name', 3]
+df.columns = ['0name', 'age.year', 'state name', 3]
 print(df)
-#         名前  age.year state name   3
+#      0name  age.year state name   3
 # 0    Alice        24         NY  64
 # 1      Bob        42         CA  92
 # 2  Charlie        18         CA  70
@@ -216,15 +204,8 @@ print(df)
 # 4    Ellen        24         CA  88
 # 5    Frank        30         NY  57
 
-print(df.query('名前 == ["Alice", "Dave"]'))
-#       名前  age.year state name   3
-# 0  Alice        24         NY  64
-# 3   Dave        68         TX  70
-
-print(df.query('名前.str.contains("li")', engine='python'))
-#         名前  age.year state name   3
-# 0    Alice        24         NY  64
-# 2  Charlie        18         CA  70
+# print(df.query('0name.str.endswith("e")'))
+# SyntaxError: invalid syntax
 
 # print(df.query('age.year < 25'))
 # UndefinedVariableError: name 'age' is not defined
@@ -232,49 +213,40 @@ print(df.query('名前.str.contains("li")', engine='python'))
 # print(df.query('state name == "CA"'))
 # SyntaxError: invalid syntax
 
-# print(df.query('3 > 75'))
-# KeyError: False
+print(df.query('`0name`.str.endswith("e")'))
+#      0name  age.year state name   3
+# 0    Alice        24         NY  64
+# 2  Charlie        18         CA  70
+# 3     Dave        68         TX  70
 
-print(df[df['age.year'] < 25])
-#         名前  age.year state name   3
+print(df.query('`age.year` < 25'))
+#      0name  age.year state name   3
 # 0    Alice        24         NY  64
 # 2  Charlie        18         CA  70
 # 4    Ellen        24         CA  88
 
-print(df[df['state name'] == 'CA'])
-#         名前  age.year state name   3
+print(df.query('`state name` == "CA"'))
+#      0name  age.year state name   3
 # 1      Bob        42         CA  92
 # 2  Charlie        18         CA  70
 # 4    Ellen        24         CA  88
 
+# print(df.query('3 > 75'))
+# KeyError: False
+
+# print(df.query('`3` > 75'))
+# UndefinedVariableError: name 'BACKTICK_QUOTED_STRING_3' is not defined
+
 print(df[df[3] > 75])
-#       名前  age.year state name   3
+#    0name  age.year state name   3
 # 1    Bob        42         CA  92
 # 4  Ellen        24         CA  88
 
-df.rename(columns={3: 'point'}, inplace=True)
-print(df)
-#         名前  age.year state name  point
-# 0    Alice        24         NY     64
-# 1      Bob        42         CA     92
-# 2  Charlie        18         CA     70
-# 3     Dave        68         TX     70
-# 4    Ellen        24         CA     88
-# 5    Frank        30         NY     57
+df = pd.read_csv('data/src/sample_pandas_normal.csv')
 
-df.columns = [str(s).replace(' ', '_').replace('.', '_') for s in df.columns]
+df.query('age > 25', inplace=True)
 print(df)
-#         名前  age_year state_name  point
-# 0    Alice        24         NY     64
-# 1      Bob        42         CA     92
-# 2  Charlie        18         CA     70
-# 3     Dave        68         TX     70
-# 4    Ellen        24         CA     88
-# 5    Frank        30         NY     57
-
-df.query('age_year > 25', inplace=True)
-print(df)
-#       名前  age_year state_name  point
-# 1    Bob        42         CA     92
-# 3   Dave        68         TX     70
-# 5  Frank        30         NY     57
+#     name  age state  point
+# 1    Bob   42    CA     92
+# 3   Dave   68    TX     70
+# 5  Frank   30    NY     57
