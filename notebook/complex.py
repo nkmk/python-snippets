4e2fcfcce498a323c3d2302b5cd9a4ec28d87e71
nkmk <nkmk.on@gmail.com>
2023-07-11 23:14:35 +0900

    update complex

--- notebook/complex.py
+++ notebook/complex.py
@@ -1,39 +1,32 @@
 c = 3 + 4j
-
 print(c)
-print(type(c))
 # (3+4j)
+
+print(type(c))
 # <class 'complex'>
 
 # c = 3 + j
 # NameError: name 'j' is not defined
 
 c = 3 + 1j
-
 print(c)
 # (3+1j)
 
 c = 3j
-
 print(c)
 # 3j
 
 c = 3 + 0j
-
 print(c)
 # (3+0j)
 
 c = 1.2e3 + 3j
-
 print(c)
 # (1200+3j)
 
 c = complex(3, 4)
-
 print(c)
-print(type(c))
 # (3+4j)
-# <class 'complex'>
 
 c = 3 + 4j
 
@@ -51,17 +44,14 @@ print(type(c.imag))
 # AttributeError: readonly attribute
 
 c = 3 + 4j
-
 print(c.conjugate())
 # (3-4j)
 
 c = 3 + 4j
-
 print(abs(c))
 # 5.0
 
 c = 1 + 1j
-
 print(abs(c))
 # 1.4142135623730951
 
@@ -80,11 +70,13 @@ print(c1 * c2)
 print(c1 / c2)
 # (0.4+2.2j)
 
-print(c1 ** 3)
+print(c1**3)
 # (-117+44j)
 
-print(c1 + 3)
+c = 3 + 4j
+
+print(c + 3)
 # (6+4j)
 
-print(c1 * 0.5)
+print(c * 0.5)
 # (1.5+2j)
--- notebook/complex_cmath.py
+++ notebook/complex_cmath.py
@@ -3,9 +3,6 @@ import math
 
 c = 1 + 1j
 
-print(math.atan2(c.imag, c.real))
-# 0.7853981633974483
-
 print(cmath.phase(c))
 # 0.7853981633974483
 
@@ -18,9 +15,7 @@ print(math.degrees(cmath.phase(c)))
 c = 1 + 1j
 
 print(cmath.polar(c))
-print(type(cmath.polar(c)))
 # (1.4142135623730951, 0.7853981633974483)
-# <class 'tuple'>
 
 print(cmath.polar(c)[0] == abs(c))
 # True
@@ -28,17 +23,17 @@ print(cmath.polar(c)[0] == abs(c))
 print(cmath.polar(c)[1] == cmath.phase(c))
 # True
 
-print(cmath.rect(1, 1))
-# (0.5403023058681398+0.8414709848078965j)
-
 print(cmath.rect(1, 0))
 # (1+0j)
 
-print(cmath.rect(cmath.polar(c)[0], cmath.polar(c)[1]))
-# (1.0000000000000002+1j)
+print(cmath.rect(1, math.pi / 2))
+# (6.123233995736766e-17+1j)
 
-r = 2
-ph = math.pi
+r = math.sqrt(2)
+ph = math.pi / 4
+
+print(cmath.rect(r, ph))
+# (1.0000000000000002+1j)
 
 print(cmath.rect(r, ph).real == r * math.cos(ph))
 # True
