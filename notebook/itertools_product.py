baa8e5a70de514f6789950b095a8fc6a07fafb7c
nkmk <nkmk.on@gmail.com>
2023-07-11 23:12:33 +0900

    update itertools.product

--- notebook/itertools_product.py
+++ notebook/itertools_product.py
@@ -1,69 +1,53 @@
 import itertools
 import pprint
 
-l1 = ['a', 'b', 'c']
-l2 = ['X', 'Y', 'Z']
+l1 = [1, 2, 3]
+l2 = ['A', 'B']
 
 p = itertools.product(l1, l2)
-
 print(p)
-# <itertools.product object at 0x1026edd80>
+# <itertools.product object at 0x105e6e2c0>
 
 print(type(p))
 # <class 'itertools.product'>
 
 for v in p:
     print(v)
-# ('a', 'X')
-# ('a', 'Y')
-# ('a', 'Z')
-# ('b', 'X')
-# ('b', 'Y')
-# ('b', 'Z')
-# ('c', 'X')
-# ('c', 'Y')
-# ('c', 'Z')
+# (1, 'A')
+# (1, 'B')
+# (2, 'A')
+# (2, 'B')
+# (3, 'A')
+# (3, 'B')
 
 for v in p:
     print(v)
 
 for v1, v2 in itertools.product(l1, l2):
     print(v1, v2)
-# a X
-# a Y
-# a Z
-# b X
-# b Y
-# b Z
-# c X
-# c Y
-# c Z
+# 1 A
+# 1 B
+# 2 A
+# 2 B
+# 3 A
+# 3 B
 
 for v1 in l1:
     for v2 in l2:
         print(v1, v2)
-# a X
-# a Y
-# a Z
-# b X
-# b Y
-# b Z
-# c X
-# c Y
-# c Z
+# 1 A
+# 1 B
+# 2 A
+# 2 B
+# 3 A
+# 3 B
+
+l1 = [1, 2, 3]
+l2 = ['A', 'B']
 
 l_p = list(itertools.product(l1, l2))
-
-pprint.pprint(l_p)
-# [('a', 'X'),
-#  ('a', 'Y'),
-#  ('a', 'Z'),
-#  ('b', 'X'),
-#  ('b', 'Y'),
-#  ('b', 'Z'),
-#  ('c', 'X'),
-#  ('c', 'Y'),
-#  ('c', 'Z')]
+print(l_p)
+# [(1, 'A'), (1, 'B'), (2, 'A'), (2, 'B'), (3, 'A'), (3, 'B')]
 
 print(type(l_p))
 # <class 'list'>
@@ -71,79 +55,77 @@ print(type(l_p))
 print(type(l_p[0]))
 # <class 'tuple'>
 
-t = ('one', 'two')
+t = ('A', 'B')
 d = {'key1': 'value1', 'key2': 'value2'}
 r = range(2)
 
-l_p = list(itertools.product(t, d, r))
+pprint.pprint(list(itertools.product(t, d, r)))
+# [('A', 'key1', 0),
+#  ('A', 'key1', 1),
+#  ('A', 'key2', 0),
+#  ('A', 'key2', 1),
+#  ('B', 'key1', 0),
+#  ('B', 'key1', 1),
+#  ('B', 'key2', 0),
+#  ('B', 'key2', 1)]
 
-pprint.pprint(l_p)
-# [('one', 'key1', 0),
-#  ('one', 'key1', 1),
-#  ('one', 'key2', 0),
-#  ('one', 'key2', 1),
-#  ('two', 'key1', 0),
-#  ('two', 'key1', 1),
-#  ('two', 'key2', 0),
-#  ('two', 'key2', 1)]
-
-l1 = ['a', 'b']
+l1 = ['A', 'B']
 
 pprint.pprint(list(itertools.product(l1, repeat=3)))
-# [('a', 'a', 'a'),
-#  ('a', 'a', 'b'),
-#  ('a', 'b', 'a'),
-#  ('a', 'b', 'b'),
-#  ('b', 'a', 'a'),
-#  ('b', 'a', 'b'),
-#  ('b', 'b', 'a'),
-#  ('b', 'b', 'b')]
+# [('A', 'A', 'A'),
+#  ('A', 'A', 'B'),
+#  ('A', 'B', 'A'),
+#  ('A', 'B', 'B'),
+#  ('B', 'A', 'A'),
+#  ('B', 'A', 'B'),
+#  ('B', 'B', 'A'),
+#  ('B', 'B', 'B')]
 
 pprint.pprint(list(itertools.product(l1, l1, l1)))
-# [('a', 'a', 'a'),
-#  ('a', 'a', 'b'),
-#  ('a', 'b', 'a'),
-#  ('a', 'b', 'b'),
-#  ('b', 'a', 'a'),
-#  ('b', 'a', 'b'),
-#  ('b', 'b', 'a'),
-#  ('b', 'b', 'b')]
+# [('A', 'A', 'A'),
+#  ('A', 'A', 'B'),
+#  ('A', 'B', 'A'),
+#  ('A', 'B', 'B'),
+#  ('B', 'A', 'A'),
+#  ('B', 'A', 'B'),
+#  ('B', 'B', 'A'),
+#  ('B', 'B', 'B')]
 
-l1 = ['a', 'b']
-l2 = ['X', 'Y']
+l1 = [1, 2]
+l2 = ['A', 'B']
 
 pprint.pprint(list(itertools.product(l1, l2, repeat=2)))
-# [('a', 'X', 'a', 'X'),
-#  ('a', 'X', 'a', 'Y'),
-#  ('a', 'X', 'b', 'X'),
-#  ('a', 'X', 'b', 'Y'),
-#  ('a', 'Y', 'a', 'X'),
-#  ('a', 'Y', 'a', 'Y'),
-#  ('a', 'Y', 'b', 'X'),
-#  ('a', 'Y', 'b', 'Y'),
-#  ('b', 'X', 'a', 'X'),
-#  ('b', 'X', 'a', 'Y'),
-#  ('b', 'X', 'b', 'X'),
-#  ('b', 'X', 'b', 'Y'),
-#  ('b', 'Y', 'a', 'X'),
-#  ('b', 'Y', 'a', 'Y'),
-#  ('b', 'Y', 'b', 'X'),
-#  ('b', 'Y', 'b', 'Y')]
+# [(1, 'A', 1, 'A'),
+#  (1, 'A', 1, 'B'),
+#  (1, 'A', 2, 'A'),
+#  (1, 'A', 2, 'B'),
+#  (1, 'B', 1, 'A'),
+#  (1, 'B', 1, 'B'),
+#  (1, 'B', 2, 'A'),
+#  (1, 'B', 2, 'B'),
+#  (2, 'A', 1, 'A'),
+#  (2, 'A', 1, 'B'),
+#  (2, 'A', 2, 'A'),
+#  (2, 'A', 2, 'B'),
+#  (2, 'B', 1, 'A'),
+#  (2, 'B', 1, 'B'),
+#  (2, 'B', 2, 'A'),
+#  (2, 'B', 2, 'B')]
 
 pprint.pprint(list(itertools.product(l1, l2, l1, l2)))
-# [('a', 'X', 'a', 'X'),
-#  ('a', 'X', 'a', 'Y'),
-#  ('a', 'X', 'b', 'X'),
-#  ('a', 'X', 'b', 'Y'),
-#  ('a', 'Y', 'a', 'X'),
-#  ('a', 'Y', 'a', 'Y'),
-#  ('a', 'Y', 'b', 'X'),
-#  ('a', 'Y', 'b', 'Y'),
-#  ('b', 'X', 'a', 'X'),
-#  ('b', 'X', 'a', 'Y'),
-#  ('b', 'X', 'b', 'X'),
-#  ('b', 'X', 'b', 'Y'),
-#  ('b', 'Y', 'a', 'X'),
-#  ('b', 'Y', 'a', 'Y'),
-#  ('b', 'Y', 'b', 'X'),
-#  ('b', 'Y', 'b', 'Y')]
+# [(1, 'A', 1, 'A'),
+#  (1, 'A', 1, 'B'),
+#  (1, 'A', 2, 'A'),
+#  (1, 'A', 2, 'B'),
+#  (1, 'B', 1, 'A'),
+#  (1, 'B', 1, 'B'),
+#  (1, 'B', 2, 'A'),
+#  (1, 'B', 2, 'B'),
+#  (2, 'A', 1, 'A'),
+#  (2, 'A', 1, 'B'),
+#  (2, 'A', 2, 'A'),
+#  (2, 'A', 2, 'B'),
+#  (2, 'B', 1, 'A'),
+#  (2, 'B', 1, 'B'),
+#  (2, 'B', 2, 'A'),
+#  (2, 'B', 2, 'B')]
