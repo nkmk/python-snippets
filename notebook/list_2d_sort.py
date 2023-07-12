402f66a41c9a3505f0a0165941b739e6e2e3fc8a
nkmk <nkmk.on@gmail.com>
2023-07-12 23:20:17 +0900

    update 2D list sort

--- notebook/list_2d_sort.py
+++ notebook/list_2d_sort.py
@@ -1,14 +1,5 @@
 import pprint
 
-print([100] > [-100])
-# True
-
-print([1, 2, 100] > [1, 2, -100])
-# True
-
-print([1, 2, 100] > [1, 100])
-# False
-
 l_2d = [[20, 3, 100], [1, 200, 30], [300, 10, 2]]
 pprint.pprint(l_2d, width=20)
 # [[20, 3, 100],
@@ -20,20 +11,12 @@ pprint.pprint(sorted(l_2d), width=20)
 #  [20, 3, 100],
 #  [300, 10, 2]]
 
-pprint.pprint(l_2d, width=20)
-# [[20, 3, 100],
-#  [1, 200, 30],
-#  [300, 10, 2]]
-
 pprint.pprint([sorted(l) for l in l_2d], width=20)
 # [[3, 20, 100],
 #  [1, 30, 200],
 #  [2, 10, 300]]
 
-pprint.pprint(
-    [list(x) for x in zip(*[sorted(l) for l in zip(*l_2d)])],
-    width=20
-)
+pprint.pprint([list(x) for x in zip(*[sorted(l) for l in zip(*l_2d)])], width=20)
 # [[1, 3, 2],
 #  [20, 10, 30],
 #  [300, 200, 100]]
@@ -61,11 +44,8 @@ print(type(np.sort(l_2d).tolist()))
 
 l_2d_error = [[1, 2], [3, 4, 5]]
 
-print(np.sort(l_2d_error))
-# [list([1, 2]) list([3, 4, 5])]
-# 
-# /usr/local/lib/python3.8/site-packages/numpy/core/_asarray.py:136: VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated. If you meant to do this, you must specify 'dtype=object' when creating the ndarray
-#   return array(a, dtype, copy=False, order=order, subok=True)
+# print(np.sort(l_2d_error))
+# ValueError: setting an array element with a sequence. The requested array has an inhomogeneous shape after 1 dimensions. The detected shape was (2,) + inhomogeneous part.
 
 pprint.pprint(sorted(l_2d, key=lambda x: x[1]), width=20)
 # [[20, 3, 100],
@@ -84,55 +64,80 @@ pprint.pprint(sorted(l_2d, key=operator.itemgetter(1)), width=20)
 #  [300, 10, 2],
 #  [1, 200, 30]]
 
-l_2d_dup = [[20, 3, 100], [1, 200, 30], [1, 10, 2]]
-pprint.pprint(l_2d_dup, width=20)
-# [[20, 3, 100],
+pprint.pprint(sorted(l_2d, key=operator.itemgetter(2)), width=20)
+# [[300, 10, 2],
 #  [1, 200, 30],
-#  [1, 10, 2]]
+#  [20, 3, 100]]
+
+l_2d_dup = [[1, 3, 100], [1, 200, 30], [1, 3, 2]]
+pprint.pprint(l_2d_dup, width=20)
+# [[1, 3, 100],
+#  [1, 200, 30],
+#  [1, 3, 2]]
 
 pprint.pprint(sorted(l_2d_dup), width=20)
-# [[1, 10, 2],
-#  [1, 200, 30],
-#  [20, 3, 100]]
+# [[1, 3, 2],
+#  [1, 3, 100],
+#  [1, 200, 30]]
 
-pprint.pprint(sorted(l_2d_dup, key=operator.itemgetter(0)), width=20)
-# [[1, 200, 30],
-#  [1, 10, 2],
-#  [20, 3, 100]]
-
-pprint.pprint(sorted(l_2d_dup, key=operator.itemgetter(0, 1)), width=20)
-# [[1, 10, 2],
+pprint.pprint(sorted(l_2d_dup, key=operator.itemgetter(0, 2)), width=20)
+# [[1, 3, 2],
 #  [1, 200, 30],
-#  [20, 3, 100]]
+#  [1, 3, 100]]
 
-pprint.pprint(sorted(l_2d_dup, key=lambda x: (x[0], x[1])), width=20)
-# [[1, 10, 2],
+pprint.pprint(sorted(l_2d_dup, key=lambda x: (x[0], x[2])), width=20)
+# [[1, 3, 2],
 #  [1, 200, 30],
-#  [20, 3, 100]]
+#  [1, 3, 100]]
 
 import pandas as pd
 
 df = pd.DataFrame(l_2d_dup, columns=['A', 'B', 'C'], index=['X', 'Y', 'Z'])
 print(df)
-#     A    B    C
-# X  20    3  100
-# Y   1  200   30
-# Z   1   10    2
+#    A    B    C
+# X  1    3  100
+# Y  1  200   30
+# Z  1    3    2
 
-print(df.sort_values('A'))
-#     A    B    C
-# Y   1  200   30
-# Z   1   10    2
-# X  20    3  100
+print(df.sort_values('C'))
+#    A    B    C
+# Z  1    3    2
+# Y  1  200   30
+# X  1    3  100
 
-print(df.sort_values('X', axis=1))
-#      B   A    C
-# X    3  20  100
-# Y  200   1   30
-# Z   10   1    2
+print(df.sort_values('Z', axis=1))
+#    A    C    B
+# X  1  100    3
+# Y  1   30  200
+# Z  1    2    3
 
-print(df.sort_values(['A', 'B']))
-#     A    B    C
-# Z   1   10    2
-# Y   1  200   30
-# X  20    3  100
+print(df.sort_values(['A', 'C']))
+#    A    B    C
+# Z  1    3    2
+# Y  1  200   30
+# X  1    3  100
+
+df = pd.DataFrame(l_2d_dup)
+print(df)
+#    0    1    2
+# 0  1    3  100
+# 1  1  200   30
+# 2  1    3    2
+
+print(df.sort_values(2))
+#    0    1    2
+# 2  1    3    2
+# 1  1  200   30
+# 0  1    3  100
+
+print(df.sort_values(2, axis=1))
+#    0    2    1
+# 0  1  100    3
+# 1  1   30  200
+# 2  1    2    3
+
+print(df.sort_values([0, 2]))
+#    0    1    2
+# 2  1    3    2
+# 1  1  200   30
+# 0  1    3  100
