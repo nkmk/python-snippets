3b7202d041b7457de2473adec07a2c81642b2939
nkmk <nkmk.on@gmail.com>
2023-09-20 21:27:59 +0900

    update download files

--- notebook/beautiful_soup_image.py
+++ notebook/beautiful_soup_image.py
@@ -1,36 +1,28 @@
 import os
 import time
-import urllib.error
 import urllib.request
+
+os.makedirs('data/temp/yahoo', exist_ok=True)
+
+def download_file_to_dir(url, dst_dir):
+    urllib.request.urlretrieve(url, os.path.join(dst_dir, os.path.basename(url)))
 
 from bs4 import BeautifulSoup
 
 url = 'https://news.yahoo.co.jp/list/'
-ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '\
+ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '\
      'AppleWebKit/537.36 (KHTML, like Gecko) '\
-     'Chrome/55.0.2883.95 Safari/537.36 '
+     'Chrome/116.0.0.0 Safari/537.36'
 
 req = urllib.request.Request(url, headers={'User-Agent': ua})
 html = urllib.request.urlopen(req)
 
 soup = BeautifulSoup(html, "html.parser")
 
-url_list = [img.get('data-src') for img in soup.find(class_='list').find_all('img')]
-
-def download_file(url, dst_path):
-    try:
-        with urllib.request.urlopen(url) as web_file, open(dst_path, 'wb') as local_file:
-            local_file.write(web_file.read())
-    except urllib.error.URLError as e:
-        print(e)
-
-def download_file_to_dir(url, dst_dir):
-    download_file(url, os.path.join(dst_dir, os.path.basename(url)))
-
-download_dir = 'data/temp'
+dst_dir = 'data/temp/yahoo'
 sleep_time_sec = 1
 
-for url in url_list:
-#     print(url)
-#     download_file_to_dir(url, download_dir)
+for img in soup.find(class_='newsFeed_list').find_all('img'):
+    url = img.get('src')
+    download_file_to_dir(url, dst_dir)
     time.sleep(sleep_time_sec)
--- notebook/urllib_request_file.py
+++ notebook/urllib_request_file.py
@@ -1,65 +1,86 @@
-import os
-import pprint
-import time
-import urllib.error
 import urllib.request
-
-def download_file(url, dst_path):
-    try:
-        with urllib.request.urlopen(url) as web_file:
-            data = web_file.read()
-            with open(dst_path, mode='wb') as local_file:
-                local_file.write(data)
-    except urllib.error.URLError as e:
-        print(e)
-
-def download_file(url, dst_path):
-    try:
-        with urllib.request.urlopen(url) as web_file, open(dst_path, 'wb') as local_file:
-            local_file.write(web_file.read())
-    except urllib.error.URLError as e:
-        print(e)
 
 url = 'https://www.python.org/static/img/python-logo.png'
 dst_path = 'data/temp/py-logo.png'
+
+urllib.request.urlretrieve(url, dst_path)
+# ('data/temp/py-logo.png', <http.client.HTTPMessage at 0x102199550>)
+
+def download_file(url, dst_path):
+    with urllib.request.urlopen(url) as web_file:
+        with open(dst_path, 'wb') as local_file:
+            local_file.write(web_file.read())
+
+url = 'https://www.python.org/static/img/python-logo.png'
+dst_path = 'data/temp/py-logo.png'
+
 download_file(url, dst_path)
+
+def download_file(url, dst_path):
+    with urllib.request.urlopen(url) as web_file, open(dst_path, 'wb') as local_file:
+        local_file.write(web_file.read())
+
+download_file(url, dst_path)
+
+import os
 
 def download_file_to_dir(url, dst_dir):
     download_file(url, os.path.join(dst_dir, os.path.basename(url)))
 
+url = 'https://www.python.org/static/img/python-logo.png'
 dst_dir = 'data/temp'
+
 download_file_to_dir(url, dst_dir)
 
-url_error = 'https://www.python.org/static/img/python-logo_xxx.png'
-download_file_to_dir(url_error, dst_dir)
+url_error = 'https://www.python.org/static/img/python-logo-abc.png'
+dst_path = 'data/temp/py-logo.png'
+
+# download_file(url_error, dst_path)
+# HTTPError: HTTP Error 404: Not Found
+
+import urllib.error
+
+try:
+    download_file(url_error, dst_path)
+except urllib.error.HTTPError as e:
+    print(e)
 # HTTP Error 404: Not Found
+
+url = 'https://www.python.org/static/img/python-logo.png'
+dst_path_error = 'data/not_exist/py-logo.png'
+
+try:
+    download_file(url, dst_path_error)
+except (urllib.error.HTTPError, FileNotFoundError) as e:
+    print(e)
+# [Errno 2] No such file or directory: 'data/not_exist/py-logo.png'
 
 url_zip = 'https://github.com/nkmk/python-snippets/raw/master/notebook/data/src/sample_header.csv.zip'
 download_file_to_dir(url_zip, dst_dir)
 
-url_xlsx = 'https://github.com/nkmk/python-snippets/raw/master/notebook/data/src/sample.xlsx'
-download_file_to_dir(url_xlsx, dst_dir)
-
 url_pdf = 'https://github.com/nkmk/python-snippets/raw/master/notebook/data/src/pdf/sample1.pdf'
 download_file_to_dir(url_pdf, dst_dir)
 
-url_list = ['https://example.com/basedir/base_{:03}.jpg'.format(i) for i in range(5)]
-pprint.pprint(url_list)
-# ['https://example.com/basedir/base_000.jpg',
-#  'https://example.com/basedir/base_001.jpg',
-#  'https://example.com/basedir/base_002.jpg',
-#  'https://example.com/basedir/base_003.jpg',
-#  'https://example.com/basedir/base_004.jpg']
+import time
 
-download_dir = 'data/temp'
+url_list = ['url_0', 'url_1', 'url_2', 'url_3', 'url_4']
+dst_dir = 'data/temp'
 sleep_time_sec = 1
 
 for url in url_list:
-    print(url)
-#     download_file_dir(url, download_dir)
+#     download_file_to_dir(url, dst_dir)
     time.sleep(sleep_time_sec)
-# https://example.com/basedir/base_000.jpg
-# https://example.com/basedir/base_001.jpg
-# https://example.com/basedir/base_002.jpg
-# https://example.com/basedir/base_003.jpg
-# https://example.com/basedir/base_004.jpg
+
+dst_dir = 'data/temp'
+sleep_time_sec = 1
+
+for i in range(5):
+    url = f'https://example.com/dir/base_{i:03}.jpg'
+    print(url)
+#     download_file_to_dir(url, dst_dir)
+    time.sleep(sleep_time_sec)
+# https://example.com/dir/base_000.jpg
+# https://example.com/dir/base_001.jpg
+# https://example.com/dir/base_002.jpg
+# https://example.com/dir/base_003.jpg
+# https://example.com/dir/base_004.jpg
