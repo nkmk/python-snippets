6968b50f2294bd3b10b52cc865c1b14b4cfc649e
nkmk <nkmk.on@gmail.com>
2023-10-05 23:07:50 +0900

    update split strip list

--- notebook/split_strip_list.py
+++ notebook/split_strip_list.py
@@ -57,7 +57,7 @@ s = '-+- one -+-'
 print(s.strip('-+ '))
 # one
 
-s = 'one, two, three'
+s = 'one, two,  three'
 l = [x.strip() for x in s.split(',')]
 print(l)
 # ['one', 'two', 'three']
@@ -65,40 +65,32 @@ print(l)
 s = ''
 l = [x.strip() for x in s.split(',')]
 print(l)
-print(len(l))
 # ['']
-# 1
 
 s = ''
 l = [x.strip() for x in s.split(',') if not s == '']
 print(l)
-print(len(l))
 # []
-# 0
 
-s = 'one, , three'
+s = 'one, ,  three'
 l = [x.strip() for x in s.split(',')]
 print(l)
-print(len(l))
 # ['one', '', 'three']
-# 3
 
-s = 'one, ,three'
+s = 'one, ,  three'
 l = [x.strip() for x in s.split(',') if not x.strip() == '']
 print(l)
-print(len(l))
 # ['one', 'three']
-# 2
 
-s = '1, 2, 3, 4'
+s = '1, 2,  3,   4'
 l = [x.strip() for x in s.split(',')]
 print(l)
 print(type(l[0]))
 # ['1', '2', '3', '4']
 # <class 'str'>
 
-s = '1, 2, 3, 4'
-l = [int(x.strip()) for x in s.split(',')]
+s = '1, 2,  3,   4'
+l = [int(x) for x in s.split(',')]
 print(l)
 print(type(l[0]))
 # [1, 2, 3, 4]
