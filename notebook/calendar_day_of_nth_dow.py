2b481fae609d722290a8a0cfa9bf3566e941c558
nkmk <nkmk.on@gmail.com>
2023-10-11 21:19:52 +0900

    update nth dow to date

--- notebook/calendar_day_of_nth_dow.py
+++ notebook/calendar_day_of_nth_dow.py
@@ -5,14 +5,14 @@ def get_day_of_nth_dow(year, month, nth, dow):
     '''dow: Monday(0) - Sunday(6)'''
     if nth < 1 or dow < 0 or dow > 6:
         return None
-    
+
     first_dow, n = calendar.monthrange(year, month)
     day = 7 * (nth - 1) + (dow - first_dow) % 7 + 1
-    
+
     return day if day <= n else None
 
-print(calendar.month(2019, 1))
-#     January 2019
+print(calendar.month(2023, 8))
+#     August 2023
 # Mo Tu We Th Fr Sa Su
 #     1  2  3  4  5  6
 #  7  8  9 10 11 12 13
@@ -21,45 +21,45 @@ print(calendar.month(2019, 1))
 # 28 29 30 31
 # 
 
-print(get_day_of_nth_dow(2019, 1, 1, 1))  # 1st Tuesday(1)
+print(get_day_of_nth_dow(2023, 8, 1, 1))  # 1st Tuesday(1)
 # 1
 
-print(get_day_of_nth_dow(2019, 1, 2, 0))  # 2nd Monday(0)
+print(get_day_of_nth_dow(2023, 8, 2, 0))  # 2nd Monday(0)
 # 14
 
-print(get_day_of_nth_dow(2019, 1, 3, 6))  # 3rd Sunday(6)
+print(get_day_of_nth_dow(2023, 8, 3, 6))  # 3rd Sunday(6)
 # 20
 
-print(get_day_of_nth_dow(2019, 1, 5, 3))  # 5th Thursday(3)
+print(get_day_of_nth_dow(2023, 8, 5, 3))  # 5th Thursday(3)
 # 31
 
-print(get_day_of_nth_dow(2019, 1, 5, 4))
+print(get_day_of_nth_dow(2023, 8, 5, 4))
 # None
 
-print(get_day_of_nth_dow(2019, 1, 0, 4))
+print(get_day_of_nth_dow(2023, 8, 0, 4))
 # None
 
-print(get_day_of_nth_dow(2019, 1, 1, 10))
+print(get_day_of_nth_dow(2023, 8, 1, 10))
 # None
 
-print(get_day_of_nth_dow(2019, 1, 2, 1.8))
+print(get_day_of_nth_dow(2023, 8, 2, 1.8))
 # 8.8
 
 def get_date_of_nth_dow(year, month, nth, dow):
     day = get_day_of_nth_dow(year, month, nth, dow)
     return datetime.date(year, month, day) if day else None
 
-print(get_date_of_nth_dow(2019, 1, 1, 1))
-# 2019-01-01
+print(get_date_of_nth_dow(2023, 8, 1, 1))
+# 2023-08-01
 
-print(get_date_of_nth_dow(2019, 1, 1, 10))
+print(get_date_of_nth_dow(2023, 8, 1, 10))
 # None
 
-# print(get_date_of_nth_dow(2019, 1, 2, 1.8))
-# TypeError: integer argument expected, got float
+# print(get_date_of_nth_dow(2023, 8, 2, 1.8))
+# TypeError: 'float' object cannot be interpreted as an integer
 
-print([(m, get_day_of_nth_dow(2019, m, 2, 0)) for m in range(1, 13)])
-# [(1, 14), (2, 11), (3, 11), (4, 8), (5, 13), (6, 10), (7, 8), (8, 12), (9, 9), (10, 14), (11, 11), (12, 9)]
+print([(m, get_day_of_nth_dow(2023, m, 2, 0)) for m in range(1, 13)])
+# [(1, 9), (2, 13), (3, 13), (4, 10), (5, 8), (6, 12), (7, 10), (8, 14), (9, 11), (10, 9), (11, 13), (12, 11)]
 
 for y in range(2020, 2030):
     print(get_date_of_nth_dow(y, 1, 2, 0))
