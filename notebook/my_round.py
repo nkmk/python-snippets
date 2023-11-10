80ee7bbe5acd269b23daaeec8ae91c8cdf53226b
nkmk <nkmk.on@gmail.com>
2023-11-10 17:04:58 +0900

    update round

--- notebook/my_round.py
+++ notebook/my_round.py
@@ -1,15 +1,16 @@
-def my_round(val, digit=0):
-    p = 10 ** digit
-    return (val * p * 2 + 1) // 2 / p
+def my_round(number, ndigits=0):
+    p = 10**ndigits
+    return (number * p * 2 + 1) // 2 / p
 
-my_round_int = lambda x: int((x * 2 + 1) // 2)
+def my_round_int(number):
+    return int((number * 2 + 1) // 2)
 
 f = 123.456
 
-print(int(my_round(f)))
-# 123
+print(my_round(f))
+# 123.0
 
-print(my_round_int(f))
+print(int(my_round(f)))
 # 123
 
 print(my_round(f, 1))
@@ -18,12 +19,15 @@ print(my_round(f, 1))
 print(my_round(f, 2))
 # 123.46
 
-print(int(my_round(0.4)))
-print(int(my_round(0.5)))
-print(int(my_round(0.6)))
-# 0
-# 1
-# 1
+print(my_round_int(f))
+# 123
+
+print('0.4 =>', int(my_round(0.4)))
+print('0.5 =>', int(my_round(0.5)))
+print('0.6 =>', int(my_round(0.6)))
+# 0.4 => 0
+# 0.5 => 1
+# 0.6 => 1
 
 i = 99518
 
@@ -36,30 +40,29 @@ print(int(my_round(i, -2)))
 print(int(my_round(i, -3)))
 # 100000
 
-print(int(my_round(4, -1)))
-print(int(my_round(5, -1)))
-print(int(my_round(6, -1)))
-# 0
-# 10
-# 10
+print('4 =>', int(my_round(4, -1)))
+print('5 =>', int(my_round(5, -1)))
+print('6 =>', int(my_round(6, -1)))
+# 4 => 0
+# 5 => 10
+# 6 => 10
 
-print(int(my_round(-0.4)))
-print(int(my_round(-0.5)))
-print(int(my_round(-0.6)))
-# 0
-# 0
-# -1
+print('-0.4 =>', int(my_round(-0.4)))
+print('-0.5 =>', int(my_round(-0.5)))
+print('-0.6 =>', int(my_round(-0.6)))
+# -0.4 => 0
+# -0.5 => 0
+# -0.6 => -1
 
 import math
 
-def my_round2(val, digit=0):
-    p = 10 ** digit
-    s = math.copysign(1, val)
-    return (s * val * p * 2 + 1) // 2 / p * s
+def my_round2(number, ndigits=0):
+    p = 10**ndigits
+    return (abs(number) * p * 2 + 1) // 2 / p * math.copysign(1, number)
 
-print(int(my_round2(-0.4)))
-print(int(my_round2(-0.5)))
-print(int(my_round2(-0.6)))
-# 0
-# -1
-# -1
+print('-0.4 =>', int(my_round2(-0.4)))
+print('-0.5 =>', int(my_round2(-0.5)))
+print('-0.6 =>', int(my_round2(-0.6)))
+# -0.4 => 0
+# -0.5 => -1
+# -0.6 => -1
