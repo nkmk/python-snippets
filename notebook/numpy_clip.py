fc7793f9594084ef0fbf9b6d435f28e46cec3795
nkmk <nkmk.on@gmail.com>
2023-11-16 16:48:56 +0900

    update numpy.clip

--- notebook/numpy_clip.py
+++ notebook/numpy_clip.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+print(np.__version__)
+# 1.26.1
+
 a = np.arange(10)
 print(a)
 # [0 1 2 3 4 5 6 7 8 9]
@@ -16,12 +19,25 @@ print(np.clip(a, 2, None))
 # print(np.clip(a, 2))
 # TypeError: clip() missing 1 required positional argument: 'a_max'
 
+print(np.clip(a, 7, 2))
+# [2 2 2 2 2 2 2 2 2 2]
+
 a_clip = np.clip(a, 2, 7)
 print(a_clip)
 # [2 2 2 3 4 5 6 7 7 7]
 
 print(a)
 # [0 1 2 3 4 5 6 7 8 9]
+
+l = list(range(10))
+print(l)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(np.clip(l, 2, 7))
+# [2 2 2 3 4 5 6 7 7 7]
+
+print(type(np.clip(l, 2, 7)))
+# <class 'numpy.ndarray'>
 
 print(a.clip(2, 7))
 # [2 2 2 3 4 5 6 7 7 7]
@@ -40,6 +56,9 @@ print(a.clip(min=2))
 
 print(a.clip(max=7))
 # [0 1 2 3 4 5 6 7 7 7]
+
+print(a.clip(7, 2))
+# [2 2 2 2 2 2 2 2 2 2]
 
 a_clip = a.clip(2, 7)
 print(a_clip)
