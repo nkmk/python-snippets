2a4597b0986276a982467fa59457e61cc8b88054
nkmk <nkmk.on@gmail.com>
2023-11-25 17:51:31 +0900

    update NumPy remove nan

--- notebook/numpy_nan_remove.py
+++ notebook/numpy_nan_remove.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+print(np.__version__)
+# 1.26.1
+
 a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
 print(a)
 # [[11. 12. nan 14.]
@@ -31,6 +34,37 @@ print(a[~np.isnan(a).any(axis=1), :])
 print(a[~np.isnan(a).any(axis=1)])
 # [[31. 32. 33. 34.]]
 
+a[1, 0] = np.nan
+a[1, 3] = np.nan
+print(a)
+# [[11. 12. nan 14.]
+#  [nan nan nan nan]
+#  [31. 32. 33. 34.]]
+
+print(np.isnan(a).all(axis=1))
+# [False  True False]
+
+print(~np.isnan(a).all(axis=1))
+# [ True False  True]
+
+print(a[~np.isnan(a).all(axis=1)])
+# [[11. 12. nan 14.]
+#  [31. 32. 33. 34.]]
+
+a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
+print(a)
+# [[11. 12. nan 14.]
+#  [21. nan nan 24.]
+#  [31. 32. 33. 34.]]
+
+print(np.isnan(a))
+# [[False False  True False]
+#  [False  True  True False]
+#  [False False False False]]
+
+print(np.isnan(a).any(axis=0))
+# [False  True  True False]
+
 print(~np.isnan(a).any(axis=0))
 # [ True False False  True]
 
@@ -39,17 +73,17 @@ print(a[:, ~np.isnan(a).any(axis=0)])
 #  [21. 24.]
 #  [31. 34.]]
 
-a = np.genfromtxt('data/src/sample_nan.csv', delimiter=',')
 a[2, 2] = np.nan
 print(a)
 # [[11. 12. nan 14.]
 #  [21. nan nan 24.]
 #  [31. 32. nan 34.]]
 
-print(a[:, ~np.isnan(a).any(axis=0)])
-# [[11. 14.]
-#  [21. 24.]
-#  [31. 34.]]
+print(np.isnan(a).all(axis=0))
+# [False False  True False]
+
+print(~np.isnan(a).all(axis=0))
+# [ True  True False  True]
 
 print(a[:, ~np.isnan(a).all(axis=0)])
 # [[11. 12. 14.]
