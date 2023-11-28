7c6a1739ed09f192a298bd72354b3d38bca28680
nkmk <nkmk.on@gmail.com>
2023-11-28 20:33:03 +0900

    update round, decimal

--- notebook/decimal_quantize.py
+++ notebook/decimal_quantize.py
@@ -9,92 +9,64 @@ print(type(Decimal(0.05)))
 print(Decimal(0.5))
 # 0.5
 
+print(Decimal(0.25))
+# 0.25
+
 print(Decimal('0.05'))
+# 0.05
+
+print(Decimal(str(0.05)))
 # 0.05
 
 f = 123.456
 
-print(Decimal(str(f)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+print(Decimal(str(f)).quantize(Decimal('0'), ROUND_HALF_UP))
+print(Decimal(str(f)).quantize(Decimal('0.1'), ROUND_HALF_UP))
+print(Decimal(str(f)).quantize(Decimal('0.01'), ROUND_HALF_UP))
 # 123
-
-print(Decimal(str(f)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP))
 # 123.5
-
-print(Decimal(str(f)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP))
 # 123.46
 
-print('0.4 =>', Decimal(str(0.4)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
-print('0.5 =>', Decimal(str(0.5)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
-print('0.6 =>', Decimal(str(0.6)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+print('0.4 =>', Decimal(str(0.4)).quantize(Decimal('0'), ROUND_HALF_UP))
+print('0.5 =>', Decimal(str(0.5)).quantize(Decimal('0'), ROUND_HALF_UP))
+print('0.6 =>', Decimal(str(0.6)).quantize(Decimal('0'), ROUND_HALF_UP))
 # 0.4 => 0
 # 0.5 => 1
 # 0.6 => 1
 
-print('0.05 =>', round(0.05, 1))
-print('0.15 =>', round(0.15, 1))
-print('0.25 =>', round(0.25, 1))
-print('0.35 =>', round(0.35, 1))
-print('0.45 =>', round(0.45, 1))
-# 0.05 => 0.1
-# 0.15 => 0.1
-# 0.25 => 0.2
-# 0.35 => 0.3
-# 0.45 => 0.5
-
-print('0.05 =>', Decimal(0.05).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.15 =>', Decimal(0.15).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.25 =>', Decimal(0.25).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.35 =>', Decimal(0.35).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.45 =>', Decimal(0.45).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-# 0.05 => 0.1
-# 0.15 => 0.1
-# 0.25 => 0.2
-# 0.35 => 0.3
-# 0.45 => 0.5
-
-print('0.05 =>', Decimal(str(0.05)).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.15 =>', Decimal(str(0.15)).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.25 =>', Decimal(str(0.25)).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.35 =>', Decimal(str(0.35)).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
-print('0.45 =>', Decimal(str(0.45)).quantize(Decimal('0.1'), rounding=ROUND_HALF_EVEN))
+print('0.05 =>', Decimal(str(0.05)).quantize(Decimal('0.1'), ROUND_HALF_EVEN))
+print('0.15 =>', Decimal(str(0.15)).quantize(Decimal('0.1'), ROUND_HALF_EVEN))
+print('0.25 =>', Decimal(str(0.25)).quantize(Decimal('0.1'), ROUND_HALF_EVEN))
+print('0.35 =>', Decimal(str(0.35)).quantize(Decimal('0.1'), ROUND_HALF_EVEN))
+print('0.45 =>', Decimal(str(0.45)).quantize(Decimal('0.1'), ROUND_HALF_EVEN))
 # 0.05 => 0.0
 # 0.15 => 0.2
 # 0.25 => 0.2
 # 0.35 => 0.4
 # 0.45 => 0.4
 
-print(Decimal(2.675))
-# 2.67499999999999982236431605997495353221893310546875
-
-print(Decimal(2.675).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP))
-# 2.67
-
-print(Decimal(str(2.675)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP))
-# 2.68
-
-print(Decimal(2.675).quantize(Decimal('0.01'), rounding=ROUND_HALF_EVEN))
-# 2.67
-
-print(Decimal(str(2.675)).quantize(Decimal('0.01'), rounding=ROUND_HALF_EVEN))
-# 2.68
-
-d = Decimal('123.456').quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-
+d = Decimal('123.456').quantize(Decimal('0.01'), ROUND_HALF_UP)
 print(d)
 # 123.46
 
 print(type(d))
 # <class 'decimal.Decimal'>
 
-# print(1.2 + d)
-# TypeError: unsupported operand type(s) for +: 'float' and 'decimal.Decimal'
+f = float(d)
+print(f)
+# 123.46
 
-print(1.2 + float(d))
-# 124.66
+print(type(f))
+# <class 'float'>
+
+print(Decimal(f))
+# 123.4599999999999937472239253111183643341064453125
+
+print(Decimal(str(f)))
+# 123.46
 
 i = 99518
-
-print(Decimal(i).quantize(Decimal('10'), rounding=ROUND_HALF_UP))
+print(Decimal(i).quantize(Decimal('10'), ROUND_HALF_UP))
 # 99518
 
 print(Decimal('10').as_tuple())
@@ -103,21 +75,30 @@ print(Decimal('10').as_tuple())
 print(Decimal('1E1').as_tuple())
 # DecimalTuple(sign=0, digits=(1,), exponent=1)
 
-print(Decimal(i).quantize(Decimal('1E1'), rounding=ROUND_HALF_UP))
+print(Decimal(i).quantize(Decimal('1E1'), ROUND_HALF_UP))
+print(Decimal(i).quantize(Decimal('1E2'), ROUND_HALF_UP))
+print(Decimal(i).quantize(Decimal('1E3'), ROUND_HALF_UP))
 # 9.952E+4
+# 9.95E+4
+# 1.00E+5
 
-print(int(Decimal(i).quantize(Decimal('1E1'), rounding=ROUND_HALF_UP)))
-# 99520
+def remove_exponent(d):
+    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
 
-print(int(Decimal(i).quantize(Decimal('1E2'), rounding=ROUND_HALF_UP)))
+d = Decimal(i).quantize(Decimal('1E2'), ROUND_HALF_UP)
+print(d)
+# 9.95E+4
+
+d_remove = remove_exponent(d)
+print(d_remove)
 # 99500
 
-print(int(Decimal(i).quantize(Decimal('1E3'), rounding=ROUND_HALF_UP)))
-# 100000
+print(type(d_remove))
+# <class 'decimal.Decimal'>
 
-print('4 =>', int(Decimal(4).quantize(Decimal('1E1'), rounding=ROUND_HALF_UP)))
-print('5 =>', int(Decimal(5).quantize(Decimal('1E1'), rounding=ROUND_HALF_UP)))
-print('6 =>', int(Decimal(6).quantize(Decimal('1E1'), rounding=ROUND_HALF_UP)))
-# 4 => 0
-# 5 => 10
-# 6 => 10
+i = int(d)
+print(i)
+# 99500
+
+print(type(i))
+# <class 'int'>
