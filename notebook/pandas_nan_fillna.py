9dff2824a8b874a44614520ec44438d8971bf36a
nkmk <nkmk.on@gmail.com>
2023-12-10 15:56:29 +0900

    update pandas fillna, ffill, bfill

--- notebook/pandas_nan_fillna.py
+++ notebook/pandas_nan_fillna.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 print(pd.__version__)
-# 2.0.3
+# 2.1.4
 
 df = pd.read_csv('data/src/sample_pandas_normal_nan.csv')
 print(df)
@@ -80,7 +80,7 @@ print(df.fillna(df.mode().iloc[0]))
 # 4    Ellen  24.0    CA   88.0    NaN
 # 5    Frank  30.0    CA   70.0    NaN
 
-print(df.fillna(method='ffill'))
+print(df.ffill())
 #       name   age state  point  other
 # 0    Alice  24.0    NY    NaN    NaN
 # 1    Alice  24.0    NY    NaN    NaN
@@ -89,7 +89,7 @@ print(df.fillna(method='ffill'))
 # 4    Ellen  68.0    CA   88.0    NaN
 # 5    Frank  30.0    CA   88.0    NaN
 
-print(df.fillna(method='bfill'))
+print(df.bfill())
 #       name   age state  point  other
 # 0    Alice  24.0    NY   70.0    NaN
 # 1  Charlie  68.0    CA   70.0    NaN
@@ -98,47 +98,11 @@ print(df.fillna(method='bfill'))
 # 4    Ellen  30.0    CA   88.0    NaN
 # 5    Frank  30.0   NaN    NaN    NaN
 
-print(df.fillna(method='ffill', limit=1))
+print(df.ffill(limit=1))
 #       name   age state  point  other
 # 0    Alice  24.0    NY    NaN    NaN
 # 1    Alice  24.0    NY    NaN    NaN
 # 2  Charlie   NaN    CA    NaN    NaN
-# 3     Dave  68.0    TX   70.0    NaN
-# 4    Ellen  68.0    CA   88.0    NaN
-# 5    Frank  30.0    CA   88.0    NaN
-
-print(df.fillna(method='bfill', limit=1))
-#       name   age state  point  other
-# 0    Alice  24.0    NY    NaN    NaN
-# 1  Charlie   NaN    CA    NaN    NaN
-# 2  Charlie  68.0    CA   70.0    NaN
-# 3     Dave  68.0    TX   70.0    NaN
-# 4    Ellen  30.0    CA   88.0    NaN
-# 5    Frank  30.0   NaN    NaN    NaN
-
-print(df.fillna(method='ffill', axis=1))
-#       name      age state point other
-# 0    Alice     24.0    NY    NY    NY
-# 1      NaN      NaN   NaN   NaN   NaN
-# 2  Charlie  Charlie    CA    CA    CA
-# 3     Dave     68.0    TX  70.0  70.0
-# 4    Ellen    Ellen    CA  88.0  88.0
-# 5    Frank     30.0  30.0  30.0  30.0
-
-print(df.fillna(method='bfill', axis=1))
-#       name   age state point other
-# 0    Alice  24.0    NY   NaN   NaN
-# 1      NaN   NaN   NaN   NaN   NaN
-# 2  Charlie    CA    CA   NaN   NaN
-# 3     Dave  68.0    TX  70.0   NaN
-# 4    Ellen    CA    CA  88.0   NaN
-# 5    Frank  30.0   NaN   NaN   NaN
-
-print(df.ffill())
-#       name   age state  point  other
-# 0    Alice  24.0    NY    NaN    NaN
-# 1    Alice  24.0    NY    NaN    NaN
-# 2  Charlie  24.0    CA    NaN    NaN
 # 3     Dave  68.0    TX   70.0    NaN
 # 4    Ellen  68.0    CA   88.0    NaN
 # 5    Frank  30.0    CA   88.0    NaN
@@ -151,6 +115,36 @@ print(df.bfill(limit=1))
 # 3     Dave  68.0    TX   70.0    NaN
 # 4    Ellen  30.0    CA   88.0    NaN
 # 5    Frank  30.0   NaN    NaN    NaN
+
+print(df.ffill(axis=1))
+#       name      age state point other
+# 0    Alice     24.0    NY    NY    NY
+# 1      NaN      NaN   NaN   NaN   NaN
+# 2  Charlie  Charlie    CA    CA    CA
+# 3     Dave     68.0    TX  70.0  70.0
+# 4    Ellen    Ellen    CA  88.0  88.0
+# 5    Frank     30.0  30.0  30.0  30.0
+
+print(df.bfill(axis=1))
+#       name   age state point other
+# 0    Alice  24.0    NY   NaN   NaN
+# 1      NaN   NaN   NaN   NaN   NaN
+# 2  Charlie    CA    CA   NaN   NaN
+# 3     Dave  68.0    TX  70.0   NaN
+# 4    Ellen    CA    CA  88.0   NaN
+# 5    Frank  30.0   NaN   NaN   NaN
+
+print(df.fillna(method='ffill', limit=1))
+#       name   age state  point  other
+# 0    Alice  24.0    NY    NaN    NaN
+# 1    Alice  24.0    NY    NaN    NaN
+# 2  Charlie   NaN    CA    NaN    NaN
+# 3     Dave  68.0    TX   70.0    NaN
+# 4    Ellen  68.0    CA   88.0    NaN
+# 5    Frank  30.0    CA   88.0    NaN
+# 
+# /var/folders/rf/b7l8_vgj5mdgvghn_326rn_c0000gn/T/ipykernel_50534/2498159999.py:1: FutureWarning: DataFrame.fillna with 'method' is deprecated and will raise in a future version. Use obj.ffill() or obj.bfill() instead.
+#   print(df.fillna(method='ffill', limit=1))
 
 df.fillna(0, inplace=True)
 print(df)
@@ -190,12 +184,12 @@ print(s.fillna({1: 100, 4: -100}))
 # 5     30.0
 # Name: age, dtype: float64
 
-print(s.fillna(method='bfill', limit=1))
+print(s.ffill(limit=1))
 # 0    24.0
-# 1     NaN
-# 2    68.0
+# 1    24.0
+# 2     NaN
 # 3    68.0
-# 4    30.0
+# 4    68.0
 # 5    30.0
 # Name: age, dtype: float64
 
@@ -207,3 +201,15 @@ print(s.bfill(limit=1))
 # 4    30.0
 # 5    30.0
 # Name: age, dtype: float64
+
+print(s.fillna(method='ffill', limit=1))
+# 0    24.0
+# 1    24.0
+# 2     NaN
+# 3    68.0
+# 4    68.0
+# 5    30.0
+# Name: age, dtype: float64
+# 
+# /var/folders/rf/b7l8_vgj5mdgvghn_326rn_c0000gn/T/ipykernel_50534/2241812369.py:1: FutureWarning: Series.fillna with 'method' is deprecated and will raise in a future version. Use obj.ffill() or obj.bfill() instead.
+#   print(s.fillna(method='ffill', limit=1))
