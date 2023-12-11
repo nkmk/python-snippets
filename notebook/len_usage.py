2c74ee691dcd4120492d891c667407cf1731a394
nkmk <nkmk.on@gmail.com>
2023-12-11 20:13:52 +0900

    update len

--- notebook/len_usage.py
+++ notebook/len_usage.py
@@ -13,7 +13,7 @@ s = {0, 1, 2}
 print(len(s))
 # 3
 
-d = {'key0': 0, 'key1': 1, 'key2': 2}
+d = {'key_0': 0, 'key_1': 1, 'key_2': 2}
 
 print(len(d))
 # 3
@@ -28,68 +28,29 @@ s = 'あいうえお'
 print(len(s))
 # 5
 
-import numpy as np
+l = [0, 1, 2]
 
-a_1d = np.arange(3)
-
-print(a_1d)
-# [0 1 2]
-
-print(len(a_1d))
+print(l.__len__())
 # 3
 
-a_2d = np.arange(12).reshape((3, 4))
+s = 'abcde'
 
-print(a_2d)
-# [[ 0  1  2  3]
-#  [ 4  5  6  7]
-#  [ 8  9 10 11]]
+print(s.__len__())
+# 5
 
-print(len(a_2d))
-# 3
+i = 100
 
-a_3d = np.arange(24).reshape((2, 3, 4))
+print(hasattr(i, '__len__'))
+# False
 
-print(a_3d)
-# [[[ 0  1  2  3]
-#   [ 4  5  6  7]
-#   [ 8  9 10 11]]
-#  [[12 13 14 15]
-#   [16 17 18 19]
-#   [20 21 22 23]]]
-
-print(len(a_3d))
-# 2
-
-import pandas as pd
-
-df = pd.DataFrame({'A': [0, 1, 2], 'B': [3, 4, 5]}, index=['a', 'b', 'c'])
-
-print(df)
-#    A  B
-# a  0  3
-# b  1  4
-# c  2  5
-
-print(len(df))
-# 3
-
-s = pd.Series([0, 1, 2], index=['a', 'b', 'c'])
-
-print(s)
-# a    0
-# b    1
-# c    2
-# dtype: int64
-
-print(len(s))
-# 3
-
-# print(len(100))
+# print(len(i))
 # TypeError: object of type 'int' has no len()
 
-# print(len(0.1))
-# TypeError: object of type 'float' has no len()
+class MyClass:
+    def __len__(self):
+        return 100
 
-# print(len(True))
-# TypeError: object of type 'bool' has no len()
+mc = MyClass()
+
+print(len(mc))
+# 100
