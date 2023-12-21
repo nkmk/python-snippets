a07c9bee6733099b5486d984224ba19c295dbc46
nkmk <nkmk.on@gmail.com>
2023-12-21 19:54:39 +0900

    update pandas select_dtypes

--- notebook/pandas_select_dtypes.py
+++ notebook/pandas_select_dtypes.py
@@ -1,95 +1,118 @@
 import pandas as pd
+import numpy as np
+
+print(pd.__version__)
+# 2.1.4
+
+print(np.__version__)
+# 1.26.2
 
 df = pd.DataFrame({'a': [1, 2, 3],
-                   'b': [0.1, 0.2, 0.3],
-                   'c': ['X', 'Y', 'Z'],
-                   'd': [[0, 0], [1, 1], [2, 2]],
-                   'e': [True, True, False]})
-
-df['f'] = pd.to_datetime(['2018-01-01', '2018-02-01', '2018-03-01'])
-
+                   'b': np.array([10, 20, 30], dtype=np.int32),
+                   'c': [0.1, 0.2, 0.3],
+                   'd': ['X', 'Y', 'Z'],
+                   'e': [[0, 0], [1, 1], [2, 2]],
+                   'f': [True, True, False],
+                   'g': pd.to_datetime(['2023-12-01', '2023-12-02', '2023-12-03'])})
 print(df)
-#    a    b  c       d      e          f
-# 0  1  0.1  X  [0, 0]   True 2018-01-01
-# 1  2  0.2  Y  [1, 1]   True 2018-02-01
-# 2  3  0.3  Z  [2, 2]  False 2018-03-01
+#    a   b    c  d       e      f          g
+# 0  1  10  0.1  X  [0, 0]   True 2023-12-01
+# 1  2  20  0.2  Y  [1, 1]   True 2023-12-02
+# 2  3  30  0.3  Z  [2, 2]  False 2023-12-03
 
 print(df.dtypes)
 # a             int64
-# b           float64
-# c            object
+# b             int32
+# c           float64
 # d            object
-# e              bool
-# f    datetime64[ns]
+# e            object
+# f              bool
+# g    datetime64[ns]
 # dtype: object
 
 print(df.select_dtypes(include=int))
-#    a
-# 0  1
-# 1  2
-# 2  3
+#    a   b
+# 0  1  10
+# 1  2  20
+# 2  3  30
 
-print(df.select_dtypes(include='int'))
-#    a
-# 0  1
-# 1  2
-# 2  3
+print(df.select_dtypes(include=['int32', bool]))
+#     b      f
+# 0  10   True
+# 1  20   True
+# 2  30  False
 
-print(df.select_dtypes(include='int64'))
-#    a
-# 0  1
-# 1  2
-# 2  3
-
-print(df.select_dtypes(include='int32'))
+print(df.select_dtypes(include='float32'))
 # Empty DataFrame
 # Columns: []
 # Index: [0, 1, 2]
 
-print(df.select_dtypes(include=[float, bool, 'datetime']))
-#      b      e          f
-# 0  0.1   True 2018-01-01
-# 1  0.2   True 2018-02-01
-# 2  0.3  False 2018-03-01
-
 print(df.select_dtypes(include='number'))
-#    a    b
+#    a   b    c
+# 0  1  10  0.1
+# 1  2  20  0.2
+# 2  3  30  0.3
+
+print(df.select_dtypes(exclude=int))
+#      c  d       e      f          g
+# 0  0.1  X  [0, 0]   True 2023-12-01
+# 1  0.2  Y  [1, 1]   True 2023-12-02
+# 2  0.3  Z  [2, 2]  False 2023-12-03
+
+print(df.select_dtypes(exclude=['int32', bool]))
+#    a    c  d       e          g
+# 0  1  0.1  X  [0, 0] 2023-12-01
+# 1  2  0.2  Y  [1, 1] 2023-12-02
+# 2  3  0.3  Z  [2, 2] 2023-12-03
+
+print(df.select_dtypes(include='number', exclude='int32'))
+#    a    c
 # 0  1  0.1
 # 1  2  0.2
 # 2  3  0.3
+
+# print(df.select_dtypes(include=['int32', bool], exclude='int32'))
+# ValueError: include and exclude overlap on frozenset({<class 'numpy.int32'>})
+
+print(df.select_dtypes(include=['i8', 'int32', np.float64]))
+#    a   b    c
+# 0  1  10  0.1
+# 1  2  20  0.2
+# 2  3  30  0.3
+
+print(df.select_dtypes(include=['number', 'datetime']))
+#    a   b    c          g
+# 0  1  10  0.1 2023-12-01
+# 1  2  20  0.2 2023-12-02
+# 2  3  30  0.3 2023-12-03
 
 # print(df.select_dtypes(include=str))
 # TypeError: string dtypes are not allowed, use 'object' instead
 
 print(df.select_dtypes(include=object))
-#    c       d
+#    d       e
 # 0  X  [0, 0]
 # 1  Y  [1, 1]
 # 2  Z  [2, 2]
 
-print(type(df.at[0, 'c']))
+print(type(df.at[0, 'd']))
 # <class 'str'>
 
-print(type(df.at[0, 'd']))
+print(type(df.at[0, 'e']))
 # <class 'list'>
 
-print(df.select_dtypes(exclude='number'))
-#    c       d      e          f
-# 0  X  [0, 0]   True 2018-01-01
-# 1  Y  [1, 1]   True 2018-02-01
-# 2  Z  [2, 2]  False 2018-03-01
+print(df.iloc[0].map(type) == str)
+# a    False
+# b    False
+# c    False
+# d     True
+# e    False
+# f    False
+# g    False
+# Name: 0, dtype: bool
 
-print(df.select_dtypes(exclude=[bool, 'datetime']))
-#    a    b  c       d
-# 0  1  0.1  X  [0, 0]
-# 1  2  0.2  Y  [1, 1]
-# 2  3  0.3  Z  [2, 2]
-
-print(df.select_dtypes(include='number', exclude=int))
-#      b
-# 0  0.1
-# 1  0.2
-# 2  0.3
-
-# print(df.select_dtypes(include=[int, bool], exclude=int))
-# ValueError: include and exclude overlap on frozenset({<class 'numpy.int64'>})
+print(df.loc[:, df.iloc[0].map(type) == str])
+#    d
+# 0  X
+# 1  Y
+# 2  Z
