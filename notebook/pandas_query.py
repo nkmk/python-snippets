842035bb2c429a6736afafc138f7fa9bec7854d5
nkmk <nkmk.on@gmail.com>
2023-12-21 19:54:54 +0900

    update pandas query

--- notebook/pandas_query.py
+++ notebook/pandas_query.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 print(pd.__version__)
-# 2.0.3
+# 2.1.4
 
 df = pd.read_csv('data/src/sample_pandas_normal.csv')
 print(df)
@@ -32,6 +32,15 @@ print(df.query('age < @val'))
 # 2  Charlie   18    CA     70
 # 4    Ellen   24    CA     88
 
+print(f'age < {val}')
+# age < 25
+
+print(df.query(f'age < {val}'))
+#       name  age state  point
+# 0    Alice   24    NY     64
+# 2  Charlie   18    CA     70
+# 4    Ellen   24    CA     88
+
 print(df.query('30 <= age < 50'))
 #     name  age state  point
 # 1    Bob   42    CA     92
@@ -50,6 +59,15 @@ print(df.query('state == "CA"'))
 
 s = 'CA'
 print(df.query('state != @s'))
+#     name  age state  point
+# 0  Alice   24    NY     64
+# 3   Dave   68    TX     70
+# 5  Frank   30    NY     57
+
+# print(df.query(f'state != {s}'))
+# UndefinedVariableError: name 'CA' is not defined
+
+print(df.query(f'state != "{s}"'))
 #     name  age state  point
 # 0  Alice   24    NY     64
 # 3   Dave   68    TX     70
@@ -80,6 +98,12 @@ print(df.query('state in @l'))
 # 3   Dave   68    TX     70
 # 5  Frank   30    NY     57
 
+print(df.query(f'state in {l}'))
+#     name  age state  point
+# 0  Alice   24    NY     64
+# 3   Dave   68    TX     70
+# 5  Frank   30    NY     57
+
 print(df.query('name.str.endswith("e")'))
 #       name  age state  point
 # 0    Alice   24    NY     64
@@ -96,30 +120,29 @@ print(df.query('age.astype("str").str.endswith("8")'))
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
 
-df.at[0, 'name'] = None
-print(df)
+df_nan = df.copy()
+df_nan.at[0, 'name'] = float('NaN')
+print(df_nan)
 #       name  age state  point
-# 0     None   24    NY     64
+# 0      NaN   24    NY     64
 # 1      Bob   42    CA     92
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
 # 4    Ellen   24    CA     88
 # 5    Frank   30    NY     57
 
-# print(df.query('name.str.endswith("e")'))
+# print(df_nan.query('name.str.endswith("e")'))
 # ValueError: unknown type object
 
-print(df[df['name'].str.endswith('e', na=False)])
+print(df_nan[df_nan['name'].str.endswith('e', na=False)])
 #       name  age state  point
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
 
-print(df.query('name.str.endswith("e", na=False)'))
+print(df_nan.query('name.str.endswith("e", na=False)'))
 #       name  age state  point
 # 2  Charlie   18    CA     70
 # 3     Dave   68    TX     70
-
-df = pd.read_csv('data/src/sample_pandas_normal.csv')
 
 print(df.query('index % 2 == 0'))
 #       name  age state  point
@@ -194,8 +217,8 @@ print(df.query('(age == 24 | point > 80) & state == "CA"'))
 # 1    Bob   42    CA     92
 # 4  Ellen   24    CA     88
 
-df.columns = ['0name', 'age.year', 'state name', 3]
-print(df)
+df_rename = df.set_axis(['0name', 'age.year', 'state name', 3], axis=1)
+print(df_rename)
 #      0name  age.year state name   3
 # 0    Alice        24         NY  64
 # 1      Bob        42         CA  92
@@ -204,45 +227,43 @@ print(df)
 # 4    Ellen        24         CA  88
 # 5    Frank        30         NY  57
 
-# print(df.query('0name.str.endswith("e")'))
+# print(df_rename.query('0name.str.endswith("e")'))
 # SyntaxError: invalid syntax
 
-# print(df.query('age.year < 25'))
+# print(df_rename.query('age.year < 25'))
 # UndefinedVariableError: name 'age' is not defined
 
-# print(df.query('state name == "CA"'))
+# print(df_rename.query('state name == "CA"'))
 # SyntaxError: invalid syntax
 
-print(df.query('`0name`.str.endswith("e")'))
+print(df_rename.query('`0name`.str.endswith("e")'))
 #      0name  age.year state name   3
 # 0    Alice        24         NY  64
 # 2  Charlie        18         CA  70
 # 3     Dave        68         TX  70
 
-print(df.query('`age.year` < 25'))
+print(df_rename.query('`age.year` < 25'))
 #      0name  age.year state name   3
 # 0    Alice        24         NY  64
 # 2  Charlie        18         CA  70
 # 4    Ellen        24         CA  88
 
-print(df.query('`state name` == "CA"'))
+print(df_rename.query('`state name` == "CA"'))
 #      0name  age.year state name   3
 # 1      Bob        42         CA  92
 # 2  Charlie        18         CA  70
 # 4    Ellen        24         CA  88
 
-# print(df.query('3 > 75'))
+# print(df_rename.query('3 > 75'))
 # KeyError: False
 
-# print(df.query('`3` > 75'))
+# print(df_rename.query('`3` > 75'))
 # UndefinedVariableError: name 'BACKTICK_QUOTED_STRING_3' is not defined
 
-print(df[df[3] > 75])
+print(df_rename[df_rename[3] > 75])
 #    0name  age.year state name   3
 # 1    Bob        42         CA  92
 # 4  Ellen        24         CA  88
-
-df = pd.read_csv('data/src/sample_pandas_normal.csv')
 
 df.query('age > 25', inplace=True)
 print(df)
